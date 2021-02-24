6154320571b054c83b39bc21feebadfb19ede345
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-02-24 14:26:39 -0800

    2. Reading our MoMA Data Set

--- cleaning_and_preparing_data/main.py
+++ cleaning_and_preparing_data/main.py
@@ -4,18 +4,19 @@ def _open_data_set(file):
     file it needs to be a csv file
     """
     from csv import reader
-    with open(file) as file_opened:
-        file_reader = reader(file_opened)
-        return list(file_reader)
+    with open(file) as opened_file:
+        read_file = reader(opened_file)
+        return list(read_file)
 
 
-# Write your answer to the instructions below -- the list of
-# lists is stored using the variable name `moma`
-
+# Use the open() function to open the artworks.csv file. Assign the result to opened_file.
+# Use the reader() function to parse the data from opened_file. Assign the result to read_file.
+# Use list() to convert read_file into a list of lists. Assign the result to moma.
+# Use list slicing to remove the column names (the first row) from the moma list of lists.
 def main():
     moma = _open_data_set('artworks.csv')
-    num_rows = len(moma)
-    print(num_rows)
+    moma = moma[1:]
+    print(moma[:10])
 
 
 if __name__ == "__main__":
