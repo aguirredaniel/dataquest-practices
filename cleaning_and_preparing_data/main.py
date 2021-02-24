81b7278f898be785d68004899524b85096376276
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-02-24 14:43:12 -0800

    4. Cleaning the Nationality and Gender Columns

--- cleaning_and_preparing_data/main.py
+++ cleaning_and_preparing_data/main.py
@@ -9,14 +9,34 @@ def _open_data_set(file):
         return list(read_file)
 
 
-# We have created a variable, age1, containing the string "I am thirty-one years old".
+# Use a for loop to loop over the moma list of lists. In each iteration of the loop:
 #
-# Use the str.replace() method to create a new string, age2:
-# The new string should have the value "I am thirty-two years old".
+# Clean the Nationality column of the data set by:
+# - Assigning the nationality for each row (found at list index 2 of the row) to a variable.
+# - Using the str.replace() method to remove the open parentheses (() character.
+# - Using the str.replace() method to remove the close parentheses ()) character.
+# - Assigning the cleaned value back to list index 2 of the row.
+# Clean the Gender column of the data set (found at index 5 of the row) by repeating the same technique you used for the Nationality column.
 def main():
-    age1 = "I am thirty-one years old"
-    age2 = age1.replace('one', 'two')
-    print(age2)
+    moma = _open_data_set('artworks.csv')
+    # Removing header row
+    moma = moma[1:]
+
+    for row in moma:
+        # Cleaning the Nationality column, removing parenthesis => '(', ')'
+        nationality = row[2]
+        nationality = nationality.replace('(', '')
+        nationality = nationality.replace(')', '')
+        row[2] = nationality
+
+        # Cleaning the Gender column, same of Nationality column
+        gender = row[5]
+        gender = gender.replace('(', '')
+        gender = gender.replace(')', '')
+        row[5] = gender
+
+    for i in range(10):
+        print('Title: ', moma[i][0], ' Nationality: ', moma[1][2], ' Gender: ', moma[1][5])
 
 
 if __name__ == "__main__":
