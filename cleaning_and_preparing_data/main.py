1d2a87319a58a95b6429731a693647a124abe8be
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-02-25 13:10:57 -0800

    7. Parsing Numbers from Complex Strings, Part One

--- cleaning_and_preparing_data/main.py
+++ cleaning_and_preparing_data/main.py
@@ -11,7 +11,7 @@ def _open_data_set(file):
 
 def _clean_and_convert(date: str):
     """
-    Clean a year date value, removing removing parenthesis => '(', ')'.
+    Clean a year date value, removing parenthesis => '(', ')'.
     Return the date value as int.
     Example date param: '(1992)'
     """
@@ -22,6 +22,17 @@ def _clean_and_convert(date: str):
     date = date.replace(')', '')
 
     return int(date)
+
+
+def strip_characters(string, bad_characters):
+    """
+    Iterates over a list of "bad" characters, and removing all occurrences in a string.
+    """
+    cleaned_string = string
+    for bad_character in bad_characters:
+        cleaned_string = cleaned_string.replace(bad_character, '')
+
+    return cleaned_string
 
 
 # Use a for loop to iterate over each row in the moma list of lists. In each iteration:
@@ -67,5 +78,27 @@ def main():
         row[4] = _clean_and_convert(end_date)
 
 
+# 1. Create a function called strip_characters(), which accepts a string argument and:
+#  - Iterates over the bad_chars list, using str.replace() to remove each character.
+#  - Returns the cleaned string.
+# 2. Create an empty list, stripped_test_data.
+# 3. Iterate over the strings in test_data, and on each iteration:
+# - Use the function you created earlier to clean the string.
+# - Append the cleaned string to the stripped_test_data list.
+def strip_characters_practice():
+    test_data = ["1912", "1929", "1913-1923",
+                 "(1951)", "1994", "1934",
+                 "c. 1915", "1995", "c. 1912",
+                 "(1988)", "2002", "1957-1959",
+                 "c. 1955.", "c. 1970's",
+                 "C. 1990-1999"]
+
+    bad_chars = ["(", ")", "c", "C", ".", "s", "'", " "]
+
+    stripped_test_data = [strip_characters(data, bad_chars) for data in test_data]
+
+    print(stripped_test_data)
+
+
 if __name__ == "__main__":
-    main()
+    strip_characters_practice()
