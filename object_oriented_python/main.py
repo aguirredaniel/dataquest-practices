cfee8dfc0e472ae00afda46381029a47a2c3da76
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-03-15 19:17:34 -0700

    1. Introduction

---
+++ object_oriented_python/main.py
@@ -0,0 +1,18 @@
+# In the code editor, we have defined one variable of each of the three types we've worked with so far.
+# Follow each of the instructions below, in order.
+#
+# 1. Use the print() function to display the type of the list l.
+# 2. Use the print() function to display the type of the string s.
+# 3. Use the print() function to display the type of the dictionary d.
+def main():
+    l = [1, 2, 3]
+    s = "string"
+    d = {"a": 1, "b": 2}
+
+    print(type(l))
+    print(type(s))
+    print(type(d))
+
+
+if __name__ == "__main__":
+    main()
