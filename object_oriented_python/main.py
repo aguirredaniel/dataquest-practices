6a2ae66e97e6deefc13c778c654a57f95d2c1ca1
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-03-15 20:22:04 -0700

    9. Creating an Append Method

--- object_oriented_python/main.py
+++ object_oriented_python/main.py
@@ -1,14 +1,44 @@
-class MyClass:
-    def first_method(self):
-        return 'This is my first method'
+class MyList:
+    """ Represent a typical List
+
+      Example:
+        list = MyList([1,2,3])
+    """
+
+    def __init__(self, initial_data):
+        self.data = initial_data
+
+    def append(self, new_item):
+        """ Add an element to the end of the list
+
+        Args:
+            new_item: item to append in the list
+
+        Example:
+            list.append(17)
+
+            The values in the list are:
+            [..., 17]
+        """
+        list_item = [new_item]
+        self.data += list_item
 
 
-# 1. Remove the pass statement.
-# 2. Inside the class, define a method called first_method().
-# 3. Inside the method, return the string "This is my first method".
-# 4. Outside of the class, create an instance of MyClass and assign it to a variable name my_instance.
+# 1. Inside the MyList class, define a new append() method with two arguments:
+#  - self: The self-references to the instance.
+#  - new_item: The new item that we want to add to the list.
+# 2. Implement the append() method so that it appends the provided new_item to the list stored in self.data.
+# 3. Outside of the class, create an instance of MyList providing the list [1, 2, 3, 4, 5].
+#    Assign it to a variable named my_list.
+# 4. Print the value of my_list.data.
+# 5. Use the append() method to append value 6 to my_list.
+# 6. Print the value of my_list.data. Observe that it now contains the 6 that we added.
 def main():
-    my_instance = MyClass()
+    my_list = MyList([1, 2, 3, 4, 5])
+    print(my_list.data)
+
+    my_list.append(6)
+    print(my_list.data)
 
 
 if __name__ == "__main__":
