b41ee74b11e447bf46937abc335e29ac87edb63c
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-03-29 18:51:11 -0700

    5. Using Strptime to Parse Strings as Dates

--- working_with_dates_and_times/main.py
+++ working_with_dates_and_times/main.py
@@ -1,5 +1,7 @@
 import datetime as dt
 
+from enum import IntEnum
+
 
 def _open_data_set(file):
     """  Open a csv file and return a list of list (dataset).
@@ -29,14 +31,32 @@ def _open_data_set(file):
         return list(read_file)
 
 
-# 1. Import the datetime class using the alias dt.
-# 2. Instantiate a datetime object representing midnight on June 16, 1911.
-#    Assign the object to the variable name ibm_founded.
-# 3. Instantiate a datetime object representing 8:17 p.m. on July 20, 1969.
-#    Assign the object to the variable name man_on_moon.
+class POTUSRows(IntEnum):
+    """ An Enum that helps make the code expressive when the POTUS dataset columns are mapped.
+      """
+    APPT_START_DATE = 2
+
+
+# 1. Create a string date_format that specifies the format of the appt_start_date column:
+#  - The format of the app_start_date column is {month}/{day}/{two digit year} {hour 24hr time}:{minute}.
+#  - Substitute each of the values inside braces with the appropriate strftime code from the table above.
+# 2. Iterate over each row in the potus list of lists:
+#  - Assign the appt_start_date column, found at index 2 of each row, to a variable.
+#  - Use the datetime.strptime() constructor to convert the variable from a string to a datetime object,
+#    using the date_format string you created earlier.
+#  - Assign the datetime object back to index 2 of the row.
 def main():
-    ibm_founded = dt.datetime(1911, 6, 16)
-    man_on_moon = dt.datetime(1969, 7, 20, 20, 17)
+    potus = _open_data_set('potus_visitors_2015.csv')
+    # Removing row header
+    potus = potus[1:]
+
+    # strftime code for appt_start_date row values
+    date_format = '%m/%d/%y %H:%M'
+
+    for row in potus:
+        appt_start_date = row[POTUSRows.APPT_START_DATE]
+        row[POTUSRows.APPT_START_DATE] = dt.datetime.strptime(appt_start_date, date_format)
+
 
 if __name__ == "__main__":
     main()
