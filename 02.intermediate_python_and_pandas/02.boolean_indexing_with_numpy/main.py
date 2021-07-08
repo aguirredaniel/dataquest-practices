068140e4ad2b41f8e38f500a4f1f5767334034ff
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-08 16:59:54 -0700

    07. Assignment Using Boolean Arrays

--- 02.intermediate_python_and_pandas/02.boolean_indexing_with_numpy/main.py
+++ 02.intermediate_python_and_pandas/02.boolean_indexing_with_numpy/main.py
@@ -1,18 +1,14 @@
 import numpy as np
 
 
-# 1. The value at column index 5 (pickup_location) of row index 1066 is incorrect. Use assignment to change this value
-#    to 1 in the taxi_modified ndarray.
-# 2. The first column (index 0) contains year values as four digit numbers in the format YYYY (2016, since all trips in
-#    our data set are from 2016). Use assignment to change these values to the YY format (16) in the taxi_modified ndarray.
-# 3. The values at column index 7 (trip_distance) of rows index 550 and 551 are incorrect. Use assignment to change
-#    these values in the taxi_modified ndarray to the mean value for that column.
+# 1. Select the fourteenth column (index 13) in taxi_copy. Assign it to a variable named total_amount.
+# 2. For rows where the value of total_amount is less than 0, use assignment to change the value to 0.
 def main():
     taxi = np.genfromtxt('../nyc_taxis.csv', delimiter=',', skip_header=1)
-    taxi_modified = taxi.copy()
-    taxi_modified[1066, 5] = 1
-    taxi_modified[:, 0] = 16
-    taxi_modified[[550, 551], 7] = taxi_modified[:, 7].mean()
+    taxi_copy = taxi.copy()
+
+    total_amount = taxi_copy[:, 13]
+    total_amount[total_amount < 0] = 0
 
 
 if __name__ == "__main__":
