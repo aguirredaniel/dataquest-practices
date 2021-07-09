3443fccce36eddf0c3ccd604f21b160ad5d2e76e
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-08 20:01:21 -0700

    09. Challenge: Which is the most popular airport?

--- 02.intermediate_python_and_pandas/02.boolean_indexing_with_numpy/main.py
+++ 02.intermediate_python_and_pandas/02.boolean_indexing_with_numpy/main.py
@@ -1,22 +1,31 @@
 import numpy as np
 
 
-# 1. In our new column at index 15, assign the value 1 if the pickup_location_code (column index 5) corresponds to an
-#    airport location, leaving the value as 0 otherwise by performing these three operations:
-# 2. For rows where the value for the column index 5 is equal to 2 (JFK Airport), assign the value 1 to column index 15.
-# 3. For rows where the value for the column index 5 is equal to 3 (LaGuardia Airport), assign the value 1 to column
-#    index 15.
-# 4. For rows where the value for the column index 5 is equal to 5 (Newark Airport), assign the value 1 to column
-#    index 15.
+# 1. Using the original taxi ndarray, calculate how many trips had JFK Airport as their destination:
+#   - Use boolean indexing to select only the rows where the dropoff_location_code column (column index 6) has a value
+#     that corresponds to JFK. Assign the result to jfk.
+#   - Calculate how many rows are in the new jfk array and assign the result to jfk_count.
+# 2. Calculate how many trips from taxi had Laguardia Airport as their destination:
+#   - Use boolean indexing to select only the rows where the dropoff_location_code column (column index 6) has a value
+#     that corresponds to Laguardia. Assign the result to laguardia.
+#   - Calculate how many rows are in the new laguardia array. Assign the result to laguardia_count.
+# 3. Calculate how many trips from taxi had Newark Airport as their destination:
+#   - Select only the rows where the dropoff_location_code column has a value that corresponds to Newark, and assign the
+#     result to newark.
+#   - Calculate how many rows are in the new newark array and assign the result to newark_count.
 def main():
     taxi = np.genfromtxt('../nyc_taxis.csv', delimiter=',', skip_header=1)
-    # create a new column filled with `0`.
-    zeros = np.zeros([taxi.shape[0], 1])
-    taxi_modified = np.concatenate([taxi, zeros], axis=1)
 
-    taxi_modified[taxi_modified[:, 5] == 2, 15] = 1
-    taxi_modified[taxi_modified[:, 5] == 3, 15] = 1
-    taxi_modified[taxi_modified[:, 5] == 5, 15] = 1
+    jfk = taxi[:, 6] == 2
+    jfk_count = taxi[jfk].shape[0]
+
+    laguardia = taxi[:, 6] == 3
+    laguardia_count = taxi[laguardia].shape[0]
+
+    newark = taxi[:, 6] == 5
+    newark_count = taxi[newark].shape[0]
+
+    print(jfk_count, laguardia_count, newark_count, sep='\n')
 
 
 if __name__ == "__main__":
