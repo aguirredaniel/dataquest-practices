3146a40a9df9af049ffcfc504c3499502fd1709b
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-12 19:37:41 -0700

    8. Selecting Rows From a DataFrame by Label

--- 02.intermediate_python_and_pandas/03.introduction_pandas/main.py
+++ 02.intermediate_python_and_pandas/03.introduction_pandas/main.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
 
-# 1. Select the country column. Assign the result to the variable name countries.
-# 2. In order, select the revenues and years_on_global_500_list columns. Assign the result to the variable name
-#    revenues_years.
-# 3. In order, select all columns from ceo up to and including sector. Assign the result to the variable name
-#    ceo_to_sector.
+# 1. Create a new variable toyota, with:
+#     - Just the row with index Toyota Motor.
+#     - All columns.
+# 2. Create a new variable, drink_companies, with:
+#     - Rows with indicies Anheuser-Busch InBev, Coca-Cola, and Heineken Holding, in that order.
+#     - All columns.
+# 3 . Create a new variable, middle_companies with:
+#      - All rows with indicies from Tata Motors to Nationwide, inclusive.
+#      - All columns from rank to country, inclusive.
 def main():
-    f500 = pd.read_csv('../f500.csv', index_col=1)
-    countries = f500['country']
-    revenues_years = f500[['revenues', 'years_on_global_500_list']]
-    ceo_to_sector = f500.loc[:, 'ceo':'sector']
-    print(countries, revenues_years, ceo_to_sector, sep='\n')
+    f500 = pd.read_csv('../f500.csv', index_col=0)
+    toyota = f500.loc['Toyota Motor']
+    drink_companies = f500.loc[['Anheuser-Busch InBev', 'Coca-Cola', 'Heineken Holding']]
+    middle_companies = f500.loc['Tata Motors':'Nationwide', 'rank':'country']
+    print(toyota, drink_companies, middle_companies, sep='\n')
 
 
 if __name__ == "__main__":
