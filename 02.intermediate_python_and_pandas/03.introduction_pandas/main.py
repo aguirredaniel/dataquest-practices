6d43a5bc9226fd1d67fcdadde3fb1097446ff0dd
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-12 19:06:14 -0700

    3. Introducing DataFrames

--- 02.intermediate_python_and_pandas/03.introduction_pandas/main.py
+++ 02.intermediate_python_and_pandas/03.introduction_pandas/main.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
 
-# 1. Use Python's type() function to assign the type of f500 to f500_type.
-# 2. Use the DataFrame.shape attribute to assign the shape of f500 to f500_shape.
+# 1. Use the head() method to select the first 6 rows. Assign the result to f500_head.
+# 2. Use the tail() method to select the last 8 rows. Assign the result to f500_tail.
 def main():
     f500 = pd.read_csv('../f500.csv', index_col=1)
-    f500_type = type(f500)
-    f500_shape = f500.shape
-
-    print(f500_type, f500_shape, sep='\n')
+    f500_head = f500.head(6)
+    f500_tail = f500.tail(8)
+    print(f500_head, f500_tail, sep='\n')
 
 
 if __name__ == "__main__":
