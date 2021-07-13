4e4304fd05042249e51d8b1749fe42a6c11741dc
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-12 19:26:27 -0700

    07. Selecting Columns From a DataFrame by Label Continued

--- 02.intermediate_python_and_pandas/03.introduction_pandas/main.py
+++ 02.intermediate_python_and_pandas/03.introduction_pandas/main.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
 
-# 1. Use the head() method to select the first 6 rows. Assign the result to f500_head.
-# 2. Use the tail() method to select the last 8 rows. Assign the result to f500_tail.
+# 1. Select the country column. Assign the result to the variable name countries.
+# 2. In order, select the revenues and years_on_global_500_list columns. Assign the result to the variable name
+#    revenues_years.
+# 3. In order, select all columns from ceo up to and including sector. Assign the result to the variable name
+#    ceo_to_sector.
 def main():
     f500 = pd.read_csv('../f500.csv', index_col=1)
-    f500_head = f500.head(6)
-    f500_tail = f500.tail(8)
-    print(f500_head, f500_tail, sep='\n')
+    countries = f500['country']
+    revenues_years = f500[['revenues', 'years_on_global_500_list']]
+    ceo_to_sector = f500.loc[:, 'ceo':'sector']
+    print(countries, revenues_years, ceo_to_sector, sep='\n')
 
 
 if __name__ == "__main__":
