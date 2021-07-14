4168abe09848c8cace3ace264c6148225e8303f5
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-13 20:19:30 -0700

    10. Using Boolean Arrays to Assign Values

--- 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
+++ 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
@@ -1,16 +1,19 @@
+import numpy as np
 import pandas as pd
 
 
-# 1. Create a boolean series, motor_bool, that compares whether the values in the industry column from the f500
-#    dataframe are equal to "Motor Vehicles and Parts".
-# 2. Use the motor_bool boolean series to index the country column. Assign the result to motor_countries.
+# 1. Use boolean indexing to update values in the previous_rank column of the f500 dataframe:
+#     - There should now be a value of np.nan where there previously was a value of 0.
+#     - It is up to you whether you assign the boolean series to its own variable first, or whether you complete the
+#       operation in one line.
+# 2. Create a new pandas series, prev_rank_after, using the same syntax that was used to create the prev_rank_before
+#    series.
 def main():
     f500 = pd.read_csv('../f500.csv')
-    motor_bool = f500['industry'] == 'Motor Vehicles and Parts'
-    motor_countries = f500.loc[motor_bool, 'country']
+    f500.loc[f500['previous_rank'] == 0, 'previous_rank'] = np.nan
+    prev_rank_after = f500['previous_rank'].value_counts(dropna=False).head()
 
-    print(motor_countries)
-
+    print(prev_rank_after)
 
 
 if __name__ == "__main__":
