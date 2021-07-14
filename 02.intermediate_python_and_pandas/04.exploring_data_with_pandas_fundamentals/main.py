04f6f9dc1f977c50088518e9a7826399de3f5bd8
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-13 20:24:05 -0700

    11. Creating New Columns

--- 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
+++ 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
@@ -2,18 +2,16 @@ import numpy as np
 import pandas as pd
 
 
-# 1. Use boolean indexing to update values in the previous_rank column of the f500 dataframe:
-#     - There should now be a value of np.nan where there previously was a value of 0.
-#     - It is up to you whether you assign the boolean series to its own variable first, or whether you complete the
-#       operation in one line.
-# 2. Create a new pandas series, prev_rank_after, using the same syntax that was used to create the prev_rank_before
-#    series.
+# 1. Add a new column named rank_change to the f500 dataframe by subtracting the values in the rank column from the
+#    values in the previous_rank column.
+# 2. Use the Series.describe() method to return a series of descriptive statistics for the rank_change column. Assign
+#    the result to rank_change_desc
 def main():
     f500 = pd.read_csv('../f500.csv')
-    f500.loc[f500['previous_rank'] == 0, 'previous_rank'] = np.nan
-    prev_rank_after = f500['previous_rank'].value_counts(dropna=False).head()
+    f500['rank_change'] = f500['rank'] - f500['previous_rank']
+    rank_change_desc = f500['rank_change'].describe()
 
-    print(prev_rank_after)
+    print(rank_change_desc)
 
 
 if __name__ == "__main__":
