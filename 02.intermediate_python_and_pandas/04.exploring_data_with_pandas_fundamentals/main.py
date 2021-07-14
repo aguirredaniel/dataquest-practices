bf8daf3fc37423264c53e78c8e6c3ebbdb131890
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-13 19:26:36 -0700

    06. Dataframe Exploration Methods

--- 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
+++ 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 
-# 1. Use Series.value_counts() and Series.loc to return the number of companies with a value of 0 in the previous_rank
-#    column in the f500 dataframe. Assign the results to zero_previous_rank
+# 1. Use the DataFrame.max() method to find the maximum value for only the numeric columns from f500
+#    (you may need to check the documentation). Assign the result to the variable max_f500.
 def main():
     f500 = pd.read_csv('../f500.csv')
-    zero_previous_rank = f500["previous_rank"].value_counts().loc[0]
+    max_f500 = f500.max(numeric_only=True)
 
-    print(zero_previous_rank, sep='\n')
+    print(max_f500, sep='\n')
 
 
 if __name__ == "__main__":
