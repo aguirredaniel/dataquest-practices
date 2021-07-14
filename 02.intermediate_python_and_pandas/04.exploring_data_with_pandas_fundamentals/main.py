61b250cc65d4d63a48ed34f342593b4e58717bfd
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-13 19:19:47 -0700

    05. Method Chaining

--- 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
+++ 02.intermediate_python_and_pandas/04.exploring_data_with_pandas_fundamentals/main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 
-# 1. Subtract the values in the rank column from the values in the previous_rank column. Assign the result to
-#    rank_change.
+# 1. Use Series.value_counts() and Series.loc to return the number of companies with a value of 0 in the previous_rank
+#    column in the f500 dataframe. Assign the results to zero_previous_rank
 def main():
     f500 = pd.read_csv('../f500.csv')
-    rank_change = f500['previous_rank'] - f500['rank']
+    zero_previous_rank = f500["previous_rank"].value_counts().loc[0]
+
+    print(zero_previous_rank, sep='\n')
 
 
 if __name__ == "__main__":
