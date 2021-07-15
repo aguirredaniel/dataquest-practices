42b4f0d93ed4064b284cf6c4795ed0aa7fcbd49f
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-14 21:14:23 -0700

    11. Using Loops with pandas

--- 02.intermediate_python_and_pandas/05.exploring_data_with_pandas_intermediate/main.py
+++ 02.intermediate_python_and_pandas/05.exploring_data_with_pandas_intermediate/main.py
@@ -2,16 +2,24 @@ import numpy as np
 import pandas as pd
 
 
-# 1. Find the company headquartered in Japan with the largest number of employees.
-#    - Select only the rows that have a country name equal to Japan.
+# 1. Create an empty dictionary, top_employer_by_country to store the results of the exercise.
+# 2. Use the Series.unique() method to create an array of unique values from the country column.
+# 3. Use a for loop to iterate over the array unique countries. In each iteration:
+#    - Select only the rows that have a country name equal to the current iteration.
 #    - Use DataFrame.sort_values() to sort those rows by the employees column in descending order.
-#    - Use DataFrame.iloc[] to select the first row from the sorted dataframe.
-#    - Extract the company name from the index label company from the first row. Assign the result to
-#      top_japanese_employer.
+#    - Select the first row from the sorted dataframe.
+#    - Extract the company name from the index label company from the first row.
+#    - Assign the results to the top_employer_by_country dictionary, using the country name as the key, and the company
+#      name as the value.
 def main():
     f500 = pd.read_csv('../f500.csv')
-    top_japanese_employer = f500[(f500['country'] == 'Japan')].sort_values('employees', ascending=False)['company'].iloc[0]
-    print(top_japanese_employer)
+    top_employer_by_country = {}
+    countries = f500['country'].unique()
+    for c in countries:
+        top_employer_by_country[c] = f500[f500['country'] == c]. \
+            sort_values('employees', ascending=False).iloc[0]['company']
+
+    print(top_employer_by_country)
 
 
 if __name__ == "__main__":
