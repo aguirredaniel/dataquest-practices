0313a75aa33c2c50ab3509cbed46a109d5c34574
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-14 19:58:09 -0700

    3. Using iloc to select by integer position

--- 02.intermediate_python_and_pandas/05/exploring_data_with_pandas_intermediate/main.py
+++ 02.intermediate_python_and_pandas/05/exploring_data_with_pandas_intermediate/main.py
@@ -2,19 +2,14 @@ import numpy as np
 import pandas as pd
 
 
-# 1. Select the rank, revenues, and revenue_change columns in f500. Then, use the DataFrame.head() method to select
-#    the first five rows. Assign the result to f500_selection.
-# 2. Use the variable inspector to view f500_selection. Compare the results to the first few lines of our CSV file
-#    above.
+# 1. Select just the fifth row of the f500 dataframe. Assign the result to fifth_row.
+# 2. Select the value in first row of the company column. Assign the result to company_value.
 def main():
     f500 = pd.read_csv('../f500.csv')
-    f500.index.name = None
+    fifth_row = f500.iloc[4]
+    company_value = f500.iloc[0, 0]
 
-    f500.loc[f500['previous_rank'] == 0, 'previous_rank'] = np.nan
-
-    f500_selection = f500[['rank', 'revenues', 'revenue_change']].head()
-
-    print(f500_selection)
+    print(fifth_row, company_value, sep='\n')
 
 
 if __name__ == 'main':
