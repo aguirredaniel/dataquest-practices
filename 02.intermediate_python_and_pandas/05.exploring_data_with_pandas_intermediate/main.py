d2decc0274cf0fb1072b245e4ccd3bed9e0d6da6
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-14 21:23:14 -0700

    12. Challenge: Calculating Return on Assets by Country

--- 02.intermediate_python_and_pandas/05.exploring_data_with_pandas_intermediate/main.py
+++ 02.intermediate_python_and_pandas/05.exploring_data_with_pandas_intermediate/main.py
@@ -2,24 +2,19 @@ import numpy as np
 import pandas as pd
 
 
-# 1. Create an empty dictionary, top_employer_by_country to store the results of the exercise.
-# 2. Use the Series.unique() method to create an array of unique values from the country column.
-# 3. Use a for loop to iterate over the array unique countries. In each iteration:
-#    - Select only the rows that have a country name equal to the current iteration.
-#    - Use DataFrame.sort_values() to sort those rows by the employees column in descending order.
-#    - Select the first row from the sorted dataframe.
-#    - Extract the company name from the index label company from the first row.
-#    - Assign the results to the top_employer_by_country dictionary, using the country name as the key, and the company
-#      name as the value.
+# 1. Create a new column roa in the f500 dataframe, containing the return on assets metric for each company.
+# 2. Aggregate the data by the sector column, and create a dictionary top_roa_by_sector, with:
+#     - Dictionary keys with the sector name.
+#     - Dictionary values with the company name with the highest ROA value from that sector.
 def main():
     f500 = pd.read_csv('../f500.csv')
-    top_employer_by_country = {}
-    countries = f500['country'].unique()
-    for c in countries:
-        top_employer_by_country[c] = f500[f500['country'] == c]. \
-            sort_values('employees', ascending=False).iloc[0]['company']
+    f500['roa'] = f500['profits'] / f500['assets']
+    top_roa_by_sector = {}
+    sectors = f500['sector'].unique()
+    for s in sectors:
+        top_roa_by_sector[s] = f500[f500['sector'] == s].sort_values('roa', ascending=False).iloc[0]['company']
 
-    print(top_employer_by_country)
+    print(top_roa_by_sector)
 
 
 if __name__ == "__main__":
