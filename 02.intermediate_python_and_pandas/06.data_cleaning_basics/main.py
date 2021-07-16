4d8fb19a18e5c836755a70c2120674731c1059c6
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-07-15 21:12:30 -0700

    3. Cleaning Column Names Continued

--- 02.intermediate_python_and_pandas/06.data_cleaning_basics/main.py
+++ 02.intermediate_python_and_pandas/06.data_cleaning_basics/main.py
@@ -1,22 +1,74 @@
 import pandas as pd
 
 
-# 1. Remove any whitespace from the start and end of each column name.
-#    - Create an empty list named new_columns.
-#    - Use a for loop to iterate through each column name using the DataFrame.columns attribute. Inside the body of the
-#      for loop:
-#      - Use the str.strip() method to remove whitespace from the start and end of the string.
-#      - Append the updated column name to the new_columns list.
-#    - Assign the updated column names to the DataFrame.columns attribute.
+# 1. Define a function, which accepts a string argument, and:
+#    - Removes any whitespace from the start and end of the string.
+#    - Replaces the substring Operating System with the abbreviation os.
+#    - Replaces all spaces with underscores.
+#    - Removes parentheses from the string.
+#    - Makes the entire string lowercase.
+#    - Returns the modified string.
+# 2. Use a loop to apply the function to each item in the DataFrame.columns attribute for the laptops dataframe. Assign
+#    the result back to the DataFrame.columns attribute.
+
+def clean_column_name(column):
+    """
+    Return a clean name for a column.
+    The process make:
+        - Removes any whitespace from the start and end of the string.
+        - Replaces the substring Operating System with the abbreviation os.
+        - Replaces all spaces with underscores.
+        - Removes parentheses from the string.
+        - Makes the entire string lowercase.
+        - Returns the modified string.
+
+    Parameters
+    ----------
+    column : string
+        An column name.
+
+    Raises
+    ------
+    ValueError
+        If column parm is None or not a str.
+
+    Examples
+    --------
+    Consider a column name
+    >>> column_name = 'Model Name'
+    >>> clean_column_name(column_name)
+    'model_name'
+
+    >>> column_name = 'Operating System Version'
+    >>> column_name = clean_column_name(column_name)
+    'os_version'
+
+    >>> column_name = ' Price (Euros) '
+    >>> column_name = clean_column_name(column_name)
+    'price_euros'
+    """
+    if column is None or not isinstance(column, str):
+        raise ValueError('The column is None or not a string.🙁')
+
+    clean_column = column.strip()
+    clean_column = clean_column.replace('Operating System', 'os')
+    clean_column = clean_column.replace(' ', '_')
+    clean_column = clean_column.replace('(', '').replace(')', '')
+    clean_column = clean_column.lower()
+
+    return clean_column
+
+
 def main():
     laptops = pd.read_csv('../laptops.csv', encoding='Latin-1')
     new_columns = []
     for c in laptops.columns:
-        cleaned_column = c.strip()
-        new_columns.append(cleaned_column)
+        clean_column = clean_column_name(c)
+        new_columns.append(clean_column)
     laptops.columns = new_columns
 
     print(laptops.info())
+
 
 if __name__ == '__main__':
     main()
