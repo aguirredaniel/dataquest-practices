201500dbcaba461a3243f2ab77cd54a03f198007
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-02 16:58:20 -0700

    2. Graphs

---
+++ 02.intermediate_python_and_pandas/07.line-graphs-and-time-series/main.py
@@ -0,0 +1,16 @@
+# - What is the unit length of the x-axis? Assign your answer to x_unit_length.
+# - What is the unit length of the y-axis? Assign your answer to y_unit_length.
+# - What is x-coordinate of point A? Assign your answer to x_coordinate_A.
+# - What is the y-coordinate of point B? Assign your answer to y_coordinate_B.
+# - What are the x- and y-coordinates of point C? Assign your answer as a Python list to C_coordinates — the
+#   x-coordinate must come first in your list.
+def main():
+    x_unit_length = 10
+    y_unit_length = 1000
+    x_coordinate_A = 70
+    y_coordinate_B = 5000
+    C_coordinates = [50, 9000]
+
+
+if __name__ == '__main__':
+    main()
