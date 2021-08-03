cc57f38c11746901d657ca2602363296ec4a0e7f
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-02 18:17:25 -0700

    5. Customizing a Graph

--- 02.intermediate_python_and_pandas/07.line-graphs-and-time-series/main.py
+++ 02.intermediate_python_and_pandas/07.line-graphs-and-time-series/main.py
@@ -1,16 +1,23 @@
-import matplotlib.pyplot as ptl
+import matplotlib.pyplot as plt
 
 
-# - Import the pyplot submodule as plt.
-# - Plot a line graph using plt.plot(), where month_number gives the x-coordinates, and new_deaths gives the
-#   y_coordinates.
+# - Plot a line graph using plt.plot().
+#   - month_number gives the x-coordinates
+#   - new_deaths gives the y_coordinates
+# - Using plt.title(), add the title 'New Reported Deaths By Month (Globally)'. For answer-checking purposes, make sure
+#   the title you add is exactly the same (the answer-checking is case sensitive).
+# - Using plt.xlabel(), add the x-label 'Month Number'.
+# - Using plt.ylabel(), add the y-label 'Number Of Deaths'.
 # - Display the plot using plt.show().
 def main():
     month_number = [1, 2, 3, 4, 5, 6, 7]
     new_deaths = [213, 2729, 37718, 184064, 143119, 136073, 165003]
 
-    ptl.plot(month_number, new_deaths)
-    ptl.show()
+    plt.plot(month_number, new_deaths)
+    plt.title('New Reported Deaths By Month (Globally)')
+    plt.xlabel('Month Number')
+    plt.ylabel('Number Of Deaths')
+    plt.show()
 
 
 if __name__ == '__main__':
