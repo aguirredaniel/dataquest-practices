d822638163dd0c446d5c70d09fd5f59a7c115915
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-04 15:49:20 -0700

    2. Exploring Data

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
@@ -1,16 +1,32 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-# - Import the pandas module as pd.
-# - Read in the day.csv file using the pd.read_csv() function. Assign the result to a variable named bike_sharing.
-# - Examine the first and the last five rows.
-# - Display information about the dataset using the DataFrame.info() method. How many rows and columns does this dataset
-#   have? Do you see any missing values?
+# - Plot two line graphs sharing the same x- and y-axis.
+#   - For the first graph, plot the dteday column on the x-axis and the casual column on the y-axis.
+#   - For the second graph, plot dteday on the x-axis and registered on the y-axis.
+# - Rotate the x-ticks to an angle of 30 degrees using plt.xticks().
+# - Add the label 'Bikes Rented' to the y-axis using plt.ylabel().
+# - Add the label 'Date' to the x-axis using plt.xlabel().
+# - Add the title 'Bikes Rented: Casual vs. Registered' using plt.title().
+# - Add a legend using plt.legend(). Use 'Casual' and 'Registered' as labels.
+# - What differences and similarities do you see for the two line graphs?
 def main():
     bike_sharing = pd.read_csv('../day.csv')
-    print(bike_sharing.head())
-    print(bike_sharing.tail())
-    print(bike_sharing.info(()))
+    bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
+
+    plt.plot(bike_sharing['dteday'], bike_sharing['casual'])
+    plt.plot(bike_sharing['dteday'], bike_sharing['registered'])
+
+    plt.xticks(rotation=30)
+
+    plt.ylabel('Bikes Rented')
+    plt.xlabel('Date')
+    plt.title('Bikes Rented: Casual vs. Registered')
+
+    plt.legend(labels=['Casual', 'Registered'])
+
+    plt.show()
 
 
 if __name__ == '__main__':
