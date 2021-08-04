406047dd98ae320e8bf7c30eb2b5cc1a62c05b49
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-04 15:58:36 -0700

    3. Seasonal Trends

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
@@ -2,30 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Plot two line graphs sharing the same x- and y-axis.
-#   - For the first graph, plot the dteday column on the x-axis and the casual column on the y-axis.
-#   - For the second graph, plot dteday on the x-axis and registered on the y-axis.
-# - Rotate the x-ticks to an angle of 30 degrees using plt.xticks().
-# - Add the label 'Bikes Rented' to the y-axis using plt.ylabel().
-# - Add the label 'Date' to the x-axis using plt.xlabel().
-# - Add the title 'Bikes Rented: Casual vs. Registered' using plt.title().
-# - Add a legend using plt.legend(). Use 'Casual' and 'Registered' as labels.
-# - What differences and similarities do you see for the two line graphs?
+# - Plot a line graph with the dteday column on the x-axis and the temp column on the y-axis.
+# - Rotate the x-ticks to an angle of 45 degrees using plt.xticks().
+# - Do you see any similarity in how the temperature and the number of bikes rented change?
 def main():
     bike_sharing = pd.read_csv('../day.csv')
     bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
 
-    plt.plot(bike_sharing['dteday'], bike_sharing['casual'])
-    plt.plot(bike_sharing['dteday'], bike_sharing['registered'])
-
-    plt.xticks(rotation=30)
-
-    plt.ylabel('Bikes Rented')
-    plt.xlabel('Date')
-    plt.title('Bikes Rented: Casual vs. Registered')
-
-    plt.legend(labels=['Casual', 'Registered'])
-
+    plt.plot(bike_sharing['dteday'], bike_sharing['temp'])
+    plt.xticks(rotation=45)
     plt.show()
 
 
