b3ffc7dae2179fd0d1644604b6db42a32e0ca932
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-03 18:54:34 -0700

    8. The Uniform Distribution

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations/main.py
@@ -2,24 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - The distribution of the registered column looks similar to a normal distribution. Assign the Boolean True or False
-#   to sentence_1.
-# - Most days, registered users rent between 6,000 and 7,000 bikes. Assign True or False to sentence_2.
-# - It's relatively rare that registered users rent fewer than 500 bikes in one day. Assign True or False to sentence_3.
-# - The tallest bar on the histogram corresponds to a frequency of about 140 days. Assign True or False to sentence_4.
-# - The histogram above has a total of 12 bars. Assign True or False to sentence_5.
+# - The casual distribution is not symmetrical. Assign the Boolean True or False to sentence_1.
+# - Most days, casual users rent over 2,000 bikes. Assign True or False to sentence_2.
+# - The cnt distribution is approximately symmetrical, and it looks more like a uniform distribution than a normal
+#   distribution. Assign True or False to sentence_3.
+# - The tallest bar on the cnt histogram corresponds to about 200 days. Assign True or False to sentence_4.
 def main():
     bike_sharing = pd.read_csv('../day.csv')
     bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
 
-    plt.hist(bike_sharing['registered'])
+    plt.hist(bike_sharing['cnt'])
+    plt.show()
+
+    plt.hist(bike_sharing['casual'])
     plt.show()
 
     sentence_1 = True
     sentence_2 = False
-    sentence_3 = True
-    sentence_4 = True
-    sentence_5 = False
+    sentence_3 = False
+    sentence_4 = False
 
 
 if __name__ == '__main__':
