90c297c6b39337c614b093011a37f69a135b7f21
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-04 17:41:36 -0700

    7. Measuring Pearson's r

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
@@ -2,20 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - The correlation between temp and cnt is +0.63, so it's stronger than the correlation between windspeed and cnt,
-#   which is -0.23 (the two correlation values are real). Assign the Boolean True or False to sentence_1.
-# - A correlation of +0.09 shows a weak positive correlation. Assign True or False to sentence_2.
-# - A correlation of 0 shows a very weak negative correlation. Assign True or False to sentence_3.
-# - A perfect positive correlation is equal in strength with a perfect negative correlation. Assign True or False to
-#   sentence_4.
+# - Calculate the Pearson's r between the temp and atemp columns. Assign your answer to temp_atemp_corr.
+# - Calculate the Pearson's r between the windspeed and hum columns. Assign your answer to wind_hum_corr.
+# - Generate a scatter plot with the temp column on the x-axis and the atemp column on the y-axis.
+#   - Add 'Air Temperature' as an x-label.
+#   - Add 'Feeling Temperature' as an y-label.
+# - Generate a scatter plot with the windspeed column on the x-axis and the hum column on the y-axis. You'll first need
+#   to close the previous scatter plot using plt.show()
+#   - Add 'Wind Speed' as an x-label.
+#   - Add 'Humidity' as an y-label.
 def main():
     bike_sharing = pd.read_csv('../day.csv')
     bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
 
-    sentence_1 = True
-    sentence_2 = True
-    sentence_3 = False
-    sentence_4 = True
+    temp_atemp_corr = bike_sharing['temp'].corr(bike_sharing['atemp'])
+    wind_hum_corr = bike_sharing['windspeed'].corr(bike_sharing['hum'])
+    print(temp_atemp_corr, wind_hum_corr, sep='\n')
+
+    plt.scatter(bike_sharing['temp'], bike_sharing['atemp'])
+    plt.xlabel('Air Temperature')
+    plt.ylabel('Feeling Temperature')
+    plt.show()
+
+    plt.scatter(bike_sharing['windspeed'], bike_sharing['hum'])
+    plt.xlabel('Wind Speed')
+    plt.ylabel('Humidity')
+    plt.show()
 
 
 if __name__ == '__main__':
