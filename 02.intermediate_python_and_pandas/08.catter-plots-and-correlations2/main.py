8bde40b4e7e3104ea987d03341ede96dbec1075c
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-04 17:31:45 -0700

    6. Pearson Correlation Coefficient

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
@@ -2,18 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Generate a scatter plot with the atemp column (feeling temperature) on the x-axis and the registered column on the
-#   y-axis.
-# - Run your code without submitting the answer. Inspect the resulting scatter plot.
-# - What type of correlation do you see between the atemp and registered columns? Assign your answer to the variable
-#   correlation — choose between the strings 'positive', 'negative', and 'no correlation'.
+# - The correlation between temp and cnt is +0.63, so it's stronger than the correlation between windspeed and cnt,
+#   which is -0.23 (the two correlation values are real). Assign the Boolean True or False to sentence_1.
+# - A correlation of +0.09 shows a weak positive correlation. Assign True or False to sentence_2.
+# - A correlation of 0 shows a very weak negative correlation. Assign True or False to sentence_3.
+# - A perfect positive correlation is equal in strength with a perfect negative correlation. Assign True or False to
+#   sentence_4.
 def main():
     bike_sharing = pd.read_csv('../day.csv')
     bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
 
-    plt.scatter(bike_sharing['atemp'], bike_sharing['registered'])
-    plt.show()
-    correlation = 'positive'
+    sentence_1 = True
+    sentence_2 = True
+    sentence_3 = False
+    sentence_4 = True
 
 
 if __name__ == '__main__':
