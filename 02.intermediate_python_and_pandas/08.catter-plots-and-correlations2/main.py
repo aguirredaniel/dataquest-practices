7f2ba0ad0ba5d963dc4c92c32cdd9dfc01e1eee9
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-04 17:18:37 -0700

    4. Scatter Plots

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
@@ -2,15 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Plot a line graph with the dteday column on the x-axis and the temp column on the y-axis.
-# - Rotate the x-ticks to an angle of 45 degrees using plt.xticks().
-# - Do you see any similarity in how the temperature and the number of bikes rented change?
+# - Generate a scatter plot with the windspeed column on the x-axis and the cnt column on the y-axis. Use the
+#   plt.scatter() function.
+# - Add the 'Wind Speed' x-axis label using plt.xlabel().
+# - Add the 'Bikes Rented' y-axis label using plt.ylabel().
+# - Display the plot using plt.show()
 def main():
     bike_sharing = pd.read_csv('../day.csv')
     bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
 
-    plt.plot(bike_sharing['dteday'], bike_sharing['temp'])
-    plt.xticks(rotation=45)
+    plt.scatter(bike_sharing['windspeed'], bike_sharing['cnt'])
+    plt.xlabel('Wind Speed')
+    plt.ylabel('Bikes Rented')
     plt.show()
 
 
