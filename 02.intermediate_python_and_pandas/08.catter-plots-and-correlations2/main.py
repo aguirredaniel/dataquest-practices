db7b64a88d72de27aacf21f30cf5133e98a88e24
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-04 17:26:11 -0700

    5. Correlation

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
@@ -2,19 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Generate a scatter plot with the windspeed column on the x-axis and the cnt column on the y-axis. Use the
-#   plt.scatter() function.
-# - Add the 'Wind Speed' x-axis label using plt.xlabel().
-# - Add the 'Bikes Rented' y-axis label using plt.ylabel().
-# - Display the plot using plt.show()
+# - Generate a scatter plot with the atemp column (feeling temperature) on the x-axis and the registered column on the
+#   y-axis.
+# - Run your code without submitting the answer. Inspect the resulting scatter plot.
+# - What type of correlation do you see between the atemp and registered columns? Assign your answer to the variable
+#   correlation — choose between the strings 'positive', 'negative', and 'no correlation'.
 def main():
     bike_sharing = pd.read_csv('../day.csv')
     bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
 
-    plt.scatter(bike_sharing['windspeed'], bike_sharing['cnt'])
-    plt.xlabel('Wind Speed')
-    plt.ylabel('Bikes Rented')
+    plt.scatter(bike_sharing['atemp'], bike_sharing['registered'])
     plt.show()
+    correlation = 'positive'
 
 
 if __name__ == '__main__':
