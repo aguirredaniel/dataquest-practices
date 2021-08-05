60385015e82f23d4c78f5214a2f6a7db846c2561
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-04 17:59:00 -0700

    8. Categorical Columns

--- 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
+++ 02.intermediate_python_and_pandas/08.catter-plots-and-correlations2/main.py
@@ -2,32 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Calculate the Pearson's r between the temp and atemp columns. Assign your answer to temp_atemp_corr.
-# - Calculate the Pearson's r between the windspeed and hum columns. Assign your answer to wind_hum_corr.
-# - Generate a scatter plot with the temp column on the x-axis and the atemp column on the y-axis.
-#   - Add 'Air Temperature' as an x-label.
-#   - Add 'Feeling Temperature' as an y-label.
-# - Generate a scatter plot with the windspeed column on the x-axis and the hum column on the y-axis. You'll first need
-#   to close the previous scatter plot using plt.show()
-#   - Add 'Wind Speed' as an x-label.
-#   - Add 'Humidity' as an y-label.
+# - The worse the weather, the higher the encoding number in the weathersit column. Assign True or False to sentence_1.
+# - The correlation between weathersit and cnt is -0.30 — this means that as the weather gets worse, the number of bike
+#   rentals tends to decrease. Assign True or False to sentence_2.
+# - The correlation between weathersit and hum is +0.59 — this means that as the humidity decreases, the weather tends
+#   to get better. Assign True or False to sentence_3.
 def main():
     bike_sharing = pd.read_csv('../day.csv')
     bike_sharing['dteday'] = pd.to_datetime(bike_sharing['dteday'])
 
-    temp_atemp_corr = bike_sharing['temp'].corr(bike_sharing['atemp'])
-    wind_hum_corr = bike_sharing['windspeed'].corr(bike_sharing['hum'])
-    print(temp_atemp_corr, wind_hum_corr, sep='\n')
-
-    plt.scatter(bike_sharing['temp'], bike_sharing['atemp'])
-    plt.xlabel('Air Temperature')
-    plt.ylabel('Feeling Temperature')
-    plt.show()
-
-    plt.scatter(bike_sharing['windspeed'], bike_sharing['hum'])
-    plt.xlabel('Wind Speed')
-    plt.ylabel('Humidity')
-    plt.show()
+    sentence_1 = True
+    sentence_2 = True
+    sentence_3 = False
 
 
 if __name__ == '__main__':
