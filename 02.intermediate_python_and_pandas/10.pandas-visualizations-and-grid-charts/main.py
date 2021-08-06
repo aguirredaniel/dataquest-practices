88fd4d340d3e17c66b7e5fbcb305c67fb92a4277
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-05 17:13:20 -0700

    3. Slowness in Traffic

--- 02.intermediate_python_and_pandas/10.pandas-visualizations-and-grid-charts/main.py
+++ 02.intermediate_python_and_pandas/10.pandas-visualizations-and-grid-charts/main.py
@@ -1,18 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-# - Import the pandas module as pd.
-# - Read in the traffic_sao_paulo.csv file using pd.read_csv().
-#   - The data points are separated by ;, so you'll need to use sep=';' to read in the file properly.
-#   - Assign the result to a variable named traffic.
-# - Perform a quick examination of the dataset.
-#   - Inspect the first and the last five rows.
-#   - Use DataFrame.info() to print summary information about the dataset. Do you see any missing values? Are all data
-#     types numerical?
+# - Import matplotlib.pyplot, and plot a histogram for the Slowness in traffic (%) column — run your code without
+#   submitting the answer.
+# - Based on the histogram, evaluate the truth value of the sentences below.
+#   - The distribution of Slowness in traffic (%) is right skewed. Assign the Boolean True or False to sentence_1.
+#   - Most of the values pile up on the left side of the histogram. Assign True or False to sentence_2.
+#   - Most of the values are over 15.0%. Assign True or False to sentence_3.
 def main():
     traffic = pd.read_csv('../traffic_sao_paulo.csv', sep=';')
-    print(traffic.head(), traffic.tail(), traffic.info(), sep='\n')
+    #  Cleaning and converting Slowness in traffic (%)' column to  float dtype.
+    traffic['Slowness in traffic (%)'] = traffic['Slowness in traffic (%)'].str.replace(',', '.').astype(float)
 
+    plt.hist(traffic['Slowness in traffic (%)'])
+    plt.show()
+
+    sentence_1 = True
+    sentence_2 = False
+    sentence_3 = True
 
 if __name__ == '__main__':
     main()
