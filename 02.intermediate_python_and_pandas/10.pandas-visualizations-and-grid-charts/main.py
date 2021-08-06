d06f1ed18d20ca9ea2a86cb3310dab5df26b636e
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-05 17:23:46 -0700

    5. Frequency of Incidents

--- 02.intermediate_python_and_pandas/10.pandas-visualizations-and-grid-charts/main.py
+++ 02.intermediate_python_and_pandas/10.pandas-visualizations-and-grid-charts/main.py
@@ -2,19 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Use the Series.plot.hist() method to generate a histogram for the Slowness in traffic (%) column.
-# - Add the title Distribution of Slowness in traffic (%) (the answer checking is case-sensitive).
-# - Add the x-label Slowness in traffic (%).
-# - Display the plot using plt.show().
+# - Generate a horizontal bar plot for the incidents.sum() table. Run your code without submitting the answer.
+# - Based on the resulting bar plot, evaluate the truth value of the following sentences:
+#   - Broken trucks are the most frequent cause of slow traffic. Assign the Boolean True or False to sentence_1.
+#   - Lack of electricity and flooding are less frequent than broken trucks. Assign True or False to sentence_2.
+#   - The most frequent incident type is broken trucks. Assign True or False to sentence_3.
 def main():
     traffic = pd.read_csv('../traffic_sao_paulo.csv', sep=';')
     #  Cleaning and converting 'Slowness in traffic (%)' column to  float dtype.
     traffic['Slowness in traffic (%)'] = traffic['Slowness in traffic (%)'].str.replace(',', '.').astype(float)
 
-    traffic['Slowness in traffic (%)'].plot.hist()
-    plt.title('Distribution of Slowness in traffic (%)')
-    plt.xlabel('Slowness in traffic (%)')
+    # Isolating the incident columns by dropping the columns 'Hour (Coded)' and 'Slowness in traffic (%)'
+    incidents = traffic.drop(['Hour (Coded)', 'Slowness in traffic (%)'], axis=1)
+
+    incidents.sum().plot.barh()
     plt.show()
+
+    sentence_1 = False
+    sentence_2 = True
+    sentence_3 = True
 
 
 if __name__ == '__main__':
