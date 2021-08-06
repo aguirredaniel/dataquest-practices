f1fa5c532033a691c2277fea662202cb3422c1ec
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-05 17:16:37 -0700

    4. Pandas Visualization Method

--- 02.intermediate_python_and_pandas/10.pandas-visualizations-and-grid-charts/main.py
+++ 02.intermediate_python_and_pandas/10.pandas-visualizations-and-grid-charts/main.py
@@ -2,23 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Import matplotlib.pyplot, and plot a histogram for the Slowness in traffic (%) column — run your code without
-#   submitting the answer.
-# - Based on the histogram, evaluate the truth value of the sentences below.
-#   - The distribution of Slowness in traffic (%) is right skewed. Assign the Boolean True or False to sentence_1.
-#   - Most of the values pile up on the left side of the histogram. Assign True or False to sentence_2.
-#   - Most of the values are over 15.0%. Assign True or False to sentence_3.
+# - Use the Series.plot.hist() method to generate a histogram for the Slowness in traffic (%) column.
+# - Add the title Distribution of Slowness in traffic (%) (the answer checking is case-sensitive).
+# - Add the x-label Slowness in traffic (%).
+# - Display the plot using plt.show().
 def main():
     traffic = pd.read_csv('../traffic_sao_paulo.csv', sep=';')
-    #  Cleaning and converting Slowness in traffic (%)' column to  float dtype.
+    #  Cleaning and converting 'Slowness in traffic (%)' column to  float dtype.
     traffic['Slowness in traffic (%)'] = traffic['Slowness in traffic (%)'].str.replace(',', '.').astype(float)
 
-    plt.hist(traffic['Slowness in traffic (%)'])
+    traffic['Slowness in traffic (%)'].plot.hist()
+    plt.title('Distribution of Slowness in traffic (%)')
+    plt.xlabel('Slowness in traffic (%)')
     plt.show()
 
-    sentence_1 = True
-    sentence_2 = False
-    sentence_3 = True
 
 if __name__ == '__main__':
     main()
