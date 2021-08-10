ab803cd74e128969548076762316349e687776b9
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-10 16:00:29 -0700

    6. Variable Representation: Spatial Separation

--- 02.intermediate_python_and_pandas/11.relational-plots-and-multiple-variables/main.py
+++ 02.intermediate_python_and_pandas/11.relational-plots-and-multiple-variables/main.py
@@ -3,12 +3,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-# - Add the Rooms column on the graph by using a shape representation. Leave the markers on default mode.
+# - Add the Year column by building two different plots based on the values in this column.
 # - Evaluate the truth value of the following sentences:
-#   - We can't find houses with seven rooms or more, and less than 2,000 square feet living area aboveground. Assign
-#     True or False to sentence_1.
-#   - Virtually all houses with seven rooms or more have an overall quality of seven or more. Assign True or False to
-#     sentence_2.
+#   - Most houses built in 2000 or later have a living area aboveground between 1,000 and 3,000 square feet and sell for
+#     more than 100,000 USD. Assign True or False to sentence_1.
+#   - Most of the houses with an overall quality rating of four or less were built in 1999 or earlier, have low garage
+#     area and six rooms or less. Assign True or False to sentence_2.
 def main():
     housing = pd.read_csv('../housing.csv')
 
@@ -16,11 +16,11 @@ def main():
     sns.relplot(data=housing, x='Gr Liv Area', y='SalePrice',
                 hue='Overall Qual', palette='RdYlGn',
                 size='Garage Area', sizes=(1, 300),
-                style='Rooms')
+                style='Rooms', col='Year')
     plt.show()
 
-    sentence_1 = False
-    sentence_2 = False
+    sentence_1 = True
+    sentence_2 = True
 
 
 if __name__ == '__main__':
