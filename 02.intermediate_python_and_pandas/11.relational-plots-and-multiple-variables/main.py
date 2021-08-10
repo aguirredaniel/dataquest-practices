606cbd7ffabb4e52cbf3e945973f10bcfa978645
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-10 15:56:13 -0700

    5. Variable Representation: Shape

--- 02.intermediate_python_and_pandas/11.relational-plots-and-multiple-variables/main.py
+++ 02.intermediate_python_and_pandas/11.relational-plots-and-multiple-variables/main.py
@@ -3,24 +3,24 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-# - Add the Garage Area variable on the graph by using a size representation. The size range should be between 1 and
-#   300.
+# - Add the Rooms column on the graph by using a shape representation. Leave the markers on default mode.
 # - Evaluate the truth value of the following sentences:
-#   - Houses that sell for more than 700,000 USD typically have garage areas greater than 1,000 square feet and living
-#     areas aboveground lower than 4,000 square feet. Assign True or False to sentence_1.
-#   - Houses with an overall quality of nine or ten typically have garage areas greater than 500 square feet, living
-#     areas aboveground greater than 2,000 feet, and sell for more than 400,000 USD. Assign True or False to sentence_2
+#   - We can't find houses with seven rooms or more, and less than 2,000 square feet living area aboveground. Assign
+#     True or False to sentence_1.
+#   - Virtually all houses with seven rooms or more have an overall quality of seven or more. Assign True or False to
+#     sentence_2.
 def main():
     housing = pd.read_csv('../housing.csv')
 
     sns.set_theme()
     sns.relplot(data=housing, x='Gr Liv Area', y='SalePrice',
                 hue='Overall Qual', palette='RdYlGn',
-                size='Garage Area', sizes=(1, 300))
+                size='Garage Area', sizes=(1, 300),
+                style='Rooms')
     plt.show()
 
     sentence_1 = False
-    sentence_2 = True
+    sentence_2 = False
 
 
 if __name__ == '__main__':
