b84a564d3e99135c371ecba66de3faf90abc1d83
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-17 18:39:02 -0700

    5. Mobile-Friendly Proportions

--- 02.intermediate_python_and_pandas/12.design_for_audience/main.py
+++ 02.intermediate_python_and_pandas/12.design_for_audience/main.py
@@ -2,11 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Use the Axes.barh() method to recreate the horizontal bar plot we previously generated.
-# - Call plt.show() to display the plot.
+# - Recreate the horizontal bar plot with a width of 4.5 inches and a height of 6 inches.
 def main():
     top20_deathtoll = pd.read_csv('../top20_deathtoll.csv')
-    figure, axes = plt.subplots()
+    figure, axes = plt.subplots(figsize=(4.5, 6))
     axes.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'])
     plt.show()
 
