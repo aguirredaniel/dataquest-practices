602d741225af564c2b413512f0db0ff6055bf314
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-17 19:45:07 -0700

    11. Final Touches

--- 02.intermediate_python_and_pandas/12.design_for_audience/main.py
+++ 02.intermediate_python_and_pandas/12.design_for_audience/main.py
@@ -2,15 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Add the title The Death Toll Worldwide Is 1.5M+. The title should have the following properties:
-#   - The x-coordinate is -80000
-#   - The y-coordinate is 23.5
-#  -  It is in bold text
-#   - It has a font size of 17
-# - Add the subtitle Top 20 countries by death toll (December 2020). The subtitle should have the following properties:
-#   - The x-coordinate is -80000
-#  -  The y-coordinate is 22.5
-#   - It has a font size of 12
+# - Change the x-tick labels to 0, 150,000, and 300,000.
+# - Left-align all y-tick labels. Each label must have the coordinates x=-80000 and y=i-0.15, where i is an integer in
+#   the range(0, 21).
+# - Add a vertical line with the following properties:
+#  - Its x-coordinate is 150000
+#  - Its ymin is 0.045
+#  - It has a gray color
+# - It has a transparency of 0.5
 def main():
     top20_deathtoll = pd.read_csv('../top20_deathtoll.csv')
     figure, axes = plt.subplots(figsize=(4.5, 6))
@@ -21,6 +20,12 @@ def main():
         axes.spines[spin].set_visible(False)
 
     axes.set_xticks([0, 150000, 300000])
+    axes.set_xticklabels(['0', '150,000', '300,000'])
+
+    axes.set_yticklabels([])
+
+    for i, country in zip(range(0, 21), top20_deathtoll['Country_Other']):
+        axes.text(x=-80000, y=i - 0.15, s=country)
 
     axes.xaxis.tick_top()
     axes.tick_params(axis='x', colors='grey')
@@ -28,7 +33,8 @@ def main():
 
     axes.text(x=-80000, y=23.5, s='The Death Toll Worldwide Is 1.5M+', size=17, weight='bold')
     axes.text(x=-80000, y=22.5, s='Top 20 countries by death toll (December 2020)', size=12)
-    
+
+    axes.axvline(x=150000, ymin=0.045, c='grey', alpha=0.5)
     plt.show()
 
 
