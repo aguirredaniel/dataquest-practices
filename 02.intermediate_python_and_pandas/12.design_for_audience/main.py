5f33ccb73b4b9dd12f079801d71d2d78ecfd484e
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-17 18:35:53 -0700

    4. The OO Interface

--- 02.intermediate_python_and_pandas/12.design_for_audience/main.py
+++ 02.intermediate_python_and_pandas/12.design_for_audience/main.py
@@ -2,11 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# Read in the top20_deathtoll.csv file into a pandas DataFrame. Assign the result to top20_deathtoll.
-# Using matplotlib, create a horizontal bar plot to display the top 20 countries by number of total deaths.
+# - Use the Axes.barh() method to recreate the horizontal bar plot we previously generated.
+# - Call plt.show() to display the plot.
 def main():
-    top20_deathtoll = pd.read_csv('top20_deathtoll.csv')
-    plt.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'])
+    top20_deathtoll = pd.read_csv('../top20_deathtoll.csv')
+    figure, axes = plt.subplots()
+    axes.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'])
     plt.show()
 
 
