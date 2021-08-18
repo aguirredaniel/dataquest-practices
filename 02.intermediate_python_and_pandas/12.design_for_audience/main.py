e8fcf24a569a36a11b833cb931f2e4f400e6fa74
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-17 19:16:27 -0700

    7. Erasing Non-Data Ink

--- 02.intermediate_python_and_pandas/12.design_for_audience/main.py
+++ 02.intermediate_python_and_pandas/12.design_for_audience/main.py
@@ -2,11 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Recreate the horizontal bar plot with a width of 4.5 inches and a height of 6 inches.
+# - Remove all four spines from the horizontal bar plot.
+# - Remove the bottom and left ticks from the horizontal bar plot.
 def main():
     top20_deathtoll = pd.read_csv('../top20_deathtoll.csv')
     figure, axes = plt.subplots(figsize=(4.5, 6))
     axes.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'])
+
+    for spin in ['left', 'bottom', 'right', 'top']:
+        axes.spines[spin].set_visible(False)
+
+    axes.tick_params(bottom=False, left=False)
     plt.show()
 
 
