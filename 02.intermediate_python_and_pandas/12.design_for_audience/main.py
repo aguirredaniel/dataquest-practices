634db4553aa2a487f28961712402ab064068a7b3
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-17 19:19:55 -0700

    8. Erasing Redundant Data-Ink

--- 02.intermediate_python_and_pandas/12.design_for_audience/main.py
+++ 02.intermediate_python_and_pandas/12.design_for_audience/main.py
@@ -2,17 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Remove all four spines from the horizontal bar plot.
-# - Remove the bottom and left ticks from the horizontal bar plot.
+# -Reduce the thickness of each bar to a value of 0.45.
+# - Keep only 0, 150000, and 300000 as x-tick labels.
 def main():
     top20_deathtoll = pd.read_csv('../top20_deathtoll.csv')
     figure, axes = plt.subplots(figsize=(4.5, 6))
-    axes.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'])
+    axes.barh(top20_deathtoll['Country_Other'], top20_deathtoll['Total_Deaths'],
+              height=0.45)
 
     for spin in ['left', 'bottom', 'right', 'top']:
         axes.spines[spin].set_visible(False)
 
     axes.tick_params(bottom=False, left=False)
+
+    axes.set_xticks([0, 150000, 300000])
+
     plt.show()
 
 
