c8abbbdf26a83b9c3cee1112e7b60f7cbfb42fcf
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-19 00:04:06 -0700

    3. Faster Workflow

--- 02.intermediate_python_and_pandas/13.storytelling_data_visualization/main.py
+++ 02.intermediate_python_and_pandas/13.storytelling_data_visualization/main.py
@@ -2,15 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Read in the covid_avg_deaths.csv file into a pandas DataFrame, and assign the result to a variable named death_toll.
-# - Create a grid chart of four rows by one column with a figure size of (6, 8).
-# - On each of the four plots, draw a line plot with the Month column on the x-axis and the New_deaths columns on the
-#   y-axis.
+# - Iterate over a list containing the four Axes objects. For each Axes object, do the following:
+# - Generate a line plot with the Month column on the x-axis and the New_deaths columns on the y-axis.
+# - Remove the x- and y-tick labels.
+# - Remove all ticks using the Axes.tick_params() method.
+# - Remove all the spines using the Axes.spines[location].set_visible() method.
 def main():
     death_toll = pd.read_csv('../covid_avg_deaths.csv')
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, figsize=(6, 8))
+
     for ax in [ax1, ax2, ax3, ax4]:
         ax.plot(death_toll['Month'], death_toll['New_deaths'])
+
+        # Maximizing data-ink ratio
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+
+        ax.tick_params(left=False, bottom=False)
+
+        for location in ['left', 'bottom', 'right', 'top']:
+            ax.spines[location].set_visible(False)
+        # =========================
     plt.show()
 
 
