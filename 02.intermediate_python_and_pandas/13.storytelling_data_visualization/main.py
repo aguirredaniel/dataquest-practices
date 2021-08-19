73f54f43408ca7ff9fb18116f158232c85daa902
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-19 01:17:47 -0700

    5. Adding Structural Elements

--- 02.intermediate_python_and_pandas/13.storytelling_data_visualization/main.py
+++ 02.intermediate_python_and_pandas/13.storytelling_data_visualization/main.py
@@ -6,11 +6,32 @@ import matplotlib.pyplot as plt
 # - Highlight the October–December period on the fourth plot. Use color='#af0b1e' and linewidth=2.5.
 def main():
     death_toll = pd.read_csv('../covid_avg_deaths.csv')
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, figsize=(6, 8))
-    axes = [ax1, ax2, ax3, ax4]
-    for ax in axes:
+    fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(6, 8))
+    periods = [(0, 3), (2, 6), (5, 10), (9, 12)]
+    texts_params = [(1.1, -300, 'Jan - Mar', 3),
+                    (3.7, 800, 'Mar - Jun', 0),
+                    (7.1, 500, 'Jun - Oct', 0),
+                    (10.5, 600, 'Oct - Dec', 45)]
+
+    # [y-axis] Showing  magnitude and the range of the quantities trends.
+    ax1 = axes[0]
+    ax1.text(0.5, -80, '0', alpha=0.5)
+    ax1.text(3.5, 2000, '1,844', alpha=0.5)
+    ax1.text(11.5, 2400, '2,247', alpha=0.5)
+
+    for ax, period, text_params in zip(axes, periods, texts_params):
         ax.plot(death_toll['Month'], death_toll['New_deaths'],
                 color='#af0b1e', alpha=0.1)
+
+        # Highlight periods
+        start, finish = period
+        ax.plot(death_toll['Month'][start: finish], death_toll['New_deaths'][start:finish],
+                color='#af0b1e', linewidth=2.5)
+
+        # [x-axis] Showing months periods labels
+        x, y, label, rotation = text_params
+        ax.text(x, y, label, color='#af0b1e',
+                weight='bold', rotation=rotation)
 
         # Maximizing data-ink ratio
         ax.set_xticklabels([])
@@ -20,15 +41,8 @@ def main():
 
         for location in ['left', 'bottom', 'right', 'top']:
             ax.spines[location].set_visible(False)
-        # =========================
 
-    # Highlight periods
-    for ax, periods in zip(axes, [(0, 3), (2, 6), (5, 10), (9, 12)]):
-        start, finish = periods
-        ax.plot(death_toll['Month'][start: finish], death_toll['New_deaths'][start:finish],
-                color='#af0b1e', linewidth=3.5)
-
-    plt.show()
+    plt.show()  
 
 
 if __name__ == '__main__':
