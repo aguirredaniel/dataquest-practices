c190ebf364cf1339cb5bb7fdc020691e16cd3834
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-20 18:26:42 -0700

    2. Proximity

---
+++ 02.intermediate_python_and_pandas/14.gestalt_principles_ pre_attentive_attributes/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Examine the FiveThirtyEight graph, and evaluate the truth value of the following sentences.
+# - The principle of proximity creates three main sections: the title and the subtitle section, the bar chart section,
+#   and the sources section. If you think this is true, assign the Boolean True to sentence_1; otherwise, assign False.
+# - The principle of proximity inside the bar chart creates two visually distinct sections: total mail (first section)
+#   and shipping and packaging (second section). If you think this is true, assign True to sentence_2; otherwise, assign
+#   False.
+# - The visualization is not a data story because there's no element of change. If you think this is true, assign True
+#   to sentence_3; otherwise, assign False.
+def main():
+    sentence_1 = True
+    sentence_2 = True
+    sentence_3 = False
+
+
+if __name__ == '__main__':
+    pass
