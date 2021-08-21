98a6c5d05afb166287d7ea85b5b217e8de1f8dc7
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-20 18:41:04 -0700

    4. Enclosure

--- 02.intermediate_python_and_pandas/14.gestalt_principles_ pre_attentive_attributes/main.py
+++ 02.intermediate_python_and_pandas/14.gestalt_principles_ pre_attentive_attributes/main.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 
 # Evaluate the truth value of the following sentences.
 #
-# - Based on similarity of shape, we see two different groups: people who rarely or never vote and people who sometimes
-#   vote. If you think this is true, assign the Boolean True to sentence_1; otherwise, assign False.
-# - Proximity divides the graph area into nine different bar charts. Assign True or False to sentence_2.
-# - This isn't a data story because it doesn't show a sequence of events, nor does it show any element of change. Assign
-#   True or False to sentence_3.
+# - Enclosure makes the COVID-19 period distinct. If you think this is true, assign the Boolean True to sentence_1;
+#   otherwise, assign False.
+# - Similarity of color on the bars visually separates them into two groups: negative values (yellow) and positive
+#   values (blue). Assign True or False to sentence_2.
+# - By making bar borders thicker, enclosure draws attention upon the April 2020 and June 2020 values. Assign True or
+#   False to sentence_3.
 def main():
-    sentence_1 = False
+    sentence_1 = True
     sentence_2 = True
     sentence_3 = True
 
