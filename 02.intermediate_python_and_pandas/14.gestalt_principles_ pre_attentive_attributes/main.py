086fbb4890c039b89baa94d5aae7d6b4f073ca0f
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-20 19:05:52 -0700

    6. Visual Hierarchy

--- 02.intermediate_python_and_pandas/14.gestalt_principles_ pre_attentive_attributes/main.py
+++ 02.intermediate_python_and_pandas/14.gestalt_principles_ pre_attentive_attributes/main.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 
 # Evaluate the truth value of the following sentences.
 #
-# - The principle of connection shows that the y-axis is the same for both bar plots. If you think this is true,
-#   assign the Boolean True to sentence_1; otherwise, assign False.
-# - Two curved arrows link two snippets of text to two nearby bars — this is an application of the principle of
-#    proximity. Assign True or False to sentence_2.
+# - Proximity is typically stronger than enclosure. Assign True or False to sentence_1.
+# - Similarity can be stronger than proximity in some cases, and vice-versa. Assign True or False to sentence_2.
+# - Similarity and enclosure are of equal strength. Assign True or False to sentence_3.
+# - People perceive visuals in a random way, without any principles governing their perception. Assign True or False to
+#   sentence_4.
 def main():
-    sentence_1 = True
-    sentence_2 = False
+    sentence_1 = False
+    sentence_2 = True
+    sentence_3 = False
+    sentence_4 = False
 
 
 if __name__ == '__main__':
