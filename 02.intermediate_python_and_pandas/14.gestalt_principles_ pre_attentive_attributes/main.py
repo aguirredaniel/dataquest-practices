1bb7f67ab71b48c5776116d1bf14462ca872d5d6
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-20 18:34:32 -0700

    3. Similarity

--- 02.intermediate_python_and_pandas/14.gestalt_principles_ pre_attentive_attributes/main.py
+++ 02.intermediate_python_and_pandas/14.gestalt_principles_ pre_attentive_attributes/main.py
@@ -2,18 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# Examine the FiveThirtyEight graph, and evaluate the truth value of the following sentences.
-# - The principle of proximity creates three main sections: the title and the subtitle section, the bar chart section,
-#   and the sources section. If you think this is true, assign the Boolean True to sentence_1; otherwise, assign False.
-# - The principle of proximity inside the bar chart creates two visually distinct sections: total mail (first section)
-#   and shipping and packaging (second section). If you think this is true, assign True to sentence_2; otherwise, assign
-#   False.
-# - The visualization is not a data story because there's no element of change. If you think this is true, assign True
-#   to sentence_3; otherwise, assign False.
+# Evaluate the truth value of the following sentences.
+#
+# - Based on similarity of shape, we see two different groups: people who rarely or never vote and people who sometimes
+#   vote. If you think this is true, assign the Boolean True to sentence_1; otherwise, assign False.
+# - Proximity divides the graph area into nine different bar charts. Assign True or False to sentence_2.
+# - This isn't a data story because it doesn't show a sequence of events, nor does it show any element of change. Assign
+#   True or False to sentence_3.
 def main():
-    sentence_1 = True
+    sentence_1 = False
     sentence_2 = True
-    sentence_3 = False
+    sentence_3 = True
 
 
 if __name__ == '__main__':
