9a799de8beeae6da406fb2827cae18742d6c3f6c
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-23 16:21:56 -0700

    5. Adding X-tick Labels

--- 02.intermediate_python_and_pandas/15.matplotlib_styles/main.py
+++ 02.intermediate_python_and_pandas/15.matplotlib_styles/main.py
@@ -2,19 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Using the Axes.axvline() method:
-# - Add a vertical line to the left of the labels column. The line should have the following properties:
-#   - The x-coordinate is 0.5.
-#  -  The color is grey.
-#  -  The alpha value is 0.1.
-#  -  The line width is 1.
-#   - The ymin is 0.1 and the ymax is 0.9.
-# - Add a vertical line to the right of the labels column. The line should have the following properties:
-#   - The x-coordinate is 1.45.
+# - Add a horizontal line above the red bar plot. The line should have the following properties:
+#   - The y-coordinate is 11.
 #   - The color is grey.
-#   - The alpha value is 0.1.
 #   - The line width is 1.
-#   - The ymin is 0.1 and the ymax is 0.9.
+#   - The alpha value is 0.5.
+#   - The xmin is 0.01, and the xmax is 0.32.
+# - Add the text RED WINE above the horizontal line you've just built. The text should have the following properties:
+#   - Its coordinates are -0.33 (x) and 11.2 (y).
+#   - It's bolded.
+#   - Add a horizontal line above the blue bar plot. The line should have the following properties:
+# - The y-coordinate is 11.
+#   - The color is grey.
+#   - The line width is 1.
+#   - The alpha value is 0.5.
+#   - The xmin is 0.67, and the xmax is 0.98.
+# - Add the text WHITE WINE above the horizontal line you've just built. The text should have the following
+#   properties:
+#   - Its coordinates are 1.75 (x) and 11.2 (y).
+#   - It's bolded.
 def main():
     red_wine = pd.read_csv('winequality-red.csv', sep=';')
     red_corr = red_wine.corr()['quality'][:-1]
@@ -45,10 +51,27 @@ def main():
         y_coord -= 1
 
     ax.axvline(x=0.5, color='grey', alpha=0.1,
-             linewidth=1, ymin=0.1, ymax=0.9)
-
+               linewidth=1, ymin=0.1, ymax=0.9)
     ax.axvline(x=1.45, color='grey', alpha=0.1,
-             linewidth=1, ymin=0.1, ymax=0.9)
+               linewidth=1, ymin=0.1, ymax=0.9)
+
+    ax.axhline(-1, color='grey', linewidth=1, alpha=0.5,
+               xmin=0.01, xmax=0.32)
+    ax.text(-0.7, -1.7, '-0.5' + ' ' * 31 + '+0.5',
+            color='grey', alpha=0.5)
+
+    ax.axhline(-1, color='grey', linewidth=1, alpha=0.5,
+               xmin=0.67, xmax=0.98)
+    ax.text(1.43, -1.7, '-0.5' + ' ' * 31 + '+0.5',
+            color='grey', alpha=0.5)
+
+    ax.axhline(11, color='grey', linewidth=1,
+               alpha=0.5, xmin=0.01, xmax=0.32)
+    ax.text(-0.33, 11.2, 'RED WINE', fontweight='bold')
+
+    ax.axhline(11, color='grey', linewidth=1,
+               alpha=0.5, xmin=0.67, xmax=0.98)
+    ax.text(1.75, 11.2, 'WHITE WINE', fontweight='bold')
 
     plt.show()
 
