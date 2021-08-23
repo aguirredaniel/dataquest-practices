819368a7a7691753c2a5067f95a3b93ec153e7a5
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-23 15:09:03 -0700

    2. Wine Quality Dataset

--- 02.intermediate_python_and_pandas/15.matplotlib_styles/main.py
+++ 02.intermediate_python_and_pandas/15.matplotlib_styles/main.py
@@ -2,23 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Create a line plot using the ggplot style.
-#   - The x-coordinates are [2, 4, 6].
-#   - The y-coordinates are [10, 15, 5].
-#   - Close and display the plot using plt.show().
-# - Create a line plot using the default style.
-#   - The coordinates are the same as above.
-#   - Close and display the plot using plt.show().
+# - Read in the winequality-white.csv file into a pandas DataFrame.
+#   - Use sep=';' to properly read the file.
+#   - Assign the result to white_wine.
+# - Calculate the correlation between quality and the other columns of white_wine.
+#   - The result should be a pandas Series with the same structure as the Series resulted from
+#     red_wine.corr()['quality'][:-1].
+#   - Assign the correlation values to white_corr.
+# - Examine the correlation values — what are the strongest two correlations? How does this compare to the red wine
+#   values?
 def main():
-    plt.style.use('ggplot')
-    plt.plot([2, 4, 6],
-             [10, 15, 5])
-    plt.show()
+    red_wine = pd.read_csv('winequality-red.csv', sep=';')
+    red_corr = red_wine.corr()['quality'][:-1]
 
-    plt.style.use('default')
-    plt.plot([2, 4, 6],
-             [10, 15, 5])
-    plt.show()
+    white_wine = pd.read_csv('winequality-white.csv', sep=';')
+    white_corr = white_wine.corr()['quality'][:-1]
+    print(red_corr, white_corr, sep='\n\n')
 
 
 if __name__ == '__main__':
