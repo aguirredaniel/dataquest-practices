09ac35fed03ec63d6c67f3d051e372289e58c540
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-08-23 16:26:32 -0700

    6. Adding a Signature

--- 02.intermediate_python_and_pandas/15.matplotlib_styles/main.py
+++ 02.intermediate_python_and_pandas/15.matplotlib_styles/main.py
@@ -2,25 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# - Add a horizontal line above the red bar plot. The line should have the following properties:
-#   - The y-coordinate is 11.
-#   - The color is grey.
-#   - The line width is 1.
-#   - The alpha value is 0.5.
-#   - The xmin is 0.01, and the xmax is 0.32.
-# - Add the text RED WINE above the horizontal line you've just built. The text should have the following properties:
-#   - Its coordinates are -0.33 (x) and 11.2 (y).
-#   - It's bolded.
-#   - Add a horizontal line above the blue bar plot. The line should have the following properties:
-# - The y-coordinate is 11.
-#   - The color is grey.
-#   - The line width is 1.
-#   - The alpha value is 0.5.
-#   - The xmin is 0.67, and the xmax is 0.98.
-# - Add the text WHITE WINE above the horizontal line you've just built. The text should have the following
+# - Add the title Wine Quality Most Strongly Correlated With Alcohol Level. The title should have the following
 #   properties:
-#   - Its coordinates are 1.75 (x) and 11.2 (y).
+#   - Its coordinates are -0.7 (x) and 13.5 (y).
+#   - The size is 17.
 #   - It's bolded.
+# - Add the subtitle Correlation values between wine quality and wine properties (alcohol, pH, etc.). Its coordinates
+#  are -0.7 (x) and 12.7 (y). Leave the font size on default.
 def main():
     red_wine = pd.read_csv('winequality-red.csv', sep=';')
     red_corr = red_wine.corr()['quality'][:-1]
@@ -73,6 +61,15 @@ def main():
                alpha=0.5, xmin=0.67, xmax=0.98)
     ax.text(1.75, 11.2, 'WHITE WINE', fontweight='bold')
 
+    ax.text(-0.7, -2.9,
+            '©DATAQUEST' + ' ' * 94 + 'Source: P. Cortez et al.',
+            color='#f0f0f0', backgroundcolor='#4d4d4d',
+            size=12)
+
+    ax.text(-0.7, 13.5, 'Wine Quality Most Strongly Correlated With Alcohol Level',
+            fontsize=15, fontweight='bold')
+    ax.text(-0.7, 12.7,
+            'Correlation values between wine quality and wine properties (alcohol, pH, etc.)')
     plt.show()
 
 
