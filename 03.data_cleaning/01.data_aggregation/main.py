78db7674d99bd17f30b7ed7d1e70099fb95951b3
Daniel Aguirre <aguirredaniel@garage>
2021-09-09 14:44:58 -0700

    6. Exploring GroupBy Objects

--- 03.data_cleaning/01.data_aggregation/main.py
+++ 03.data_cleaning/01.data_aggregation/main.py
@@ -1,14 +1,22 @@
 import pandas as pd
 
 
-# - Use the df.groupby() method to group happiness2015 by the Region column. Assign the result to grouped.
-# - Use the GroupBy.get_group() method to select the data for the Australia and New Zealand group only. Assign the
-#   result to aus_nz.
+# * For the following exercise, use the result from the dictionary returned by grouped.groups shown below:
+#   'North America': Int64Index([4, 14], dtype='int64'
+# - Prove that the values for the "North America" group in the dictionary returned by grouped.groups above correspond to
+#   countries in North America in the happiness2015 DataFrame.
+#   - Use the snippet above to identify the indices of the countries in happiness2015 that belong to the North America
+#     group.
+#   - Use the indices to assign just the countries in North America in happiness2015 to north_america.
+# - Use the GroupBy.get_group() method to select the data for the North America group only. Assign the result to
+#   na_group.
+# - Use the following code to compare north_america and na_group: north_america == na_group. Assign the result to equal.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     grouped = happiness2015.groupby('Region')
-    aus_nz = grouped.get_group('Australia and New Zealand')
-    print(aus_nz)
+    north_america = happiness2015.iloc[[4, 14]]
+    na_group = grouped.get_group('North America')
+    equal = north_america == na_group
 
 
 if __name__ == '__main__':
