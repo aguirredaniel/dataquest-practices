966fc525636414e487e0eb3928c07b29b25482fe
Daniel Aguirre <aguirredaniel@garage>
2021-09-09 15:27:57 -0700

    10. Computing Multiple and Custom Aggregations with the Agg() Method

--- 03.data_cleaning/01.data_aggregation/main.py
+++ 03.data_cleaning/01.data_aggregation/main.py
@@ -6,20 +6,16 @@ def dif(group):
     return (group.max() - group.mean())
 
 
-# - Apply the GroupBy.agg() method to happy_grouped. Pass a list containing np.mean and np.max into the method. Assign
-#   the result to happy_mean_max.
-# - As noted above, passing 'mean' and 'max' into the GroupBy.agg() method will also return the same results. However,
-#   for answer-checking purposes, you'll have to use np.mean and np.max.
-# - We've also created a custom function named dif to calculate the difference between the mean and maximum values. Pass
-#   dif into the GroupBy.agg() method. Assign the result to mean_max_dif.
+# - Inspect the following code: happiness_means = happiness2015.groupby('Region')['Happiness Score'].mean(). Try to
+#   answer the questions below before running the code and moving on. There is no answer-checking on this screen.
+# - If we run the code above, will the index of happiness_means be the unique values in the Region column or the
+#   Happiness Score column?
+# - Will the values aggregated in happiness_means be the values from the Region column or the Happiness Score column?
+# - Print happiness_means. Were your predictions correct?
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
-    grouped = happiness2015.groupby('Region')
-    happy_grouped = grouped['Happiness Score']
-    happy_mean_max = happy_grouped.agg([np.mean, np.max])
-    mean_max_dif = happy_grouped.agg(dif)
-
-    print(happy_mean_max, mean_max_dif, sep='\n')
+    happiness_means = happiness2015.groupby('Region')['Happiness Score'].mean()
+    print(happiness_means)
 
 
 if __name__ == '__main__':
