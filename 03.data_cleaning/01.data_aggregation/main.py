4d522183330188dbb11c5450c1fbf12db2cb2320
Daniel Aguirre <aguirredaniel@garage>
2021-09-09 15:46:59 -0700

    12. Aggregating Multiple Columns and Functions with Pivot Table

--- 03.data_cleaning/01.data_aggregation/main.py
+++ 03.data_cleaning/01.data_aggregation/main.py
@@ -7,20 +7,26 @@ def dif(group):
     return (group.max() - group.mean())
 
 
-# - Plot the resulting DataFrame, pv_happiness, using the df.plot() method. Set kind to barh, xlim to (0,10), title to
-#   'Mean Happiness Scores by Region', and legend to False. What do you notice about these results?
-# - Calculate the mean of the Happiness Score column in the original happiness 2015 data set. Assign the result to
-#   world_mean_happiness.
-#   - Does world_mean_happiness equal the value for the All group? If you can't figure out the answer, don't worry!
-#     We'll review this question on the next screen.
+# - Use the df.groupby() method to calculate the minimum, maximum, and mean family and happiness scores for each region
+#   in happiness2015.
+#   - Group happiness2015 by the Region column.
+#   - Select the Happiness Score and Family columns. Assign the result to grouped.
+#   - Apply the GroupBy.agg() method to grouped. Pass a list containing np.min, np.max, and np.mean into the method.
+#   - Assign the result to happy_family_stats.
+# - Use the pivot_table method to return the same information, but also calculate the minimum, maximum, and mean for the
+#   entire Family and Happiness Score columns.
+#   - The aggregation columns should be Happiness Score and Family.
+#   - The column to group by is Region.
+#   - The aggregation functions are np.min, np.max, and np.mean.
+#   - Set the margins parameter equal to True.
+#   - Assign the result to pv_happy_family_stats.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
-    pv_happiness = happiness2015.pivot_table(values='Happiness Score', index='Region', aggfunc=np.mean, margins=True)
-    pv_happiness.plot(kind='barh', xlim=(0, 10), title='Mean Happiness Scores by Region')
-    plt.show()
+    happy_family_stats = happiness2015.groupby('Region')[['Happiness Score', 'Family']].agg([np.min, np.max, np.mean])
+    pv_happy_family_stats = happiness2015.pivot_table(
+        values=['Family', 'Happiness Score'], index='Region', aggfunc=[np.min, np.max, np.mean], margins=True)
 
-    world_mean_happiness = happiness2015['Happiness Score'].mean()
-    print(world_mean_happiness)
+    print(happy_family_stats, pv_happy_family_stats, sep='\n')
 
 
 if __name__ == '__main__':
