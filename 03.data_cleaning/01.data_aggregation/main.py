e2c8b47d67c100e6e81ee5eecb3bff053170f9fc
Daniel Aguirre <aguirredaniel@garage>
2021-09-09 14:38:54 -0700

    5. Creating GroupBy Objects

--- 03.data_cleaning/01.data_aggregation/main.py
+++ 03.data_cleaning/01.data_aggregation/main.py
@@ -1,23 +1,14 @@
 import pandas as pd
 
 
-# - Create an empty dictionary named mean_happiness to store the results of this exercise.
-# - Use the Series.unique() method to create an array of unique values for the Region column.
-# - Use a for loop to iterate over the unique region values from the Region column.
-# - Assign the rows belonging to the current region to a variable named region_group.
-# - Use the Series.mean() method to calculate the mean happiness score for region_group.
-# - Assign the mean value to the mean_happiness dictionary, using the region name as the key and the mean happiness
-#   score as the value.
-
+# - Use the df.groupby() method to group happiness2015 by the Region column. Assign the result to grouped.
+# - Use the GroupBy.get_group() method to select the data for the Australia and New Zealand group only. Assign the
+#   result to aus_nz.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
-
-    mean_happiness = {}
-    regions = happiness2015['Region'].unique()
-    for region in regions:
-        region_group = happiness2015[happiness2015['Region'] == region]
-        mean_happiness[region] = region_group['Happiness Score'].mean()
-    print(mean_happiness)
+    grouped = happiness2015.groupby('Region')
+    aus_nz = grouped.get_group('Australia and New Zealand')
+    print(aus_nz)
 
 
 if __name__ == '__main__':
