0a59d488f225414529660a02bb25e80358c5c82b
Daniel Aguirre <aguirredaniel@garage>
2021-09-09 15:34:41 -0700

    11. Aggregation with Pivot Tables

--- 03.data_cleaning/01.data_aggregation/main.py
+++ 03.data_cleaning/01.data_aggregation/main.py
@@ -1,21 +1,26 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def dif(group):
     return (group.max() - group.mean())
 
 
-# - Inspect the following code: happiness_means = happiness2015.groupby('Region')['Happiness Score'].mean(). Try to
-#   answer the questions below before running the code and moving on. There is no answer-checking on this screen.
-# - If we run the code above, will the index of happiness_means be the unique values in the Region column or the
-#   Happiness Score column?
-# - Will the values aggregated in happiness_means be the values from the Region column or the Happiness Score column?
-# - Print happiness_means. Were your predictions correct?
+# - Plot the resulting DataFrame, pv_happiness, using the df.plot() method. Set kind to barh, xlim to (0,10), title to
+#   'Mean Happiness Scores by Region', and legend to False. What do you notice about these results?
+# - Calculate the mean of the Happiness Score column in the original happiness 2015 data set. Assign the result to
+#   world_mean_happiness.
+#   - Does world_mean_happiness equal the value for the All group? If you can't figure out the answer, don't worry!
+#     We'll review this question on the next screen.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
-    happiness_means = happiness2015.groupby('Region')['Happiness Score'].mean()
-    print(happiness_means)
+    pv_happiness = happiness2015.pivot_table(values='Happiness Score', index='Region', aggfunc=np.mean, margins=True)
+    pv_happiness.plot(kind='barh', xlim=(0, 10), title='Mean Happiness Scores by Region')
+    plt.show()
+
+    world_mean_happiness = happiness2015['Happiness Score'].mean()
+    print(world_mean_happiness)
 
 
 if __name__ == '__main__':
