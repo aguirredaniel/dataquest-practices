547b570215f38270dcafa759290a7f4d679e5025
Daniel Aguirre <aguirredaniel@garage>
2021-09-17 17:46:13 -0700

    2. Combining Dataframes with the Concat Function

--- 03.data_cleaning/02.combining-data-using-pandas/main.py
+++ 03.data_cleaning/02.combining-data-using-pandas/main.py
@@ -1,18 +1,30 @@
 import pandas as pd
 
 
-# - Use the pandas.read_csv() function to read the World_Happiness_2016.csv file into a dataframe called happiness2016
-#   and the World_Happiness_2017.csv file into a dataframe called happiness2017.
-# - Add a column called Year to each dataframe with the corresponding year. For example, the Year column in
-#   happiness2015 should contain the value 2015 for each row.
+# - Use the pd.concat() function to combine head_2015 and head_2016 along axis = 0. Remember to pass the head_2015 and
+#   head_2016 into the function as a list. Assign the result to concat_axis0.
+# - Use the pd.concat() function to combine head_2015 and head_2016 along axis = 1. Remember to pass head_2015 and
+#   head_2016 into the function as a list and set the axis parameter equal to 1. Assign the result to concat_axis1.
+# - Use the variable inspector to view concat_axis0 and concat_axis1.
+# - Assign the number of rows in concat_axis0 to a variable called question1.
+# - Assign the number of rows in concat_axis1 to a variable called question2.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     happiness2016 = pd.read_csv('World_Happiness_2016.csv')
     happiness2017 = pd.read_csv('World_Happiness_2017.csv')
 
+    # Adding corresponding 'Year' column
     happiness2015['Year'] = 2015
     happiness2016['Year'] = 2016
     happiness2017['Year'] = 2017
+
+    head_2015 = happiness2015[['Country', 'Happiness Score', 'Year']].head(3)
+    head_2016 = happiness2016[['Country', 'Happiness Score', 'Year']].head(3)
+
+    concat_axis0 = pd.concat([head_2015, head_2016])
+    concat_axis1 = pd.concat([head_2015, head_2016], axis=1)
+    question1 = 6
+    question2 = 3
 
 
 if __name__ == '__main__':
