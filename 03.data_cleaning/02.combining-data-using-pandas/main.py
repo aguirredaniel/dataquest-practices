0998e1f833b1561a78fc16b7ecfd7245ce49d4ee
Daniel Aguirre <aguirredaniel@garage>
2021-09-17 18:12:03 -0700

    6. Joining on Columns with the Merge Function

--- 03.data_cleaning/02.combining-data-using-pandas/main.py
+++ 03.data_cleaning/02.combining-data-using-pandas/main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 
-# - Use the pd.merge() function to join three_2015 and three_2016 on the Country column. Assign the result to merged.
+# - Update merged to use a left join instead of an inner join. Set the how parameter to 'left' in merge(). Assign the
+#   result to merged_left.
+# - Update merged_left so that the left parameter equals three_2016 and the right parameter equals three_2015. Assign
+#   the result to merged_left_updated.
+# - Based on the results of this exercise, when using a left join, does changing the dataframe assigned to the left and
+#   right parameters change the result? Try to answer this question before moving onto the next screen.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     happiness2016 = pd.read_csv('World_Happiness_2016.csv')
@@ -16,6 +21,8 @@ def main():
     three_2016 = happiness2016[['Country', 'Happiness Rank', 'Year']].iloc[2:5]
 
     merged = pd.merge(left=three_2015, right=three_2016, on='Country')
+    merged_left = pd.merge(left=three_2015, right=three_2016, on='Country', how='left')
+    merged_left_updated = pd.merge(left=three_2016, right=three_2015, on='Country', how='left')
 
 
 if __name__ == '__main__':
