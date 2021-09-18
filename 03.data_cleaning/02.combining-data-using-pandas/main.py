becb113b15a30bc2da84e68b1e4c0a65421de976
Daniel Aguirre <aguirredaniel@garage>
2021-09-17 18:22:13 -0700

    8. Join on Index with the Merge Function

--- 03.data_cleaning/02.combining-data-using-pandas/main.py
+++ 03.data_cleaning/02.combining-data-using-pandas/main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 
-# - Update merged to use the suffixes _2015 and _2016. Set the suffixes parameter to ('_2015', '_2016') in merge().
-#   Assign the result to merged_suffixes.
-# - Update merged_updated to use the suffixes _2015 and _2016. Notice that the "left" dataframe is three_2016 and the
-#   "right" dataframe is three_2015. Assign the result to merged_updated_suffixes.
+# - Predict the number of rows and columns the resulting dataframe will have. Assign the number of rows to a variable
+#   called rows and the number of columns to a variable called columns.
+# - To change the join type used in merge_index to a left join, set the how parameter equal to 'left'. Save the result
+#   to merge_index_left.
+# - Update rows and columns so that each contains the correct number of rows and columns in merge_index_left.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     happiness2016 = pd.read_csv('World_Happiness_2016.csv')
@@ -15,18 +16,16 @@ def main():
     happiness2016['Year'] = 2016
     happiness2017['Year'] = 2017
 
-    three_2015 = happiness2015[['Country', 'Happiness Rank', 'Year']].iloc[2:5]
+    four_2015 = happiness2015[['Country', 'Happiness Rank', 'Year']].iloc[2:6]
     three_2016 = happiness2016[['Country', 'Happiness Rank', 'Year']].iloc[2:5]
+    merge_index = pd.merge(left=four_2015, right=three_2016, left_index=True, right_index=True,
+                           suffixes=('_2015', '_2016'))
 
-    merged = pd.merge(left=three_2015, right=three_2016, how='left', on='Country')
-    merged_updated = pd.merge(left=three_2016, right=three_2015, how='left', on='Country')
+    merge_index_left = pd.merge(left=four_2015, right=three_2016, how='left', left_index=True, right_index=True,
+                                suffixes=('_2015', '_2016'))
+    rows = 4
+    columns = 6
 
-    merged_suffixes = pd.merge(left=three_2015, right=three_2016,
-                               how='left', on='Country',
-                               suffixes=('_2015', '_2016'))
-    merged_updated_suffixes = pd.merge(left=three_2016, right=three_2015,
-                                       how='left', on='Country',
-                                       suffixes=('_2016', '_2015'))
 
 if __name__ == '__main__':
     main()
