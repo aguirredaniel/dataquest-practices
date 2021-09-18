71b694847b67be229c98ef46f45dd78dd8a1e078
Daniel Aguirre <aguirredaniel@garage>
2021-09-17 18:16:46 -0700

    7. Left Joins with the Merge Function

--- 03.data_cleaning/02.combining-data-using-pandas/main.py
+++ 03.data_cleaning/02.combining-data-using-pandas/main.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
 
-# - Update merged to use a left join instead of an inner join. Set the how parameter to 'left' in merge(). Assign the
-#   result to merged_left.
-# - Update merged_left so that the left parameter equals three_2016 and the right parameter equals three_2015. Assign
-#   the result to merged_left_updated.
-# - Based on the results of this exercise, when using a left join, does changing the dataframe assigned to the left and
-#   right parameters change the result? Try to answer this question before moving onto the next screen.
+# - Update merged to use the suffixes _2015 and _2016. Set the suffixes parameter to ('_2015', '_2016') in merge().
+#   Assign the result to merged_suffixes.
+# - Update merged_updated to use the suffixes _2015 and _2016. Notice that the "left" dataframe is three_2016 and the
+#   "right" dataframe is three_2015. Assign the result to merged_updated_suffixes.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     happiness2016 = pd.read_csv('World_Happiness_2016.csv')
@@ -20,10 +18,15 @@ def main():
     three_2015 = happiness2015[['Country', 'Happiness Rank', 'Year']].iloc[2:5]
     three_2016 = happiness2016[['Country', 'Happiness Rank', 'Year']].iloc[2:5]
 
-    merged = pd.merge(left=three_2015, right=three_2016, on='Country')
-    merged_left = pd.merge(left=three_2015, right=three_2016, on='Country', how='left')
-    merged_left_updated = pd.merge(left=three_2016, right=three_2015, on='Country', how='left')
+    merged = pd.merge(left=three_2015, right=three_2016, how='left', on='Country')
+    merged_updated = pd.merge(left=three_2016, right=three_2015, how='left', on='Country')
 
+    merged_suffixes = pd.merge(left=three_2015, right=three_2016,
+                               how='left', on='Country',
+                               suffixes=('_2015', '_2016'))
+    merged_updated_suffixes = pd.merge(left=three_2016, right=three_2015,
+                                       how='left', on='Country',
+                                       suffixes=('_2016', '_2015'))
 
 if __name__ == '__main__':
     main()
