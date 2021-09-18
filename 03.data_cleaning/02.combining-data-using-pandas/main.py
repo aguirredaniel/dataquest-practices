ac700fd9121762932ad8fb4a794f4f8eb14bb9a9
Daniel Aguirre <aguirredaniel@garage>
2021-09-17 17:51:09 -0700

    4. Combining Dataframes with Different Shapes Using the Concat Function

--- 03.data_cleaning/02.combining-data-using-pandas/main.py
+++ 03.data_cleaning/02.combining-data-using-pandas/main.py
@@ -1,13 +1,9 @@
 import pandas as pd
 
 
-# - Use the pd.concat() function to combine head_2015 and head_2016 along axis = 0. Remember to pass the head_2015 and
-#   head_2016 into the function as a list. Assign the result to concat_axis0.
-# - Use the pd.concat() function to combine head_2015 and head_2016 along axis = 1. Remember to pass head_2015 and
-#   head_2016 into the function as a list and set the axis parameter equal to 1. Assign the result to concat_axis1.
-# - Use the variable inspector to view concat_axis0 and concat_axis1.
-# - Assign the number of rows in concat_axis0 to a variable called question1.
-# - Assign the number of rows in concat_axis1 to a variable called question2.
+# - Use the pd.concat() function to combine head_2015 and head_2016 along axis = 0 again. This time, however, set the
+#   ignore_index parameter to True to reset the index in the result. Assign the result to concat_update_index.
+#   - Use the variable inspector to view the results and confirm the index was reset.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     happiness2016 = pd.read_csv('World_Happiness_2016.csv')
@@ -21,10 +17,7 @@ def main():
     head_2015 = happiness2015[['Year', 'Country', 'Happiness Score', 'Standard Error']].head(4)
     head_2016 = happiness2016[['Country', 'Happiness Score', 'Year']].head(3)
 
-    concat_axis0 = pd.concat([head_2015, head_2016])
-    concat_axis1 = pd.concat([head_2015, head_2016], axis=1)
-    rows = 7
-    columns = 4
+    concat_update_index = pd.concat([head_2015, head_2016], ignore_index=True)
 
 
 if __name__ == '__main__':
