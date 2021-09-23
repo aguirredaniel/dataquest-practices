b4f8471b1ffeeb09c13d4c556382c6ce25346c72
Daniel Aguirre <aguirredaniel@garage>
2021-09-22 20:01:19 -0700

    10. Extracting More Than One Group of Patterns from a Series

--- 03.data_cleaning/04.working-with-strings-in-pandas/main.py
+++ 03.data_cleaning/04.working-with-strings-in-pandas/main.py
@@ -19,9 +19,11 @@ def extract_last_word(element):
     return element_str.split()[-1]
 
 
-# - Use the Series.str.extractall() method to extract all of the years in the IESurvey. Assign the result to years.
-# - Use the Series.value_counts() method to create a list of the unique years, along with the count. Assign the result
-#   to value_counts. Print value_counts.
+# - Use the Series.str.extractall() method to extract pattern from the IESurvey column. Assign the result to years.
+# - Use vectorized slicing to extract the first two numbers from the First_Year column in years (For example, extract
+#   "20" from "2000"). Assign the result to first_two_year.
+# - Add first_two_year to the Second_Year column in years, so that Second_Year contains the full year (ex: "2000").
+#   Assign the result to years['Second_Year'].
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     world_dev = pd.read_csv('World_dev.csv')
@@ -31,10 +33,12 @@ def main():
     merged.rename(col_renaming, axis=1, inplace=True)
 
     merged = merged.set_index('Country')
-    pattern = r"(?P<Years>[1-2][0-9]{3})"
+    pattern = r"(?P<First_Year>[1-2][0-9]{3})/?(?P<Second_Year>[0-9]{2})?"
     years = merged['IESurvey'].str.extractall(pattern)
-    value_counts = years['Years'].value_counts()
-    print(value_counts)
+    first_two_year = years['First_Year'].str[:2]
+    years['Second_Year'] = first_two_year.str.cat(years['Second_Year'])
+
+    print(years.sort_values('Second_Year').head())
 
 
 if __name__ == '__main__':
