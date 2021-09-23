5286fa353981baccbc4a56112b983d0d86273dfb
Daniel Aguirre <aguirredaniel@garage>
2021-09-22 19:18:55 -0700

    6. Finding Specific Words in Strings Continued

--- 03.data_cleaning/04.working-with-strings-in-pandas/main.py
+++ 03.data_cleaning/04.working-with-strings-in-pandas/main.py
@@ -19,9 +19,11 @@ def extract_last_word(element):
     return element_str.split()[-1]
 
 
-# - Use the Series.str.contains() method to search for pattern in the SpecialNotes column. Assign the result to
-#   national_accounts.
-# - Use the Series.head() method to print the first five rows in national_accounts.
+# - Use the Series.str.contains() method to search for pattern in the SpecialNotes column again. This time, also pass in
+#   the na parameter and set it to False. Assign the result to national_accounts.
+# - Use national_accounts to index merged, so that only rows that contain "national accounts" or "National accounts" in
+#   the SpecialNotes column are returned. Assign the result to merged_national_accounts.
+# - Use the DataFrame.head() method to print the first five rows in merged_national_accounts.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     world_dev = pd.read_csv('World_dev.csv')
@@ -31,8 +33,9 @@ def main():
     merged.rename(col_renaming, axis=1, inplace=True)
 
     pattern = r"[Nn]ational accounts"
-    national_accounts = merged['SpecialNotes'].str.contains(pattern)
-    print(national_accounts.head())
+    national_accounts = merged['SpecialNotes'].str.contains(pattern, na=False)
+    merged_national_accounts = merged[national_accounts]
+    print(merged_national_accounts.head())
 
 
 if __name__ == '__main__':
