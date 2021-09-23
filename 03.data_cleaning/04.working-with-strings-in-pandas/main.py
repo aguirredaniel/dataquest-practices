dac190d831d6eb0625673b81a8cf7d6f7678c516
Daniel Aguirre <aguirredaniel@garage>
2021-09-22 19:51:54 -0700

    9. Extracting All Matches of a Pattern from a Series

--- 03.data_cleaning/04.working-with-strings-in-pandas/main.py
+++ 03.data_cleaning/04.working-with-strings-in-pandas/main.py
@@ -19,10 +19,9 @@ def extract_last_word(element):
     return element_str.split()[-1]
 
 
-# - Create a regular expression that will match years and assign it to the variable pattern. Note: we've already set up
-#   the pattern variable. Insert your answer inside the parantheses: "(your_answer)".
-# - Use pattern and the Series.str.extract() method to extract years from the SpecialNotes column. Assign the resulting
-#   Series to years.
+# - Use the Series.str.extractall() method to extract all of the years in the IESurvey. Assign the result to years.
+# - Use the Series.value_counts() method to create a list of the unique years, along with the count. Assign the result
+#   to value_counts. Print value_counts.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     world_dev = pd.read_csv('World_dev.csv')
@@ -31,9 +30,11 @@ def main():
     col_renaming = {'SourceOfMostRecentIncomeAndExpenditureData': 'IESurvey'}
     merged.rename(col_renaming, axis=1, inplace=True)
 
-    pattern = r"([1-2][0-9][0-9][0-9])"
-    years = merged['SpecialNotes'].str.extract(pattern)
-    print(years.head())
+    merged = merged.set_index('Country')
+    pattern = r"(?P<Years>[1-2][0-9]{3})"
+    years = merged['IESurvey'].str.extractall(pattern)
+    value_counts = years['Years'].value_counts()
+    print(value_counts)
 
 
 if __name__ == '__main__':
