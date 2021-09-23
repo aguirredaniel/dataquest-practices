6551a0a37db41e89ae1baf6c37d9f1d9e99fbaec
Daniel Aguirre <aguirredaniel@garage>
2021-09-22 19:24:43 -0700

    7. Extracting Substrings from a Series

--- 03.data_cleaning/04.working-with-strings-in-pandas/main.py
+++ 03.data_cleaning/04.working-with-strings-in-pandas/main.py
@@ -19,11 +19,10 @@ def extract_last_word(element):
     return element_str.split()[-1]
 
 
-# - Use the Series.str.contains() method to search for pattern in the SpecialNotes column again. This time, also pass in
-#   the na parameter and set it to False. Assign the result to national_accounts.
-# - Use national_accounts to index merged, so that only rows that contain "national accounts" or "National accounts" in
-#   the SpecialNotes column are returned. Assign the result to merged_national_accounts.
-# - Use the DataFrame.head() method to print the first five rows in merged_national_accounts.
+# - Create a regular expression that will match years and assign it to the variable pattern. Note: we've already set up
+#   the pattern variable. Insert your answer inside the parantheses: "(your_answer)".
+# - Use pattern and the Series.str.extract() method to extract years from the SpecialNotes column. Assign the resulting
+#   Series to years.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     world_dev = pd.read_csv('World_dev.csv')
@@ -32,10 +31,9 @@ def main():
     col_renaming = {'SourceOfMostRecentIncomeAndExpenditureData': 'IESurvey'}
     merged.rename(col_renaming, axis=1, inplace=True)
 
-    pattern = r"[Nn]ational accounts"
-    national_accounts = merged['SpecialNotes'].str.contains(pattern, na=False)
-    merged_national_accounts = merged[national_accounts]
-    print(merged_national_accounts.head())
+    pattern = r"([1-2][0-9][0-9][0-9])"
+    years = merged['SpecialNotes'].str.extract(pattern)
+    print(years.head())
 
 
 if __name__ == '__main__':
