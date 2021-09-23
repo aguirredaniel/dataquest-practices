30c6505b01f6b9dba4dd981ffcc399ac146778aa
Daniel Aguirre <aguirredaniel@garage>
2021-09-22 19:08:23 -0700

    4. Exploring Missing Values with Vectorized String Methods

--- 03.data_cleaning/04.working-with-strings-in-pandas/main.py
+++ 03.data_cleaning/04.working-with-strings-in-pandas/main.py
@@ -19,9 +19,11 @@ def extract_last_word(element):
     return element_str.split()[-1]
 
 
-# - Use the Series.str.split() method to split the CurrencyUnit column into a list of words and then use the
-#   Series.str.get() method to select just the last word. Assign the result to merged['Currency Vectorized'].
-# - Use the Series.head() method to print the first five rows in merged['Currency Vectorized'].
+# - Use the Series.str.len() method to return the length of each element in the CurrencyUnit column. Assign the result
+#   to lengths.
+# - Use the Series.value_counts() method to return the count of unique values in lengths. Set the dropna parameter to
+#   False so NaNs are counted, too. Assign the result to value_counts.
+#   - If value_counts contains NaNs, it means the Series.str.len() method excluded them and didn't treat them as strings.
 def main():
     happiness2015 = pd.read_csv('World_Happiness_2015.csv')
     world_dev = pd.read_csv('World_dev.csv')
@@ -30,8 +32,9 @@ def main():
     col_renaming = {'SourceOfMostRecentIncomeAndExpenditureData': 'IESurvey'}
     merged.rename(col_renaming, axis=1, inplace=True)
 
-    merged['Currency Vectorized'] = merged['CurrencyUnit'].str.split().str.get(-1)
-    print(merged['Currency Vectorized'].head())
+    lengths = merged['CurrencyUnit'].str.len()
+    value_counts = lengths.value_counts(dropna=False)
+    print(lengths, value_counts, sep='\n')
 
 
 if __name__ == '__main__':
