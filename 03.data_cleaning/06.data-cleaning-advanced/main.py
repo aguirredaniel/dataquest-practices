6fb6ba352d5d2145e23d614cc23c3137dcd2a116
Daniel Aguirre <aguirredaniel@garage>
2021-10-05 16:16:57 -0700

    3. Counting Matches with pandas Methods

--- 03.data_cleaning/06.data-cleaning-advanced/main.py
+++ 03.data_cleaning/06.data-cleaning-advanced/main.py
@@ -2,21 +2,17 @@ import pandas as pd
 import re
 
 
-# - Initialize a variable python_mentions with the integer value 0.
-# - Create a string — pattern — containing a regular expression pattern that uses a set to match Python or python.
-# - Use a loop to iterate over each item in the titles list, and for each item:
-#   - Use the re.search() function to check whether pattern matches the title.
-#   - If re.search() returns a match object, increment (add 1 to) the python_mentions variable.
+# - Assign the title column from the hn dataframe to the variable titles.
+# - Use Series.str.contains() and Series.sum() with the provided regex pattern to count how many Hacker News titles
+#   contain Python or python. Assign the result to python_mentions.
 def main():
     hn = pd.read_csv('hacker_news.csv')
-    titles = hn["title"].tolist()
+    titles = hn["title"]
 
-    python_mentions = 0
     pattern = '[Pp]ython'
+    python_mentions = titles.str.contains(pattern).sum()
 
-    for title in titles:
-        if re.search(pattern, title):
-            python_mentions += 1
+    print(python_mentions)
 
 
 if __name__ == '__main__':
