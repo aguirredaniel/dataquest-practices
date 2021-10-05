6879c84eaa9fec26b03d06f0c8de87cacfb71e4b
Daniel Aguirre <aguirredaniel@garage>
2021-10-05 16:13:08 -0700

    2. The Regular Expression Module

--- 03.data_cleaning/06.data-cleaning-advanced/main.py
+++ 03.data_cleaning/06.data-cleaning-advanced/main.py
@@ -1,9 +1,22 @@
 import pandas as pd
+import re
 
 
+# - Initialize a variable python_mentions with the integer value 0.
+# - Create a string — pattern — containing a regular expression pattern that uses a set to match Python or python.
+# - Use a loop to iterate over each item in the titles list, and for each item:
+#   - Use the re.search() function to check whether pattern matches the title.
+#   - If re.search() returns a match object, increment (add 1 to) the python_mentions variable.
 def main():
     hn = pd.read_csv('hacker_news.csv')
-    print(hn.info(), hn.head(), sep='\n')
+    titles = hn["title"].tolist()
+
+    python_mentions = 0
+    pattern = '[Pp]ython'
+
+    for title in titles:
+        if re.search(pattern, title):
+            python_mentions += 1
 
 
 if __name__ == '__main__':
