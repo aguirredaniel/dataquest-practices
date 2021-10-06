a719499b2b76d336f0f109fa2892c7e051158932
Daniel Aguirre <aguirredaniel@garage>
2021-10-05 17:13:22 -0700

    9. Word Boundaries

--- 03.data_cleaning/06.data-cleaning-advanced/main.py
+++ 03.data_cleaning/06.data-cleaning-advanced/main.py
@@ -15,13 +15,13 @@ def first_10_matches(titles: pd.Series, pattern: str):
 # - Write a regular expression that will match titles containing Java.
 #   - You might like to use the first_10_matches() function or a site like RegExr to build your regular expression.
 #   - The regex should match whether or not the first character is capitalized.
-#   - The regex shouldn't match where 'Java' is followed by the letter 'S' or 's'.
+#   - The regex should match only where 'Java' is preceded and followed by a word boundary.
 # - Select every row from titles that match the regular expression. Assign the result to java_titles.
 def main():
     hn = pd.read_csv('hacker_news.csv')
     titles = hn["title"]
 
-    pattern = r'[Jj]ava[^Ss]'
+    pattern = r'\b[Jj]ava\b'
     java_titles = titles[titles.str.contains(pattern)]
     print(java_titles)
 
