be1d5a234204b9367966903ecb2c39f40ec09e08
Daniel Aguirre <aguirredaniel@garage>
2021-10-05 17:03:46 -0700

    8. Negative Character Classes

--- 03.data_cleaning/06.data-cleaning-advanced/main.py
+++ 03.data_cleaning/06.data-cleaning-advanced/main.py
@@ -2,16 +2,28 @@ import pandas as pd
 import re
 
 
-# - Use Series.str.extract() and Series.value_counts() with the modified regex pattern to produce a frequency table of
-#  all the tags in the titles series. Assign the frequency table to tag_freq.
+def first_10_matches(titles: pd.Series, pattern: str):
+    """
+    Return the first 10 story titles that match
+    the provided regular expression
+    """
+    all_matches = titles[titles.str.contains(pattern)]
+    first_10 = all_matches.head(10)
+    return first_10
+
+
+# - Write a regular expression that will match titles containing Java.
+#   - You might like to use the first_10_matches() function or a site like RegExr to build your regular expression.
+#   - The regex should match whether or not the first character is capitalized.
+#   - The regex shouldn't match where 'Java' is followed by the letter 'S' or 's'.
+# - Select every row from titles that match the regular expression. Assign the result to java_titles.
 def main():
     hn = pd.read_csv('hacker_news.csv')
     titles = hn["title"]
 
-    pattern = r'\[(\w+)\]'
-    tag_freq = titles.str.extract(pattern, expand=False).value_counts()
-
-    print(tag_freq)
+    pattern = r'[Jj]ava[^Ss]'
+    java_titles = titles[titles.str.contains(pattern)]
+    print(java_titles)
 
 
 if __name__ == '__main__':
