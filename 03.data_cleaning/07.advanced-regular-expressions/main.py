bec15b80a906a7d0e3f22d2d09b355aab665f353
Daniel Aguirre <aguirredaniel@garage>
2021-10-08 00:34:18 -0700

    10. Using Named Capture Groups to Extract Data

--- 03.data_cleaning/07.advanced-regular-expressions/main.py
+++ 03.data_cleaning/07.advanced-regular-expressions/main.py
@@ -13,33 +13,17 @@ def first_10_matches(titles: pd.Series, pattern: str):
     return first_10
 
 
-# - Write a regular expression that extracts URL components using three capture groups:
-#   - The first capture group should include the protocol text, up to but not including ://.
-#   - The second group should contain the domain, from after :// up to but not including /.
-#   - The third group should contain the page path, from after / to the end of the string.
-# - Use the regular expression pattern to extract the URL components from the test_urls series. Assign the results to
-#   test_url_parts.
-# - Use the regular expression pattern to extract the URL components from the url column of the hn dataframe. Assign the
-#   results to url_parts.
+# - Uncomment the regular expression pattern. Add names to each capture group:
+#   - The first capture group should be called protocol.
+#   - The second capture group should be called domain.
+#  - The third capture group should be called path.
+# - Use the regular expression pattern to extract three named columns of url components from the url column of the hn
+#   dataframe. Assign the result to url_parts
 def main():
     hn = pd.read_csv('hacker_news.csv')
-    test_urls = pd.Series([
-        'https://www.amazon.com/Technology-Ventures-Enterprise-Thomas-Byers/dp/0073523429',
-        'http://www.interactivedynamicvideo.com/',
-        'http://www.nytimes.com/2007/11/07/movies/07stein.html?_r=0',
-        'http://evonomics.com/advertising-cannot-maintain-internet-heres-solution/',
-        'HTTPS://github.com/keppel/pinn',
-        'Http://phys.org/news/2015-09-scale-solar-youve.html',
-        'https://iot.seeed.cc',
-        'http://www.bfilipek.com/2016/04/custom-deleters-for-c-smart-pointers.html',
-        'http://beta.crowdfireapp.com/?beta=agnipath',
-        'https://www.valid.ly?param',
-        'http://css-cursor.techstream.org'
-    ])
-
-    pattern = r'(https?):\/{2}([\w\.-]+)/?(.*)'
-    test_url_parts = test_urls.str.extract(pattern, expand=False, flags=re.I)
+    pattern = r"(?P<protocol>https?)://(?P<domain>[\w\.\-]+)/?(?P<path>.*)"
     url_parts = hn['url'].str.extract(pattern, expand=False, flags=re.I)
+    print(url_parts)
 
 
 if __name__ == '__main__':
