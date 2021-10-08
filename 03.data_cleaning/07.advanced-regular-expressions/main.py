4633047937bcdee0b620e4f3f509893195889f03
Daniel Aguirre <aguirredaniel@garage>
2021-10-07 23:44:32 -0700

    9. Extracting URL Parts Using Multiple Capture Groups

--- 03.data_cleaning/07.advanced-regular-expressions/main.py
+++ 03.data_cleaning/07.advanced-regular-expressions/main.py
@@ -13,17 +13,14 @@ def first_10_matches(titles: pd.Series, pattern: str):
     return first_10
 
 
-# - Write a regular expression to extract the domains from test_urls and assign the result to test_urls_clean. We
-#   suggest the following technique:
-#    - Using a series of characters that will match the protocol.
-#   - Inside a capture group, using a set that will match the character classes used in the domain.
-#   - Because all of the URLs either end with the domain, or continue with page path which starts with / (a character
-#     not found in any domains), we don't need to cater for this part of the URL in our regular expression.
-# - Use a regular expression to extract the domains from the url column of the hn dataframe. Assign the result to
-#   domains.
-#   - Note that passing the cases in test_urls does not guarantee passing all the cases in the url column.
-# - Use Series.value_counts() to build a frequency table of the domains in domains, limiting the frequency table to just
-#   to the top 5. Assign the result to top_domains.
+# - Write a regular expression that extracts URL components using three capture groups:
+#   - The first capture group should include the protocol text, up to but not including ://.
+#   - The second group should contain the domain, from after :// up to but not including /.
+#   - The third group should contain the page path, from after / to the end of the string.
+# - Use the regular expression pattern to extract the URL components from the test_urls series. Assign the results to
+#   test_url_parts.
+# - Use the regular expression pattern to extract the URL components from the url column of the hn dataframe. Assign the
+#   results to url_parts.
 def main():
     hn = pd.read_csv('hacker_news.csv')
     test_urls = pd.Series([
@@ -40,13 +37,9 @@ def main():
         'http://css-cursor.techstream.org'
     ])
 
-    pattern = 'https?:\/{2}([w{3}\.]?[\w\.-]+)'
-    test_urls_clean = test_urls.str.extract(pattern, expand=False, flags=re.I)
-    domains = hn['url'].str.extract(pattern, expand=False, flags=re.I)
-
-    top_domains = domains.value_counts().head()
-
-    print(test_urls_clean, top_domains, sep='\n')
+    pattern = r'(https?):\/{2}([\w\.-]+)/?(.*)'
+    test_url_parts = test_urls.str.extract(pattern, expand=False, flags=re.I)
+    url_parts = hn['url'].str.extract(pattern, expand=False, flags=re.I)
 
 
 if __name__ == '__main__':
