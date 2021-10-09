4a989297e6b592ecc7f3bb0cbfeb4d183f38348b
Daniel Aguirre <aguirredaniel@garage>
2021-10-08 17:37:46 -0700

    2. Reading a JSON file

--- 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
+++ 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
@@ -4,26 +4,11 @@ import json
 # - Import the json module.
 # - Use json.loads() to convert world_cup_str to a Python object. Assign the result to world_cup_obj
 def main():
-    world_cup_str = """
-    [
-        {
-            "team_1": "France",
-            "team_2": "Croatia",
-            "game_type": "Final",
-            "score" : [4, 2]
-        },
-        {
-            "team_1": "Belgium",
-            "team_2": "England",
-            "game_type": "3rd/4th Playoff",
-            "score" : [2, 0]
-        }
-    ]
-    """
+    with open('hn_2014.json') as file:
+        hn = json.load(file)
 
-    world_cup_obj = json.loads(world_cup_str)
-
-    print(world_cup_obj)
+        print(len(hn))
+        print(hn[0].keys())
 
 
 if __name__ == '__main__':
