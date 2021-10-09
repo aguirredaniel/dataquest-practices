99a57ae72d913c0e4f4facb45531013b93590c8c
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-08 19:19:56 -0700

    11. Exploring Tags Using the Apply Function

--- 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
+++ 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 
 def del_key(dict_, key):
@@ -23,22 +24,18 @@ def get_num_comments(hn_2014) -> int:
     return hn_2014['numComments']
 
 
-# - Using sorted() and a lambda function, sort the hn_clean JSON list by the number of points (dictionary key points)
-#   from highest to lowest:
-#   - Check the documentation for sorted() to see how to reverse the order to highest to lowest.
-#   - Assign the result to hn_sorted_points.
-# - Use a list comprehension to return a list of the five post titles (dictionary key title) that have the most points
-#   in our data set:
-#   - Assign the result to top_5_titles.
+# - Use Series.apply() and len() to create a boolean mask based on whether each item in tags has a length of 4.
+# - Use the boolean mask to filter tags. Assign the result to four_tags.
 def main():
     with open('hn_2014.json') as file:
         hn = json.load(file)
         hn_clean = [del_key(story, 'createdAtI') for story in hn]
+        hn_df = pd.DataFrame.from_dict(hn_clean)
 
-        hn_sorted_points = sorted(hn_clean, key=lambda story: story['points'], reverse=True)
-        top_5_titles = [story['title'] for story in hn_sorted_points[:5]]
+        tags = hn_df['tags']
+        four_tags = tags[tags.apply(len) == 4]
 
-        print(top_5_titles)
+        print(four_tags)
 
 
 if __name__ == '__main__':
