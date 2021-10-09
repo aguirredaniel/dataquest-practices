c4812e6d0be6cd7bcdd30cbc7a8d3c72df087df8
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-08 18:25:11 -0700

    6. Using List Comprehensions to Reduce a List

--- 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
+++ 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
@@ -10,13 +10,19 @@ def del_key(dict_, key):
     return modified_dict
 
 
-# - Use a list comprehension to extract the url value from each dictionary in hn_clean. Assign the result to urls
+# - Use list comprehension to create a new list, thousand_points:
+#   - The list should contain values from hn_clean where the points key has a value greater than 1000.
+# - Count the number of values in thousand_points and assign the result to num_thousand_points.
 def main():
     with open('hn_2014.json') as file:
         hn = json.load(file)
         hn_clean = [del_key(story, 'createdAtI') for story in hn]
 
         urls = [story['url'] for story in hn_clean]
+
+        thousand_points = [story for story in hn_clean if story['points'] > 1000]
+        num_thousand_points = len(thousand_points)
+        print(num_thousand_points)
 
 
 if __name__ == '__main__':
