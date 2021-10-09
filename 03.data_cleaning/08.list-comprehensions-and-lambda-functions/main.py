c0c58da3e3a03f53cfe9ab9b04edb7c23fb40a52
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-08 17:49:52 -0700

    3. Deleting Dictionary Keys

--- 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
+++ 03.data_cleaning/08.list-comprehensions-and-lambda-functions/main.py
@@ -1,14 +1,23 @@
 import json
 
 
-# - Use the open() function to open the hn_2014.json file as a file object.
-# - Use the json.load() function to parse the file object and assign the result to hn.
+def del_key(dict_, key):
+    """
+    Return a copy of our dictionary with the key removed
+    """
+    modified_dict = dict_.copy()
+    del modified_dict[key]
+    return modified_dict
+
+
+# - Create an empty list, hn_clean to store the cleaned data set.
+# - Loop over the dictionaries in the hn list. In each iteration:
+#   - Use the del_key() function to delete the createdAtI key from the dictionary.
+#   - Append the cleaned dictionary to hn_clean.
 def main():
     with open('hn_2014.json') as file:
         hn = json.load(file)
-
-        print(len(hn))
-        print(hn[0].keys())
+        hn_clean = [del_key(story, 'createdAtI') for story in hn]
 
 
 if __name__ == '__main__':
