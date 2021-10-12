a19121578e6f4e3bf2af682717045d60c919685b
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-12 16:50:22 -0700

    2. Verifying the Total Columns

--- 03.data_cleaning/09.working-with-missing-data/main.py
+++ 03.data_cleaning/09.working-with-missing-data/main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
 
-# - Create a series that counts the number of null values in each of the columns in the mvc dataframe. Assign the result
-#   to null_counts.
+# - Select the first three columns from killed and sum each row. Assign the result to killed_manual_sum.
+# - Create a boolean mask that checks whether each value in killed_manual_sum is not equal to the values in
+#   the total_killed column. Assign the boolean mask to killed_mask.
+# - Use killed_mask to filter the rows in killed. Assign the result to killed_non_eq.
 def main():
     mvc = pd.read_csv('nypd_mvc_2018.csv')
-    null_counts = mvc.isnull().sum()
 
-    print(null_counts)
+    killed_cols = [col for col in mvc.columns if 'killed' in col]
+    killed = mvc[killed_cols].copy()
+    killed_manual_sum = killed.iloc[:, :3].sum(axis=1)
+    killed_mask = killed['total_killed'] != killed_manual_sum
+    killed_non_eq = killed[killed_mask]
+    print(killed_non_eq)
 
 
 if __name__ == '__main__':
