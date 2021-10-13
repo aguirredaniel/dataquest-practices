4d5517c457514e2e1a1f54fa348843f3d676f529
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-12 17:05:02 -0700

    3. Filling and Verifying the Killed and Injured Data

--- 03.data_cleaning/09.working-with-missing-data/main.py
+++ 03.data_cleaning/09.working-with-missing-data/main.py
@@ -1,19 +1,34 @@
+import numpy as np
 import pandas as pd
 
 
-# - Select the first three columns from killed and sum each row. Assign the result to killed_manual_sum.
-# - Create a boolean mask that checks whether each value in killed_manual_sum is not equal to the values in
-#   the total_killed column. Assign the boolean mask to killed_mask.
-# - Use killed_mask to filter the rows in killed. Assign the result to killed_non_eq.
+# - Use Series.mask() to replace any null values from the total_injured column with their equivalents from the
+#   injured_manual_sum series.
+# - Use Series.mask() to replace any numbers from total_injured that aren't equal to their equivalents in
+#   injured_manual_sum with np.nan.
 def main():
     mvc = pd.read_csv('nypd_mvc_2018.csv')
 
     killed_cols = [col for col in mvc.columns if 'killed' in col]
     killed = mvc[killed_cols].copy()
     killed_manual_sum = killed.iloc[:, :3].sum(axis=1)
-    killed_mask = killed['total_killed'] != killed_manual_sum
-    killed_non_eq = killed[killed_mask]
-    print(killed_non_eq)
+
+    killed_null_mask = killed['total_killed'].isnull()
+    killed['total_killed'] = killed['total_killed'].mask(killed_null_mask, killed_manual_sum)
+
+    killed_no_equal_mask = killed['total_killed'] != killed_manual_sum
+    killed['total_killed'] = killed['total_killed'].mask(killed_no_equal_mask, np.nan)
+
+    # Create an injured dataframe and manually sum values
+    injured_columns = [col for col in mvc.columns if 'injured' in col]
+    injured = mvc[injured_columns].copy()
+    injured_manual_sum = injured.iloc[:, :3].sum(axis=1)
+
+    injured_null_mask = injured['total_injured'].isnull()
+    injured['total_injured'] = injured['total_injured'].mask(injured_null_mask, injured_manual_sum)
+
+    injured_no_equal_mask = injured['total_injured'] != injured_manual_sum
+    injured['total_injured'] = injured['total_injured'].mask(injured_no_equal_mask, np.nan)
 
 
 if __name__ == '__main__':
