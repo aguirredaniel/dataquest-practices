aa88a749af11f98c119fc6e9cb47c38b7830228d
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-12 17:33:10 -0700

    5. Visualizing Missing Data with Plots

--- 03.data_cleaning/09.working-with-missing-data/main.py
+++ 03.data_cleaning/09.working-with-missing-data/main.py
@@ -1,5 +1,52 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_null_correlations(df):
+    # create a correlation matrix only for columns with at least
+    # one missing value
+    cols_with_missing_values = df.columns[df.isnull().sum() > 0]
+    missing_corr = df[cols_with_missing_values].isnull().corr()
+
+    # create a mask to avoid repeated values and make
+    # the plot easier to read
+    missing_corr = missing_corr.iloc[1:, :-1]
+    # see triu: https://numpy.org/doc/stable/reference/generated/numpy.triu.html
+    # >>> np.triu([[1,2,3],[4,5,6],[7,8,9],[10,11,12]], -1)
+    # array([[ 1,  2,  3],
+    #        [ 4,  5,  6],
+    #        [ 0,  8,  9],
+    #        [ 0,  0, 12]])
+    # see ones_like: https://numpy.org/doc/stable/reference/generated/numpy.ones_like.html
+    # >>> x = np.arange(6)
+    # >>> x = x.reshape((2, 3))
+    # >>> x
+    # array([[0, 1, 2],
+    #        [3, 4, 5]])
+    # >>>> np.ones_like(x)
+    # array([[1, 1, 1],
+    #        [1, 1, 1]])
+    mask = np.triu(np.ones_like(missing_corr), k=1)
+
+    # plot a heatmap of the values
+    plt.figure(figsize=(20, 14))
+    ax = sns.heatmap(missing_corr, vmin=-1, vmax=1, cbar=False,
+                     cmap='RdBu', mask=mask, annot=True)
+
+    # format the text in the plot to make it easier to read
+    for text in ax.texts:
+        t = float(text.get_text())
+        if -0.05 < t < 0.01:
+            text.set_text('')
+        else:
+            text.set_text(round(t, 2))
+        text.set_fontsize('x-large')
+    plt.xticks(rotation=90, size='x-large')
+    plt.yticks(rotation=0, size='x-large')
+
+    plt.show()
 
 
 # - Assign the total_injured column from the injured dataframe to the same column in the mvc dataframe.
@@ -30,6 +77,9 @@ def main():
 
     mvc['total_killed'] = killed['total_killed']
     mvc['total_injured'] = injured['total_injured']
+
+    vehicle = mvc[[col for col in mvc.columns if 'vehicle' in col]]
+    plot_null_correlations(vehicle)
 
 
 if __name__ == '__main__':
