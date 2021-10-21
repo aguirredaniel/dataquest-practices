9fb87f1eb337995293aff669aff3955a11f14c73
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-20 19:28:42 -0700

    10. Converting AP Test Scores

--- challenge_03_data_cleaning/11-data-cleaning-walkthroug-combining-the-data/main.py
+++ challenge_03_data_cleaning/11-data-cleaning-walkthroug-combining-the-data/main.py
@@ -1,10 +1,15 @@
+import pandas
+
 from challenge_03_data_cleaning.nyc_high_school_data import read_data, make_initial_clean
 import numpy as np
 
 
-# - Filter graduation, only select rows where the Cohort column equals 2006.
-# - Filter graduation, only select rows where the Demographic column equals Total Cohort.
-# - Display the first few rows of data["graduation"] to verify that everything worked properly.
+# - Convert each of the following columns in ap_2010 to numeric values using the pandas.to_numeric() function with the
+#   keyword argument errors="coerce".
+#   - AP Test Takers
+#   - Total Exams Taken
+#   - Number of Exams with scores 3 4 or 5
+# - Display the column types using the dtypes attribute.
 def main():
     data = read_data()
     data = make_initial_clean(data)
@@ -26,6 +31,13 @@ def main():
     graduation = data['graduation']
     graduation = graduation[(graduation['Cohort'] == '2006') & (graduation['Demographic'] == 'Total Cohort')]
     data['graduation'] = graduation
+
+    cols = ['AP Test Takers ', 'Total Exams Taken', 'Number of Exams with scores 3 4 or 5']
+
+    for col in cols:
+        data['ap_2010'][col] = data['ap_2010'][col].apply(lambda score: pandas.to_numeric(score, errors="coerce"))
+
+    print(data['ap_2010'][cols].dtypes)
 
 
 if __name__ == '__main__':
