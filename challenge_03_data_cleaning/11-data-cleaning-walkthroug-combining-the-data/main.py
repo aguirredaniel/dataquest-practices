25901052e42ef6f91baa061978751962d4f5bb71
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-20 20:42:03 -0700

    12. Performing the Left Joins

--- challenge_03_data_cleaning/11-data-cleaning-walkthroug-combining-the-data/main.py
+++ challenge_03_data_cleaning/11-data-cleaning-walkthroug-combining-the-data/main.py
@@ -1,15 +1,15 @@
-import pandas
-
+import pandas as pd
 from challenge_03_data_cleaning.nyc_high_school_data import read_data, make_initial_clean
-import numpy as np
 
 
-# - Convert each of the following columns in ap_2010 to numeric values using the pandas.to_numeric() function with the
-#   keyword argument errors="coerce".
-#   - AP Test Takers
-#   - Total Exams Taken
-#   - Number of Exams with scores 3 4 or 5
-# - Display the column types using the dtypes attribute.
+# - Use the pandas pandas.DataFrame.merge() method to merge the ap_2010 dataset into combined.
+#   - Make sure to specify how="left" as a keyword argument to indicate the correct join type.
+#   - Make sure to assign the result of the merge operation back to combined.
+# - Use the pandas df.merge() method to merge the graduation dataset into combined.
+#   - Make sure to specify how="left" as a keyword argument to get the correct join type.
+#   -Make sure to assign the result of the merge operation back to combined.
+# - Display the first few rows of combined to verify that the correct operations occurred.
+# - Use the pandas.DataFrame.shape attribute to display the shape of the dataframe and see how many rows now exist.
 def main():
     data = read_data()
     data = make_initial_clean(data)
@@ -35,9 +35,15 @@ def main():
     cols = ['AP Test Takers ', 'Total Exams Taken', 'Number of Exams with scores 3 4 or 5']
 
     for col in cols:
-        data['ap_2010'][col] = data['ap_2010'][col].apply(lambda score: pandas.to_numeric(score, errors="coerce"))
+        data['ap_2010'][col] = pd.to_numeric(data['ap_2010'][col], errors="coerce")
 
-    print(data['ap_2010'][cols].dtypes)
+    combined = data["sat_results"]
+
+    combined = pd.merge(combined, data['ap_2010'], how='left')
+    combined = pd.merge(combined, data['graduation'], how='left')
+
+    print(combined, combined.shape, sep='\n')
+    print(combined.shape)
 
 
 if __name__ == '__main__':
