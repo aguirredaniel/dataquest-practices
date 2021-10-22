173815ad5cd36573fb3affddd24f68c4b9e041e1
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-10-21 18:48:45 -0700

    5. Plotting Enrollment With the Plot() Accessor

--- challenge_03_data_cleaning/12.data-cleaning-walkthrough-analyzing-and-visualizing-the-data/main.py
+++ challenge_03_data_cleaning/12.data-cleaning-walkthrough-analyzing-and-visualizing-the-data/main.py
@@ -1,17 +1,18 @@
 from challenge_03_data_cleaning.nyc_high_school_data import read_data, make_initial_clean, merge_data
 
+import matplotlib.pyplot as plt
 
-# - Use the pandas.DataFrame.corr() method on the combined dataframe to find all possible correlations. Assign the
-#   result to correlations.
-# - Filter correlations so that it only shows correlations for the column sat_score.
-# - Display all of the rows in correlations and examine them.
+
+# - Create a scatterplot of total_enrollment (on the horizontal axis) versus sat_score (on the vertical axis).
 def main():
     data = read_data()
     data = make_initial_clean(data)
     combined = merge_data(data)
 
     correlations = combined.corr()['sat_score']
-    print(correlations)
+
+    combined.plot(kind='scatter', x='total_enrollment', y='sat_score')
+    plt.show()
 
 
 if __name__ == '__main__':
