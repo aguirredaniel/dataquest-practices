15c58b953e1af03b452bd1c217ed9a624fce97ee
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-04 22:20:16 -0700

    3. Populations and Samples

---
+++ challenge_06_probability_statistics/01.sampling/main.py
@@ -0,0 +1,21 @@
+# Identify which is the case, and assign to the corresponding variable the string 'population' or 'sample'.
+# Here are the questions we need to answer:
+# - What's the average salary of the individuals in our company working in IT roles? (Assign either 'population' or
+#   'sample' to the variable question1.)
+# - What's the proportion of individuals in the IT department having salaries under $60000? (Assign either 'population'
+#   or 'sample' to the variable question2.)
+# - What's the minimum salary in the entire company? (Assign either 'population' or 'sample' to the variable question3.)
+# - What's the minimum salary in the IT department of our company? (Assign either 'population' or 'sample' to the
+#   variable question4.)
+# - What's the proportion of salaries under $20000 in the entire company? (Assign either 'population' or 'sample' to
+#   the variable question5.)
+def main():
+    question1 = 'population'
+    question2 = 'population'
+    question3 = 'sample'
+    question4 = 'population'
+    question5 = 'sample'
+
+
+if __name__ == '__main__':
+    main()
