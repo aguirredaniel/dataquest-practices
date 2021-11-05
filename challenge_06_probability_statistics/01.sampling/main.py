73e6474277aa48e3e98d928622e3e519b141152b
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-04 23:42:26 -0700

    8. Proportional Stratified Sampling

--- challenge_06_probability_statistics/01.sampling/main.py
+++ challenge_06_probability_statistics/01.sampling/main.py
@@ -2,31 +2,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# Perform stratified sampling: stratify the dataset by player position, and then do simple random sampling on every
-# stratum. At the end, use the sample to determine which position scores the highest number of points per game.
-# - Create a new column that describes the number of points a player scored per game during the season. The number of
-#   total points a player scored during the entire season is in the PTS column, and the number of games played is in the
-#   Games Played column. Give the new column a relevant name.
-# - Stratify the wnba data set by player position. The Pos column describes a player's position. Assign each stratum to
-#   a different variable.
-# - Loop through the strata, and for each stratum, do the following:
-#    - Sample 10 observations using simple random sampling (set random_state = 0).
-#    - Find the mean points per game using the sample. Use the new column you created earlier.
-#    - Find a way to store the mean along with its corresponding position. You can use a dictionary.
-# - Find the position that scores the highest number of points per game, and assign its name to a variable named
-#   position_most_points.
-# To find the dictionary key that has the greatest dictionary value, you can use this technique.
+# Perform stratified sampling on the data set 100 times, and sample strata proportionally.
+# - Stratify the dataset by the number of games played in the following way: the first stratum should include players
+#   who played 12 games or fewer; the second stratum should players who played more than 12 games but up to 22
+#   (included); the third stratum should include players who played more than 22 games (22 not included).
+# - Perform stratified sampling 100 times. For each of the 100 iterations of a for loop, do the following:
+#   - Sample each stratum proportionally. Sample at random: one sample observation from the first stratum, two sample
+#      observations from the second, and seven sample observations from the third stratum (eventually, we'll concatenate
+#      these sample observations and calculate the mean of the PTS column).
+#   - random_state should vary from 0 to 99: 0 for the first iteration, 99 for the final iteration.
+#   - Once you're done with the sampling for the current iteration of the loop, concatenate all the sample observations
+#     into one final sample. You can use pd.concat().
+#   - Compute the mean of the final sample, and append it to a list defined outside the loop. The mean should be for the
+#     PTS column.
+# - Display the entire sampling process.
+#   - Using plt.scatter(), display the sampling means on a scatter plot. Place the means on the y-axis and the sample
+#     numbers on the x-axis (the numbers should range from 1 to 100 - both endpoints included).
+#   - Using plt.axhline(), display the population mean for the total points in the form of a horizontal line.
 def main():
     wnba = pd.read_csv('wnba.csv')
 
-    wnba['PTG'] = wnba['PTS'] / wnba['Games Played']
+    stratum1 = wnba[wnba['Games Played'] < 13]
+    stratum2 = wnba[(wnba['Games Played'] >= 13) & (wnba['Games Played'] <= 22)]
+    stratum3 = wnba[wnba['Games Played'] > 22]
 
-    strata = {pos: wnba[wnba['Pos'] == pos] for pos in wnba['Pos'].unique()}
+    samples = []
+    for i in range(100):
+        srs_1 = stratum1['PTS'].sample(n=1, random_state=i)
+        srs_2 = stratum2['PTS'].sample(n=2, random_state=i)
+        srs_3 = stratum3['PTS'].sample(n=7, random_state=i)
 
-    stratified_sampling = {pos: df['PTG'].sample(n=10, random_state=0).mean() for pos, df in strata.items()}
-    position_most_points = max(stratified_sampling, key=stratified_sampling.get)
+        sample = pd.concat([srs_1, srs_2, srs_3])
+        samples.append(sample.mean())
 
-    print(position_most_points)
+    parameter = wnba['PTS'].mean()
+
+    plt.scatter(x=range(1, 101), y=samples)
+    plt.axhline(parameter)
+    plt.show()
 
 
 if __name__ == '__main__':
