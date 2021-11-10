0419ef017f8cd6c38ca313f4722bc3a0d6463a47
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-09 18:00:55 -0800

    7. The Difference Between Ratio and Interval Scales

--- challenge_06_probability_statistics/02.variables-in-statistics/main.py
+++ challenge_06_probability_statistics/02.variables-in-statistics/main.py
@@ -1,21 +1,19 @@
 import pandas as pd
 
 
-# - Using the Height_labels variable only, we can tell whether player Kiah Stokes is taller than Riquna Williams. Assign
-#   your answer to a variable named question1.
-# - We can measure the height difference between Kiah Stokes and Riquna Williams using the Height_labels variable. Assign
-#   your answer to question2.
-# - The Height_labels and the College variables are both measured on an ordinal scale. Assign your answer to question3.
-# - The Games Played variable is not measured on an ordinal scale. Assign your answer to question4.
-# - The Experience variable is measured on an ordinal scale. Assign your answer to question5.
-# - The Height_labels variable is qualitative because it is measured using words. Assign your answer to question6.
+# - For the variables measured on a interval scale, add their names as a string to a list named interval. Sort the
+#   list alphabetically.
+# - For the variables measured on a ratio scale, add their names as a string to a list named ratio. Sort the list
+#   alphabetically.
+# - We've also added the Weight_deviation variable to the dataset, so make sure you include that one too in one of the
+#   lists.
 def main():
-    question1 = True
-    question2 = False
-    question3 = False
-    question4 = True
-    question5 = False
-    question6 = False
+    wnba = pd.read_csv('wnba.csv')
+
+    interval = sorted(['Birthdate', 'Weight_deviation'])
+    ratio = sorted(['Height', 'Weight', 'BMI', 'Age', 'Experience', 'Games Played', 'MIN', 'FGM', 'FGA', 'FG%', '15:00',
+                    '3PA', '3P%', 'FTM', 'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TO', 'PTS', 'DD2',
+                    'TD3'])
 
 
 if __name__ == '__main__':
