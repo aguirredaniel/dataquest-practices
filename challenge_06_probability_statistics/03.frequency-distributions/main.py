acbdb99d40256230478eb97bb7902fc6e46c0c40
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-15 22:25:53 -0800

    9. Information Loss

--- challenge_06_probability_statistics/03.frequency-distributions/main.py
+++ challenge_06_probability_statistics/03.frequency-distributions/main.py
@@ -1,22 +1,29 @@
 import pandas as pd
 
 
-# - Examine the frequency table for the PTS (total points) variable trying to find some patterns in the distribution of
-#   values. Then, generate a grouped frequency distribution table for the PTS variable with the following
-#   characteristics:
-# - The table has 10 class intervals.
-# - For each class interval, the table shows percentages instead of frequencies.
-# - The class intervals are sorted in descending order.
-# - Assign the table to a variable named grouped_freq_table, then print it and try again to find some patterns in the
-#  distribution of values.
+# - Generate a grouped frequency distribution for the MIN variable (minutes played during the season), and experiment
+#   with the number of class intervals to get a sense for what conclusions you can draw as you vary the number of class
+#   intervals. Try to experiment with the following numbers of class intervals:
+#   -  1
+#   -  2
+#   -  3
+#   -  5
+#   - 10
+#   - 15
+#   - 20
+#   - 40
 def main():
     pd.options.display.max_rows = 200
     pd.options.display.max_columns = 50
 
     wnba = pd.read_csv('../data/wnba.csv')
 
-    grouped_freq_table = (wnba['PTS'].value_counts(bins=10, normalize=True) * 100).sort_index(ascending=False)
-    print(grouped_freq_table)
+    class_intervals = [1, 2, 3, 5, 10, 15, 20, 40]
+
+    for class_interval in class_intervals:
+        grouped_freq_table = (wnba['MIN'].value_counts(bins=class_interval, normalize=True) * 100).sort_index(
+            ascending=False)
+        print(grouped_freq_table)
 
 
 if __name__ == '__main__':
