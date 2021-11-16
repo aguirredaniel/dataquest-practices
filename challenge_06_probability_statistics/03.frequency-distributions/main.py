22804e35aae755eb4c0edc04b38ed028b53c8fd8
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-15 21:58:23 -0800

    7. Finding Percentiles with pandas

--- challenge_06_probability_statistics/03.frequency-distributions/main.py
+++ challenge_06_probability_statistics/03.frequency-distributions/main.py
@@ -1,37 +1,28 @@
 import pandas as pd
-from scipy.stats import percentileofscore
 
 
-def make_pts_ordinal(row):
-    if row['PTS'] <= 20:
-        return 'very few points'
-    if 20 < row['PTS'] <= 80:
-        return 'few points'
-    if 80 < row['PTS'] <= 150:
-        return 'many, but below average'
-    if 150 < row['PTS'] <= 300:
-        return 'average number of points'
-    if 300 < row['PTS'] <= 450:
-        return 'more than average'
-    else:
-        return 'much more than average'
-
-
-# - What percentage of players played half the number of games or less in the 2016-2017 season (there are 34 games in
-#   the WNBA’s regular season)? Use the Games Played column to find the data you need, and assign your answer to a
-#    variable named percentile_rank_half_less.
-# - What percentage of players played more than half the number of games of the season 2016-2017? Assign your result to
-#   percentage_half_more.
+# - Use the Age variable along with Series.describe() to answer the following questions:
+#   - What's the upper quartile of the Age variable? Assign your answer to a variable named age_upper_quartile.
+#   - What's the middle quartile of the Age variable? Assign your answer to a variable named age_middle_quartile.
+#   - What's the 95th percentile of the Age variable? Assign your answer to a variable named age_95th_percentile.
+# - Indicate the truth value of the following sentences:
+#   - A percentile is a value of a variable, and it corresponds to a certain percentile rank in the distribution of that
+#     variable. (If you think this is true, assign True (boolean, not string) to a variable named question1, otherwise
+#     assign False.)
+#   - A percentile rank is a numerical value from the distribution of a variable. (Assign True or False to question2.)
+#   - The 25th percentile is the same thing as the lower quartile, and the upper quartile is the same thing as the third
+#     quartile. (Assign True or False to question3)
 def main():
     pd.options.display.max_rows = 200
     pd.options.display.max_columns = 50
 
     wnba = pd.read_csv('../data/wnba.csv')
-    wnba['PTS_ordinal_scale'] = wnba.apply(make_pts_ordinal, axis=1)
 
-    percentile_rank_half_less = percentileofscore(wnba['Games Played'], 17, kind='weak')
-    percentage_half_more = 100 - percentile_rank_half_less
-    print(percentile_rank_half_less, percentage_half_more, sep='\n')
+    age_describe = wnba['Age'].describe(percentiles=[.75, .50, .95])
+    age_middle_quartile, age_upper_quartile, age_95th_percentile = age_describe[4:-1]
+
+    question1, question2, question3 = True, False, True
+    print(age_upper_quartile, age_middle_quartile, age_95th_percentile)
 
 
 if __name__ == '__main__':
