6a2b81f2e9ea06223b197d748a51f60cb85370b6
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-15 20:25:11 -0800

    2. Frequency Distribution Tables

--- challenge_06_probability_statistics/03.frequency-distributions/main.py
+++ challenge_06_probability_statistics/03.frequency-distributions/main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
 
+# - Using the Series.value_counts() method, generate frequency distribution tables for the following columns:
+# - Pos. Assign the frequency distribution table to a variable named freq_distro_pos.
+# - Height. Assign the frequency distribution table to a variable named freq_distro_height.
 def main():
     pd.options.display.max_rows = 200
     pd.options.display.max_columns = 50
 
     wnba = pd.read_csv('../data/wnba.csv')
-    print(wnba.shape)
-    print(wnba)
+
+    freq_distro_pos = wnba['Pos'].value_counts()
+    freq_distro_height = wnba['Height'].value_counts()
+
+    print(freq_distro_pos, freq_distro_height, sep='\n')
 
 
 if __name__ == '__main__':
