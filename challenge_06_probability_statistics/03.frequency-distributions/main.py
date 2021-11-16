133a1616c47484134a5615940bc1a303abfbc4a8
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-15 22:34:11 -0800

    10. Readability for Grouped Frequency Tables

--- challenge_06_probability_statistics/03.frequency-distributions/main.py
+++ challenge_06_probability_statistics/03.frequency-distributions/main.py
@@ -1,29 +1,28 @@
 import pandas as pd
 
 
-# - Generate a grouped frequency distribution for the MIN variable (minutes played during the season), and experiment
-#   with the number of class intervals to get a sense for what conclusions you can draw as you vary the number of class
-#   intervals. Try to experiment with the following numbers of class intervals:
-#   -  1
-#   -  2
-#   -  3
-#   -  5
-#   - 10
-#   - 15
-#   - 20
-#   - 40
+# - Using the techniques above, generate a grouped frequency table for the PTS variable. The table should have the
+#   following characteristics:
+# - There are 10 class intervals.
+# - The first class interval starts at 0 (not included).
+# - The last class interval ends at 600 (included).
+# - Each interval has a range of 60 points.
+# - Assign the table to a variable named gr_freq_table_10.
 def main():
     pd.options.display.max_rows = 200
     pd.options.display.max_columns = 50
 
     wnba = pd.read_csv('../data/wnba.csv')
 
-    class_intervals = [1, 2, 3, 5, 10, 15, 20, 40]
+    intervals = pd.interval_range(start=0, end=600, freq=60)
+    gr_freq_table_10 = pd.Series([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], index=intervals)
 
-    for class_interval in class_intervals:
-        grouped_freq_table = (wnba['MIN'].value_counts(bins=class_interval, normalize=True) * 100).sort_index(
-            ascending=False)
-        print(grouped_freq_table)
+    for value in wnba['PTS']:
+        for interval in intervals:
+            if value in interval:
+                gr_freq_table_10.loc[interval] += 1
+                break
+    print(gr_freq_table_10)
 
 
 if __name__ == '__main__':
