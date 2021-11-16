504ca31236ea31779a7b77f747a44fb48c705ab4
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-15 20:37:15 -0800

    3. Sorting Frequency Distribution Tables

--- challenge_06_probability_statistics/03.frequency-distributions/main.py
+++ challenge_06_probability_statistics/03.frequency-distributions/main.py
@@ -1,19 +1,29 @@
 import pandas as pd
 
 
-# - Using the Series.value_counts() method, generate frequency distribution tables for the following columns:
-# - Pos. Assign the frequency distribution table to a variable named freq_distro_pos.
-# - Height. Assign the frequency distribution table to a variable named freq_distro_height.
+# - Generate a frequency distribution table for the Age variable, which is measured on a ratio scale, and sort the table
+#   by unique values.
+# - Sort the table by unique values in an ascending order, and assign the result to a variable named age_ascending.
+# - Sort the table by unique values in a descending order, and assign the result to a variable named age_descending.
+# - How many players are under 20?
+# - How many players are 30 or over?
 def main():
     pd.options.display.max_rows = 200
     pd.options.display.max_columns = 50
 
     wnba = pd.read_csv('../data/wnba.csv')
 
-    freq_distro_pos = wnba['Pos'].value_counts()
-    freq_distro_height = wnba['Height'].value_counts()
+    freq_distro_age = wnba['Age'].value_counts()
+    age_ascending = freq_distro_age.sort_index()
+    age_descending = freq_distro_age.sort_index(ascending=False)
 
-    print(freq_distro_pos, freq_distro_height, sep='\n')
+    print(age_ascending, age_descending, sep='\n')
+
+    players_under_20 = 0
+    players_are_or_over_30 = 38
+
+    assert players_under_20 == freq_distro_age[freq_distro_age.index < 20].sum()
+    assert players_are_or_over_30 == freq_distro_age[freq_distro_age.index >= 30].sum()
 
 
 if __name__ == '__main__':
