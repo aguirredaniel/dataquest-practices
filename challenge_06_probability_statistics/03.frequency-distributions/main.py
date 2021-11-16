5fd4f940af4bd6d90597737ba5623d3c57a4ce4e
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-15 21:25:50 -0800

    5. Proportions and Percentages

--- challenge_06_probability_statistics/03.frequency-distributions/main.py
+++ challenge_06_probability_statistics/03.frequency-distributions/main.py
@@ -16,19 +16,25 @@ def make_pts_ordinal(row):
         return 'much more than average'
 
 
-# - Generate a frequency distribution table for the transformed PTS_ordinal_scale column.
-# - Order the table by unique values in a descending order (not alphabetically).
-# - Assign the result to a variable named pts_ordinal_desc.
+# - Answer the following questions about the Age variable:
+# - What proportion of players are 25 years old? Assign your answer to a variable named proportion_25.
+# - What percentage of players are 30 years old? Assign your answer to a variable named percentage_30.
+# - What percentage of players are 30 years or older? Assign your answer to a variable named percentage_over_30.
+# - What percentage of players are 23 years or younger? Assign your answer to a variable named percentage_below_23.
 def main():
     pd.options.display.max_rows = 200
     pd.options.display.max_columns = 50
 
     wnba = pd.read_csv('../data/wnba.csv')
     wnba['PTS_ordinal_scale'] = wnba.apply(make_pts_ordinal, axis=1)
-    freq_distro_pts_ordinal = wnba['PTS_ordinal_scale'].value_counts()
-    print(freq_distro_pts_ordinal)
+    freq_relative_age = wnba['Age'].value_counts(normalize=True) * 100
 
-    pts_ordinal_desc = freq_distro_pts_ordinal.iloc[[4, 3, 0, 2, 1, -1]]
+    proportion_25 = freq_relative_age[25] / 100
+    percentage_30 = freq_relative_age[30]
+    percentage_over_30 = freq_relative_age[freq_relative_age.index >= 30].sum()
+    percentage_below_23 = freq_relative_age[freq_relative_age.index <= 23].sum()
+
+    print(proportion_25, percentage_30, percentage_over_30, percentage_below_23, sep='\n')
 
 
 if __name__ == '__main__':
