00621a051329de4a53bd2f6401c520d415d2e62d
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-17 20:33:54 -0800

    2. Bar Plots

---
+++ challenge_06_probability_statistics/04.visualizing-frequency-distributions/main.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def experience_to_ordinal_scale(experience: int) -> str:
+    """
+     Converts number of years a player has played (experience) to ordinal scale value:
+    Args:
+        experience:
+            Experience in years in WNBA
+    Returns:
+        One of the next ordinal scale values:
+        - Rookie (0)
+        - Little experience (1-3)
+        - Experienced (4-5)
+        - Very experienced (5-10)
+        - Veteran (>10)
+    Example:
+        >>> experience_to_ordinal_scale(2)
+        'Little experience'
+    """
+
+    if pd.isna(experience):
+        return 'Rookie'
+
+    if experience and experience <= 3:
+        return 'Little experience'
+
+    if experience <= 5:
+        return 'Experienced'
+
+    if experience <= 10:
+        return 'Very experienced'
+
+    return 'Veteran'
+
+
+def main():
+    wnba = pd.read_csv('../data/wnba.csv')
+    wnba['Exp_ordinal'] = pd.to_numeric(wnba['Experience'], errors='coerce').apply(experience_to_ordinal_scale)
+    wnba['Exp_ordinal'].value_counts().iloc[[-2, 0, 2, 1, -1]].plot.bar()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
