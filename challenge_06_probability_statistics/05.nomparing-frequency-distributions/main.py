15822759a1492136826ecd7a7d7c89de3ae58939
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-11-19 21:16:48 -0800

    9. Outliers

--- challenge_06_probability_statistics/05.nomparing-frequency-distributions/main.py
+++ challenge_06_probability_statistics/05.nomparing-frequency-distributions/main.py
@@ -36,17 +36,34 @@ def experience_to_ordinal_scale(experience: int) -> str:
     return 'Veteran'
 
 
-# - Using sns.boxplot(), generate a series of box plots to examine the distribution of player weight as a function of
-#   player position. Place the Pos variable on the x-axis and the Weight variable on the y-axis.
-# - Specify plt.show() to display the plot.
+# - Consider the quartiles of the Games Played variable:
+# - Find the interquartile range, and assign the result to a variable named iqr.
+# - Using a factor of 1.5, calculate the lower and upper bound outside which values are considered outliers.
+#   -Assign the value of the lower bound to a variable named lower_bound.
+#   - Assign the upper bound to a variable named upper_bound.
+# - Find how many values in the distribution are outliers.
+#   - Assign the number of outliers below the lower bound to a variable named outliers_low.
+#  - Assign the number of outliers above the upper bound to a variable named outliers_high.
+# - Plot a boxplot to check whether your answers are sensible.
+#   - Specify plt.show() to display the plot.
 def main():
     wnba = pd.read_csv('../data/wnba.csv')
     wnba['Exp_ordinal'] = pd.to_numeric(wnba['Experience'], errors='coerce').apply(experience_to_ordinal_scale)
 
-    wnba['age_mean_relative'] = wnba['Age'].apply(lambda x: 'old' if x >= 27 else 'young')
-    wnba['min_mean_relative'] = wnba['MIN'].apply(lambda x: 'average or above' if x >= 497 else 'below average')
+    games_played = wnba['Games Played']
+    quartiles = games_played.describe()[4:-1]
+    upper_quartile = quartiles[-1]
+    lower_quartile = quartiles[0]
+    factor = 1.5
+    iqr = upper_quartile - lower_quartile
+    iqr_factor = (iqr * factor)
+    lower_bound = lower_quartile - iqr_factor
+    upper_bound = upper_quartile + iqr_factor
 
-    sns.boxplot(x='Pos', y='Weight', data=wnba)
+    outliers_low = games_played[games_played < lower_bound].size
+    outliers_high = games_played[games_played > upper_bound].size
+
+    sns.boxplot(x=games_played)
     plt.show()
 
 
