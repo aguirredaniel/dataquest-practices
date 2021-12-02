2ad64a6aeb998d409025dbefeddd50357859c4bc
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-12-01 21:44:09 -0800

    11. The Sample Mean as an Unbiased Estimator

--- challenge_06_probability_statistics/06.the-mean/main.py
+++ challenge_06_probability_statistics/06.the-mean/main.py
@@ -6,29 +6,23 @@ def mean(distribution):
     return sum(distribution) / len(distribution)
 
 
-# -Take 10000 samples of sample size 100 from the population of sale prices and measure the mean of each sample. For each of the 10000 iterations of a for loop:
-#
-# Use Series.sample() to take a sample of size 100 from the SalePrice variable. The random_state parameter is 0 for the first iteration, 1 for the second iteration, 2 for the third iteration, and so on.
-# Compute the mean of the sample.
-# Use plt.hist() to generate a histogram to visualize the distribution of sample means.
-#
-# Draw a vertical line for the population mean.
-# Label the x-axis "Sample mean".
-# Label the y-axis "Frequency".
-# Set the range of the x-axis to (0,500000). This is the same range as the histogram we built above has. Can you observe any obvious difference between the two histograms now that we've increased the sample size?
+# - Check whether the population mean of the population [3, 7, 2] is equal to the mean of all the sample means of size
+#   2 that we can get if we do sampling without replacement.
+#   - Compute the mean for each sample.
+#   - Compute the mean of all the sample means. Compare it with the population mean using the == operator, and assign
+#     the result of the comparison to a variable named unbiased.
 def main():
-    houses = pd.read_csv('AmesHousing_1.txt', sep='\t')
-    sale_price = houses['SalePrice']
-    parameter = sale_price.mean()
-    statistics = [sale_price.sample(n=100, random_state=i).mean() for i in range(10000)]
+    population = [3, 7, 2]
 
-    plt.hist(statistics)
-    plt.xlabel('Sample mean')
-    plt.ylabel('Frequency')
-    plt.axvline(parameter)
+    parameter = sum(population) / len(population)
 
-    plt.xlim((0, 500000))
-    plt.show()
+    statistics = []
+    for v1 in population:
+        statistics += [sum([v1, v2]) / 2 for v2 in population]
+
+    statistic = sum(statistics) / len(statistics)
+
+    assert statistic == parameter
 
 
 if __name__ == '__main__':
