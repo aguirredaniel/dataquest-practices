1e079b09502f4c142a3e0e081505069f7245e6f5
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-12-01 19:26:07 -0800

    7. Mean House Prices

--- challenge_06_probability_statistics/06.the-mean/main.py
+++ challenge_06_probability_statistics/06.the-mean/main.py
@@ -1,28 +1,24 @@
 import pandas as pd
 
 
-# - The data set is stored in a file named AmesHousing_1.txt. Read the file as a pandas DataFrame, and store it in a
-#   variable named houses.
-#   - The values in each row are tab-separated, which means AmesHousing_1.txt is a TSV (tab-separated value) file. This
-#     is different from a CSV (comma-separated values) file, where the values are separated by commas, not by a tab
-#     character.
-#    - Use the pd.read_table() function or pd.read_csv(sep = '\t') to read in the data set.
-# - With the help of the documentation and by exploring the data set yourself, assess the truth value of the following
-#   sentences:
-#   - This data set has variables measured on every scale of measurement: nominal, ordinal, interval and ratio. (If you
-#     think this is true, assign the boolean True to the variable one, otherwise assign False.)
-#   - The SalePrice column is continuous and measured on an interval scale. (If you think this is true, assign the
-#     boolean True to the variable two, otherwise assign False.)
-#   - In the paper he published here, professor Dean DeCock wrote "The initial Excel file contained 113 variables
-#     describing 3970 property sales that had occurred in Ames, Iowa between 2006 an 2010". If we wanted to measure the
-#     mean sale prices for all the houses sold between 2006 and 2010 in Ames, Iowa, the data stored in the
-#      AmesHousing_1.txt would be a sample. (If you think the last sentence is true, assign the boolean True to the
-#      variable three, otherwise assign False.)
+def mean(distribution):
+    return sum(distribution) / len(distribution)
+
+
+# - Use the function you wrote in step 5 to compute the mean of the SalePrice distribution. Assign the result to a
+#   variable named function_mean.
+# - Use Series.mean() to compute the mean of the SalePrice distribution. Assign the result to a variable named
+#   pandas_mean.
+# - Compare function_mean with pandas_mean using the == operator. Assign the result of the comparison to a variable
+#   named means_are_equal.
+#   - The two means should be equal, so we expect the comparison to resolve to True
 def main():
     houses = pd.read_csv('AmesHousing_1.txt', sep='\t')
-    one = True
-    two = False
-    three = True
+    function_mean = mean(houses['SalePrice'])
+    pandas_mean = houses['SalePrice'].mean()
+
+    means_are_equal = function_mean == pandas_mean
+
 
 if __name__ == '__main__':
     main()
