f521f0c6153722367fd07b7af8df15422dd242d4
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-12-02 19:30:31 -0800

    4. The Median for Open-ended Distributions

--- challenge_06_probability_statistics/07.weighted-mean-and-the-median/main.py
+++ challenge_06_probability_statistics/07.weighted-mean-and-the-median/main.py
@@ -27,17 +27,10 @@ def weighted_mean(means, weights) -> float:
     return mean_weight / weight
 
 
-# - Write a function that computes the weighted mean for any array of numbers.
-#   - The function should take in two arrays: one array containing the mean values, and another array with the
-#     corresponding weights.
-#   - The function returns the weighted mean.
-# - Use the function you wrote to compute the weighted mean for the Mean Price column in the houses_per_year data set.
-#   Assign the result to a variable named weighted_mean_function.
-# - Use the numpy.average() function to compute the weighted mean for the same Mean Price column. Read the documentation
-#   to figure out how you can pass in the weights. Assign the result to a variable named weighted_mean_numpy.
-# - Compare the two weighted means (the one from your function and the one from np.average()) using the == operator.
-#   Round each mean to 10 decimal places to get rid of minor rounding errors. Assign the result to a variable named
-#   equal.
+# - Compute the median for each of the three distributions we already defined in the code editor.
+#   - Assign the median of distribution1 to a variable named median1.
+#   - Assign the median of distribution2 to a variable named median2.
+#   - Assign the median of distribution3 to a variable named median3.
 def main():
     houses = pd.read_csv('../data/AmesHousing_1.txt', sep='\t')
     houses_per_year_data = {
@@ -45,12 +38,15 @@ def main():
         'Mean Price': [181761.648000, 185138.207493, 178841.750804, 181404.567901, 172597.598240],
         'Houses Sold': [625, 694, 622, 648, 341]
     }
-
     houses_per_year = pd.DataFrame(houses_per_year_data)
-    weighted_mean_function = weighted_mean(houses_per_year['Mean Price'], houses_per_year['Houses Sold'])
-    weighted_mean_numpy = np.average(houses_per_year['Mean Price'], weights=houses_per_year['Houses Sold'])
-    equal = round(weighted_mean_function) == round(weighted_mean_numpy)
-    assert equal
+
+    distribution1 = [23, 24, 22, '20 years or lower,', 23, 42, 35]
+    distribution2 = [55, 38, 123, 40, 71]
+    distribution3 = [45, 22, 7, '5 books or lower', 32, 65, '100 books or more']
+
+    median1 = 23
+    median2 = 55
+    median3 = 32
 
 
 if __name__ == '__main__':
