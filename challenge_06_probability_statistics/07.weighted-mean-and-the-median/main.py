b05821fd5b72659b8a4412b241a05b5338063b4d
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-12-02 18:57:50 -0800

    3. The Weighted Mean

--- challenge_06_probability_statistics/07.weighted-mean-and-the-median/main.py
+++ challenge_06_probability_statistics/07.weighted-mean-and-the-median/main.py
@@ -1,14 +1,43 @@
+import numpy as np
 import pandas as pd
 
 
-# - Using only the data we have in the houses_per_year data set, compute the sum of prices for each year.
-# - Add all the sums together.
-# - Divide the final sum by the total number of houses sold. Assign the result to a variable named weighted_mean.
-# - Compute again the mean of the SalePrice column in the houses data set. Assign the value to a variable named
-#   mean_original.
-# - Round each mean value to 10 decimal places to get rid of minor rounding errors and then measure the difference
-#   between the two means. Assign the result to a variable named difference. If the two means are equal, you should get
-#   a difference of 0.
+def weighted_mean(means, weights) -> float:
+    """
+        Calculate the weighted mean.
+    Args:
+        means: Array containing the mean values.
+        weights: Array with the corresponding weights for mean values.
+
+    Returns:
+        Float value for result of weighted mean.
+    Examples
+    _______
+    >>> means = [181761.648000, 185138.207493, 178841.750804, 181404.567901, 172597.598240]
+    >>> weights = [625, 694, 622, 648, 341]
+    >>> weighted_mean(means, weights)
+    180796.0600682314
+    """
+    weight = 0
+    mean_weight = 0
+    for m, w in zip(means, weights):
+        mean_weight += m * w
+        weight += w
+
+    return mean_weight / weight
+
+
+# - Write a function that computes the weighted mean for any array of numbers.
+#   - The function should take in two arrays: one array containing the mean values, and another array with the
+#     corresponding weights.
+#   - The function returns the weighted mean.
+# - Use the function you wrote to compute the weighted mean for the Mean Price column in the houses_per_year data set.
+#   Assign the result to a variable named weighted_mean_function.
+# - Use the numpy.average() function to compute the weighted mean for the same Mean Price column. Read the documentation
+#   to figure out how you can pass in the weights. Assign the result to a variable named weighted_mean_numpy.
+# - Compare the two weighted means (the one from your function and the one from np.average()) using the == operator.
+#   Round each mean to 10 decimal places to get rid of minor rounding errors. Assign the result to a variable named
+#   equal.
 def main():
     houses = pd.read_csv('../data/AmesHousing_1.txt', sep='\t')
     houses_per_year_data = {
@@ -18,14 +47,10 @@ def main():
     }
 
     houses_per_year = pd.DataFrame(houses_per_year_data)
-
-    sums_price_year = (houses_per_year['Mean Price'] * houses_per_year['Houses Sold']).sum()
-    weighted_mean = sums_price_year / houses_per_year['Houses Sold'].sum()
-
-    mean_original = houses['SalePrice'].mean()
-    difference = round(mean_original) - round(weighted_mean)
-
-    print(difference)
+    weighted_mean_function = weighted_mean(houses_per_year['Mean Price'], houses_per_year['Houses Sold'])
+    weighted_mean_numpy = np.average(houses_per_year['Mean Price'], weights=houses_per_year['Houses Sold'])
+    equal = round(weighted_mean_function) == round(weighted_mean_numpy)
+    assert equal
 
 
 if __name__ == '__main__':
