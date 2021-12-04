411610c43dd242e7468d36d868143cad15d5d416
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-12-03 20:29:43 -0800

    3. The Mode for Nominal Variables

--- challenge_06_probability_statistics/08.the-mode/main.py
+++ challenge_06_probability_statistics/08.the-mode/main.py
@@ -1,57 +1,47 @@
 import pandas as pd
 
 
-def mode(values: []):
+def get_mode(values: []):
     """
     Takes in an array of values (including strings) and returns the mode of that array.
     Args:
         values: List of values to calculate the mode.
 
     Returns:
-        The mode of given array.
+        The mode of given array, and the frequency table of all values.
 
     Examples
     ______
     >>> values = [1, '30', '30', 2, 3, '30']
     >>> mode (values)
-    '30'
+    ('30', {1:1, '30': 3, 2:1, 3:1})
     """
     frequency_table = {}
     for v in values:
         if v in frequency_table:
-            frequency_table[v] = frequency_table[v] + 1
+            frequency_table[v] += 1
         else:
             frequency_table[v] = 1
 
-    return sorted(frequency_table, key=frequency_table.get)[-1]
+    return max(frequency_table, key=frequency_table.get), frequency_table
 
 
-# - Write a function that takes in an array of values (including strings) and returns the mode of that array. Inside the
-#   function's definition:
-#   - Initialize an empty dictionary.
-#   - Loop through the values of the array that the function takes in. For each iteration of the loop:
-#     - If the value is already a key in the dictionary we initialized before the loop, increment its dictionary value
-#       by 1.
-#     - Else, define the value as a key in the dictionary, and set the initial dictionary value to 1.
-#   - You should end up with a dictionary containing the unique values of the array as dictionary keys and the count for
-#     each unique value as a dictionary value: example_dictionary = {'unique_value1': 230, 'unique_value2': 23,
-#     'unique_value3': 328}.
-#   - Return the key with the highest count (this key is the mode of the array). For instance, for the
-#     example_dictionary above, you should return the string unique_value3.
-#   - You can use this technique to return the key corresponding to the highest value.
-# - Using the function you wrote, measure the mode of the Land Slope variable, and assign the result to a variable
-#   named mode_function.
-# - Using the Series.mode() method, measure the mode of the Land Slope variable, and assign the result to a
-#   variable named mode_method.
-# - Compare the two modes using the == operator to check whether they are the same and assign the result of the
-#   comparison to a variable named same.
+# - Edit the function you wrote to return both the mode of an array and the dictionary containing the count for each
+#   unique value in the array.
+# - Use the edited function to return, at the same time, the mode of the Roof Style variable and the dictionary
+#   containing the counts for each unique value.
+#   - Assign the mode to a variable named mode.
+#   - Assign the dictionary to a variable named value_counts.
+# - Inspect the content of value_counts and compare it to the value count we'd get by using the Series.value_counts()
+#   method.
+#   - This exercise is meant to give you a better understanding of what happens under the hood when we run
+#     Series.value_counts().
 def main():
     houses = pd.read_csv('../data/AmesHousing_1.txt', sep='\t')
-    mode_function = mode(houses['Land Slope'])
-    mode_method = houses['Land Slope'].mode().iloc[0]
-    same = mode_function == mode_method
+    mode, value_counts = get_mode(houses['Roof Style'])
+    value_counts_method = houses['Roof Style'].value_counts()
 
-    assert same
+    assert value_counts == dict(value_counts_method)
 
 
 if __name__ == '__main__':
