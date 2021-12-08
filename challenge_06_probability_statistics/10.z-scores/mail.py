451017de2b75c2b9690886210e3dc13b9d2a7348
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2021-12-07 22:17:24 -0800

    8. Using Standardization for Comparisons

--- challenge_06_probability_statistics/10.z-scores/mail.py
+++ challenge_06_probability_statistics/10.z-scores/mail.py
@@ -24,18 +24,19 @@ def z_score(distribution: [], value, bessel=0) -> float:
     return (value - mean(distribution)) / std(distribution, ddof=bessel)
 
 
-# - Compute the standard deviation of standardized_sample using the sample standard deviation formula, and assign the
-#   result to a variable named stdev_sample.
-# - Inspect the result to see if the standard deviation equals 1.
+# - Standardize the distributions of the index_1 and index_2 variables. We've coded these columns under the hood, and
+#   they're already part of the houses data set.
+#  - Assume that the values we have for both variables constitute populations.
+#  - Don't forget that each distribution has its own mean and standard deviation.
+# - Print the z-scores for the first two houses in the data set to find out which house has a better overall quality.
+#   - Assign your answer as a string to a variable named better — if the first house is better, assign 'first',
+#     otherwise assign 'second'.
 def main():
     houses = pd.read_csv('../data/AmesHousing_1.txt', sep='\t')
-
-    population = [0, 8, 0, 8]
-
-    standardized_sample = [(p - mean(population)) / std(population, ddof=1) for p in population]
-    stdev_sample = std(standardized_sample, ddof=1)
-
-    print(stdev_sample)
+    houses['z_index_1'] = houses['index_1'].apply(lambda v: (v - houses['index_1'].mean()) / houses['index_1'].std())
+    houses['z_index_2'] = houses['index_2'].apply(lambda v: (v - houses['index_2'].mean()) / houses['index_2'].std())
+    print(houses[['z_index_1', 'index_2']].iloc[:2])
+    better = 'first'
 
 
 if __name__ == '__main__':
