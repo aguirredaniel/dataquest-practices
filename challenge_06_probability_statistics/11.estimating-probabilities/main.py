a362c5ff2e8c09a6686b5a92f9f2c78595d3f448
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-03 22:33:33 -0800

    7. Events vs. Outcomes

--- challenge_06_probability_statistics/11.estimating-probabilities/main.py
+++ challenge_06_probability_statistics/11.estimating-probabilities/main.py
@@ -1,17 +1,13 @@
-# - Find the theoretical probability of getting a 5 when rolling a six-sided die. Assign your answer to p_5.
-# - Tossing a coin twice has four possible outcomes (assume all the outcomes are equally likely):
-#   - Heads on the first toss and heads on the second toss (HH).
-#   - Heads on the first toss and tails on the second (HT).
-#   - Tails on the first toss and heads on the second (TH).
-#   - Tails on the first toss and tails on the second (TT).
-# - Find:
-#   - P(HT): Assign your result to p_ht.
-#   - P(TT): Assign your result to p_tt.
+# - Assume all the outcomes of rolling a six-sided die have an equal chance of occurring. Calculate as proportion the
+#   probability of the following events:
+#   - We get an even number — assign your answer to p_even.
+#   - We get an odd number different than 3 — assign your answer to p_odd_no_3.
+#   - We get an odd number greater than 5 — assign your answer to p_odd_greater_5.
 def main():
-    p_5 = 1 / 6
-    p_ht = 1 / 4
-    p_tt = 1 / 4
-    print(p_5, p_ht, p_tt, sep='\n')
+    p_even = 3 / 6
+    p_odd_no_3 = 2 / 6
+    p_odd_greater_5 = 0 / 6
+    print(p_even, p_odd_no_3, p_odd_greater_5, sep='\n')
 
 
 if __name__ == '__main__':
