c04e524e6223199e5381f25fb624ba70f8e96970
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-03 21:09:57 -0800

    3. Probability as Relative Frequency

--- challenge_06_probability_statistics/11.estimating-probabilities/main.py
+++ challenge_06_probability_statistics/11.estimating-probabilities/main.py
@@ -1,17 +1,17 @@
-# - Just like in our example above, we tossed a coin 100 times and got heads 56 times. Calculate the probability of
-#    getting tails using the formula above and assign the result to p_tail.
-# - We rolled a regular six-sided die 200 times and:
-#   - We got a six 28 times. Calculate the probability of getting a 6 when rolling a six-sided die. Assign the result to
-#     p_six.
-#   - We got an odd number (a 1, a 3, or a 5) 102 times. Calculate the probability of getting an odd number when rolling
-#     a six-sided die. Assign the result to p_odd.
-# - Print p_tail, p_six, and p_odd to examine the probabilities.
+# - We tossed a coin 300 times and got tails 162 times.
+#   - Find the probability of getting heads. Assign your result to p_heads_1.
+#   - Transform the probability in p_heads_1 to a percentage value. Assign the result to percentage_1.
+# - In a different trial, we tossed a coin 5,000 times and got tails 2,450 times.
+#   - Find the probability of getting heads. Assign your result to p_heads_2.
+#   - Transform the probability in p_heads_2 to a percentage value. Assign the result to percentage_2.
 def main():
-    p_tail = 44 / 100
-    p_six = 28 / 200
-    p_odd = 102 / 200
+    p_heads_1 = (300 - 162) / 300
+    percentage_1 = p_heads_1 * 100
 
-    print(p_tail, p_six, p_odd, sep='\n')
+    p_heads_2 = (5000 - 2450) / 5000
+    percentage_2 = p_heads_2 * 100
+
+    print(percentage_1, percentage_2, sep='\n')
 
 
 if __name__ == '__main__':
