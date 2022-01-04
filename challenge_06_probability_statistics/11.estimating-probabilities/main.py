d56c5466d42cb4795662e526176c4e391386564c
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-03 21:21:26 -0800

    4. Repeating an Experiment

--- challenge_06_probability_statistics/11.estimating-probabilities/main.py
+++ challenge_06_probability_statistics/11.estimating-probabilities/main.py
@@ -1,17 +1,36 @@
-# - We tossed a coin 300 times and got tails 162 times.
-#   - Find the probability of getting heads. Assign your result to p_heads_1.
-#   - Transform the probability in p_heads_1 to a percentage value. Assign the result to percentage_1.
-# - In a different trial, we tossed a coin 5,000 times and got tails 2,450 times.
-#   - Find the probability of getting heads. Assign your result to p_heads_2.
-#   - Transform the probability in p_heads_2 to a percentage value. Assign the result to percentage_2.
+from numpy.random import seed, randint
+
+
+# - For this exercise, we already defined the function coin_toss() and the variables probabilities and heads. We're
+#   going to repeat the coin toss 10,000 times, and for each toss we want to save the value of P(H) up to that point.
+# - For each of the 10,000 iterations of a for loop:
+#   - Save the outcome of the coin_toss() function to a variable named outcome.
+#   - If outcome stores the string 'HEAD', then increase the value of heads by 1.
+#   - Divide heads by n to get the current value of P(H). Assign the value to current_probability.
+#     - n comes from for n in range(1, 10001)
+#   - Append current_probability to the list probabilities. The probabilities should be expressed as proportions, not
+#     percentages.
+#  - Print the first and last 10 values in probabilities to inspect the evolution of P(H). What do you observe? We'll
+#    talk more about this on the next screen.
+def coin_toss():
+    return 'HEAD' if randint(0, 2) else 'TAIL'
+
+
 def main():
-    p_heads_1 = (300 - 162) / 300
-    percentage_1 = p_heads_1 * 100
+    seed(1)
+    probabilities = []
+    heads = 0
 
-    p_heads_2 = (5000 - 2450) / 5000
-    percentage_2 = p_heads_2 * 100
+    for n in range(1, 10001):
+        outcome = coin_toss()
 
-    print(percentage_1, percentage_2, sep='\n')
+        if outcome == 'HEAD':
+            heads += 1
+
+        probability = heads / n
+        probabilities.append(probability)
+
+    print(probabilities[:10], probabilities[-10:], sep='\n')
 
 
 if __name__ == '__main__':
