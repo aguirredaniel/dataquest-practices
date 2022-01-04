c3f19c225a52ff52b0e9e54fb76c5970206a075f
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-03 21:34:47 -0800

    5. The True Probability Value

--- challenge_06_probability_statistics/11.estimating-probabilities/main.py
+++ challenge_06_probability_statistics/11.estimating-probabilities/main.py
@@ -1,36 +1,25 @@
-from numpy.random import seed, randint
-
-
-# - For this exercise, we already defined the function coin_toss() and the variables probabilities and heads. We're
-#   going to repeat the coin toss 10,000 times, and for each toss we want to save the value of P(H) up to that point.
-# - For each of the 10,000 iterations of a for loop:
-#   - Save the outcome of the coin_toss() function to a variable named outcome.
-#   - If outcome stores the string 'HEAD', then increase the value of heads by 1.
-#   - Divide heads by n to get the current value of P(H). Assign the value to current_probability.
-#     - n comes from for n in range(1, 10001)
-#   - Append current_probability to the list probabilities. The probabilities should be expressed as proportions, not
-#     percentages.
-#  - Print the first and last 10 values in probabilities to inspect the evolution of P(H). What do you observe? We'll
-#    talk more about this on the next screen.
-def coin_toss():
-    return 'HEAD' if randint(0, 2) else 'TAIL'
-
-
+# - An insurance company conducted a study with 200 individuals, and found that:
+#   - 87 individuals opted for at least a life insurance policy.
+#   - 40 individuals opted for at least life and car insurance policies.
+#   - 63 individuals opted for at least a house insurance policy.
+#   - 160 individuals opted for at least one type of insurance policy.
+# - We can't predict people's choices with certainty, so an individual choosing to buy an insurance policy is a random
+#   experiment. 200 individuals were part of the study, so you can consider the random experiment was performed 200
+#   times. Find:
+#   - P(L): The probability that a new customer opts for at least a life insurance. Assign your answer to a variable
+#     named p_l.
+#   - P(L and C): The probability that an individual opts for at least a life and a car insurance policy. Assign your
+#     answer to a variable named p_l_and_c.
+#   - P(H): The probability that an individual opts for at least a house insurance policy. Assign your answer to a
+#     variable named p_h.
+#   -P(NO): The probability that an individual opts for no insurance at all. Assign your answer to a variable named
+#    p_no.
 def main():
-    seed(1)
-    probabilities = []
-    heads = 0
-
-    for n in range(1, 10001):
-        outcome = coin_toss()
-
-        if outcome == 'HEAD':
-            heads += 1
-
-        probability = heads / n
-        probabilities.append(probability)
-
-    print(probabilities[:10], probabilities[-10:], sep='\n')
+    p_l = 87 / 200
+    p_l_and_c = 40 / 200
+    p_h = 63 / 200
+    p_no = (200 - 160) / 200
+    print(p_l, p_l_and_c, p_h, p_no, sep='\n')
 
 
 if __name__ == '__main__':
