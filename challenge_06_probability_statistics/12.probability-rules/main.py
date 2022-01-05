d8dc0a09610fec07419188b2d3c658a3ee9068ec
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-04 22:34:35 -0800

    7. Mutually Exclusive Events

--- challenge_06_probability_statistics/12.probability-rules/main.py
+++ challenge_06_probability_statistics/12.probability-rules/main.py
@@ -1,31 +1,14 @@
-# For the following exercise, we'll consider a random experiment where we roll a fair six-sided die two times ("fair"
-# means all outcomes have equal chances of occurring). The sample space of this experiment has 36 possible outcomes (all
-# the sequences of numbers we can get from the two throws):
-# E = {(1,1), (1,2), (1,3), ....., (3,1), (3,2), ....., (6,1), (6,2) }
-# For each outcome, we sum up the two numbers and get the following sums:
-#       2  3  4  5  6  7
-#       3  4  5  6  7  8
-#       .  .  .  .  .  .
-#       .  .  .  .  .  .
-#       7 8 9 10, 11, 12
-# - Make sure you've read the instructions above and calculate (notice that for this exercise we're considering a single
-#   die, not the sum of two dice):
-#   - P(C) — assign your answer to p_c. C = {2, 4, 6} — getting an even number
-#   - P(D) — assign your answer to p_d. D = {4, 5, 6} — getting a number greater than 3
-# - The event "getting a number that is either even or greater than 3" corresponds to the event "C or D". Calculate:
-#   - P(C or D) using the addition rule — assign your answer to p_c_d_addition. Check the hint if you're not sure about
-#     the outcomes corresponding to the event "C or D".
-#   - P(C or D) using the formula we've been using to compute theoretical probabilities (the number of successful
-#     outcomes divided by the number of possible outcomes) — assign your answer to p_c_d_formula.
-# - Print p_c_d_addition and p_c_d_formula. Why do you think we see different results (we'll explain this on the next
-#   screen)?
+# - An online betting company offers customers the possibility of betting on a variety of games and events (football,
+#   tennis, hockey, horse races, car races, etc.). Based on historical data, the company knows the empirical
+#   probabilities of the following events:
+#   - Event F (a new customer's first bet is on football) — the probability is 0.26.
+#   - Event T (a new customer's first bet is on tennis) — the probability is 0.11.
+#   - Event "T and F" (a new customer's first bet is on both football and tennis) — the probability is 0.03.
+# - Find the probability that a new customer's first bet is either on football or tennis. Assign your answer to p_f_or_t
+#   You can't use theoretical probability formula to solve this, so you'll need to make use of the addition rule.
 def main():
-    p_c = 3 / 6
-    p_d = 3 / 6
-    p_c_d_addition = 6 / 6
-    p_c_d_formula = 4 / 6
-
-    print(p_c, p_d, p_c_d_addition, p_c_d_formula, sep='\n')
+    p_f_or_t = 0.26 + 0.11 - 0.03
+    print(p_f_or_t)
 
 
 if __name__ == '__main__':
