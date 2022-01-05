bad2e0e7f9d226b1a1cfe065ce6183f139669d66
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-04 21:31:15 -0800

    1. Sample Space

---
+++ challenge_06_probability_statistics/12.probability-rules/main.py
@@ -0,0 +1,9 @@
+# - We toss a normal coin two times. Find the sample space of this experiment and assign it to the list coin_toss_omega.
+#   - One of the outcomes is heads first, tails second. Abbreviate heads as H, tails as T, and assign the string 'HT'
+#     (heads first, tails second) to the list coin_toss_omega. Repeat this pattern for the other outcomes.
+def main():
+    coin_toss_omega = ['HH', 'HT', 'TH', 'TT']
+
+
+if __name__ == '__main__':
+    main()
