5b89fea9548fa571cb6104dbee01d126e2405be4
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-04 22:01:14 -0800

    5. Venn Diagrams

--- challenge_06_probability_statistics/12.probability-rules/main.py
+++ challenge_06_probability_statistics/12.probability-rules/main.py
@@ -8,17 +8,24 @@
 #       .  .  .  .  .  .
 #       .  .  .  .  .  .
 #       7 8 9 10, 11, 12
-# - Using the table above, find for the same experiment the probability of the following events:
-#   - The sum is either 5 or 9 — assign your answer to p_5_or_9.
-#   - The sum is either even or less than 2 — assign your answer to p_even_or_less_2.
-#   - The sum is either 4 or a multiple of 3 — assign your answer to p_4_or_3_multiple. Check the hint if you don't
-#     remember what a multiple is.
+# - Make sure you've read the instructions above and calculate (notice that for this exercise we're considering a single
+#   die, not the sum of two dice):
+#   - P(C) — assign your answer to p_c. C = {2, 4, 6} — getting an even number
+#   - P(D) — assign your answer to p_d. D = {4, 5, 6} — getting a number greater than 3
+# - The event "getting a number that is either even or greater than 3" corresponds to the event "C or D". Calculate:
+#   - P(C or D) using the addition rule — assign your answer to p_c_d_addition. Check the hint if you're not sure about
+#     the outcomes corresponding to the event "C or D".
+#   - P(C or D) using the formula we've been using to compute theoretical probabilities (the number of successful
+#     outcomes divided by the number of possible outcomes) — assign your answer to p_c_d_formula.
+# - Print p_c_d_addition and p_c_d_formula. Why do you think we see different results (we'll explain this on the next
+#   screen)?
 def main():
-    p_5_or_9 = 8 / 36
-    p_even_or_less_2 = 18 / 36
-    p_4_or_3_multiple = 15 / 36
+    p_c = 3 / 6
+    p_d = 3 / 6
+    p_c_d_addition = 6 / 6
+    p_c_d_formula = 4 / 6
 
-    print(p_5_or_9, p_even_or_less_2, p_4_or_3_multiple, sep='\n')
+    print(p_c, p_d, p_c_d_addition, p_c_d_formula, sep='\n')
 
 
 if __name__ == '__main__':
