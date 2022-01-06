68a0b9bc48b35200ec8e875c0e808fa938a3e67a
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-05 23:46:44 -0800

    7. Combining Formulas

--- challenge_06_probability_statistics/13.solving-complex-probability-problems/main.py
+++ challenge_06_probability_statistics/13.solving-complex-probability-problems/main.py
@@ -1,13 +1,13 @@
-# - Find the probability of:
-#   - Getting heads up 18 times in a row when flipping a fair coin. Assign your answer to p_18h.
-#   - Getting a six three times in a row when throwing a fair six-sided die. Assign your answer to p_666.
-#   - Not getting any six when throwing a fair six-sided die four times. Assign your answer to p_not_6.
+# - Find the probability of getting at least one double-six in 24 throws of two six-sided dice (the two dice are thrown
+#   simultaneously). Assign your answer to p_one_double_6. The table below shows all the outcomes of throwing two
+#   six-sided dice.
 def main():
-    p_18h = 0.5 ** 18
-    p_666 = (1 / 6) ** 3
-    p_not_6 = (5 / 6) ** 4
+    p_double_six = 1 / 6 ** 2
+    p_non_double_six = 1 - p_double_six
+    p_non_double_six_24 = p_non_double_six ** 24
+    p_one_double_6 = 1 - p_non_double_six_24
 
-    print(p_18h, p_666, p_not_6, sep='\n')
+    print(p_one_double_6)
 
 
 if __name__ == '__main__':
