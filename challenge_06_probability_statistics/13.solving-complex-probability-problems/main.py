3ada56a8feef6285c6980e2d9c2dbc8ae4b8f28b
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-05 22:30:19 -0800

    3. Example Walk-Through

--- challenge_06_probability_statistics/13.solving-complex-probability-problems/main.py
+++ challenge_06_probability_statistics/13.solving-complex-probability-problems/main.py
@@ -1,18 +1,12 @@
-# - A company that develops a time-tracking tool sells two kinds of subscription: basic and premium. When a new user
-#  tries the product, there's a 0.2 probability the user buys the basic subscription and 0.15 he buys premium. Find:
-#   - The probability that a new user doesn't buy a basic subscription (you'll need to use the P(E) = 1 - P(non-E)
-#     formula). Assign your result to p_non_basic.
-#   - The probability that a new user doesn't buy a premium subscription. Assign your result to p_non_premium.
-#   - The probability that a user buys either basic or premium. Assign your result to p_subscription (assume buying
-#     basic and buying premium are mutually exclusive).
-#   - The probability that a new user doesn't buy a subscription. Assign your result to p_non_subscription.
+# - Find the probability that it takes four flips or more for a coin to land heads up (let's call this event "B").
+#   - Begin with finding the probability of the event non-B, which is equivalent to finding the probability that we'll
+#     get at least one heads if we flip a coin three times. Assign your result to p_non_b.
+#   - Now use p_non_b to find the probability of B. Assign your result to p_b.
 def main():
-    p_non_basic = 1 - 0.2
-    p_non_premium = 1 - 0.15
-    p_subscription = 0.2 + 0.15
-    p_non_subscription = 1 - p_subscription
+    p_non_b = 7 / 8
+    p_b = 1 - p_non_b
 
-    print(p_subscription, p_non_subscription, sep='\n')
+    print(p_non_b, p_b, sep='\n')
 
 
 if __name__ == '__main__':
