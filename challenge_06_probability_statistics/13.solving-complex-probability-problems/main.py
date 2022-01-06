7a87b6d6046eff53ddda33e6937191ca7e25ec04
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-05 22:16:54 -0800

    2. Opposite Events

--- challenge_06_probability_statistics/13.solving-complex-probability-problems/main.py
+++ challenge_06_probability_statistics/13.solving-complex-probability-problems/main.py
@@ -1,20 +1,18 @@
-# - An advertisement company runs a quick test and shows two ads on the same web page (ad "A" and ad "B") to 100 users.
-#   At the end of the trial, they found:
-#   - 12 users clicked on ad "A"
-#   - 17 users clicked on ad "B"
-#   - 3 users clicked on both ad "A" and ad "B"
-# - Find:
-#   - The empirical probability that a user clicks on ad "A." Assign your result to p_a.
-#   - The empirical probability that a user clicks on ad "B." Assign your result to p_b.
-#   - The empirical probability that a user clicks on both ad "A" and ad "B." Assign your result to p_a_and_b.
-#   - The probability that a user clicks on either ad "A" or ad "B." Assign your result to p_a_or_b. For this exercise,
-#     keep in mind a user can click on both ads, so the events are not mutually exclusive — use the addition rule.
+# - A company that develops a time-tracking tool sells two kinds of subscription: basic and premium. When a new user
+#  tries the product, there's a 0.2 probability the user buys the basic subscription and 0.15 he buys premium. Find:
+#   - The probability that a new user doesn't buy a basic subscription (you'll need to use the P(E) = 1 - P(non-E)
+#     formula). Assign your result to p_non_basic.
+#   - The probability that a new user doesn't buy a premium subscription. Assign your result to p_non_premium.
+#   - The probability that a user buys either basic or premium. Assign your result to p_subscription (assume buying
+#     basic and buying premium are mutually exclusive).
+#   - The probability that a new user doesn't buy a subscription. Assign your result to p_non_subscription.
 def main():
-    p_a = 12 / 100
-    p_b = 17 / 100
-    p_a_and_b = 3 / 100
+    p_non_basic = 1 - 0.2
+    p_non_premium = 1 - 0.15
+    p_subscription = 0.2 + 0.15
+    p_non_subscription = 1 - p_subscription
 
-    p_a_or_b = p_a + p_b - p_a_and_b
+    print(p_subscription, p_non_subscription, sep='\n')
 
 
 if __name__ == '__main__':
