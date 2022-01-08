3f8e6b77c938e8eb77d6fcd6ae507ed1d510ab35
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-07 23:47:45 -0800

    6. Permutations Formulas

--- challenge_06_probability_statistics/14.permutations-and-combinations/main.py
+++ challenge_06_probability_statistics/14.permutations-and-combinations/main.py
@@ -1,6 +1,6 @@
 # - Find the probability of cracking a 4-digit PIN code using the code 8362. Assign your answer to p_crack_4.
 # -0 Find the probability of cracking a 6-digit PIN code using the code 348821. Assign your answer to p_crack_6.
-def factorial(n, k=None):
+def permutation(n, k=None):
     """
     Calculate the nPk=n⋅(n−1)⋅(n−2)⋅…⋅(n−k+1) formula.
 
@@ -13,24 +13,22 @@ def factorial(n, k=None):
 
     Examples
     --------
-    >>> factorial(5)
+    >>> permutation(5)
     120
-    >>> factorial(52, 3)
+    >>> permutation(52, 3)
     421200
     """
     if not k:
         k = n
     if k == 1:
         return n
-    return n * factorial(n - 1, k - 1)
+    return n * permutation(n - 1, k - 1)
 
 
 def main():
-    perm_3_52 = factorial(52, 3)
-    perm_4_20 = factorial(20, 4)
-    perm_4_27 = factorial(27, 4)
+    p_crack_pass = 1 / permutation(127, 16)
 
-    print(perm_3_52, perm_4_20, perm_4_27, sep='\n')
+    print(p_crack_pass)
 
 
 if __name__ == '__main__':
