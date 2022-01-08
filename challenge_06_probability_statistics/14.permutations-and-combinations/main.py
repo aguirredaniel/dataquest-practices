3a3f986c3b6468d770fa493d8813743b43a122a4
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-07 22:38:32 -0800

    4. Permutations

--- challenge_06_probability_statistics/14.permutations-and-combinations/main.py
+++ challenge_06_probability_statistics/14.permutations-and-combinations/main.py
@@ -1,13 +1,31 @@
 # - Find the probability of cracking a 4-digit PIN code using the code 8362. Assign your answer to p_crack_4.
 # -0 Find the probability of cracking a 6-digit PIN code using the code 348821. Assign your answer to p_crack_6.
+def factorial(n):
+    """
+    Calculate the n factorial.
+
+    Args:
+        n: numeric value
+
+    Returns:
+        The n! (n factorial).
+
+    Examples
+    --------
+    >>> factorial(5)
+    120
+    """
+    if n == 2:
+        return 2
+    return n * factorial(n - 1)
+
+
 def main():
-    four_digit_outcomes = 10 ** 4
-    p_crack_4 = 1 / four_digit_outcomes
+    letters = {"a", "b", "j", "k", "x", "y"}
+    permutations_1 = factorial(len(letters))
+    permutations_2 = factorial(52)
 
-    six_digit_outcomes = 10 ** 6
-    p_crack_6 = 1 / six_digit_outcomes
-
-    print(p_crack_4, p_crack_6, sep='\n')
+    print(permutations_1, permutations_2, sep='\n')
 
 
 if __name__ == '__main__':
