5337a3ee2b9d90ca9fd378adcef6b9f67b69ac0b
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-07 22:25:53 -0800

    3. Example Walkthrough

--- challenge_06_probability_statistics/14.permutations-and-combinations/main.py
+++ challenge_06_probability_statistics/14.permutations-and-combinations/main.py
@@ -1,17 +1,13 @@
-# - We roll a fair six-sided die three times and then randomly draw a card from a standard 52-card deck. One of the
-#   outcomes is (6, 6, 6, ace of diamonds), which means getting three 6's in a row when we roll the die, followed by
-#   drawing an ace of diamonds from the deck.
-#   - Use the extended rule of product to calculate the total number of outcomes. Assign your answer to total_outcomes.
-#   - Use total_outcomes to calculate the probability of getting (6, 6, 6, ace of diamonds) — three sixes in a row
-#     followed by an ace of diamonds. Assign your answer to p_666_ace_diamonds.
-#   - Use p_666_ace_diamonds to calculate the probability of getting anything but (6, 6, 6, ace of diamonds). Assign
-#     your answer to p_no_666_ace_diamonds.
+# - Find the probability of cracking a 4-digit PIN code using the code 8362. Assign your answer to p_crack_4.
+# -0 Find the probability of cracking a 6-digit PIN code using the code 348821. Assign your answer to p_crack_6.
 def main():
-    total_outcomes = 6 ** 3 * 52
-    p_666_ace_diamonds = 1 / 6 ** 3 * 1 / 52
-    p_no_666_ace_diamonds = 1 - p_666_ace_diamonds
+    four_digit_outcomes = 10 ** 4
+    p_crack_4 = 1 / four_digit_outcomes
 
-    print(total_outcomes, p_666_ace_diamonds, p_no_666_ace_diamonds, sep='\n')
+    six_digit_outcomes = 10 ** 6
+    p_crack_6 = 1 / six_digit_outcomes
+
+    print(p_crack_4, p_crack_6, sep='\n')
 
 
 if __name__ == '__main__':
