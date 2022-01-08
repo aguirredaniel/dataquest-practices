9641f83e606dfd7e612c0903986ea4e5c9b3b5bd
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-07 23:07:36 -0800

    5. More About Permutations

--- challenge_06_probability_statistics/14.permutations-and-combinations/main.py
+++ challenge_06_probability_statistics/14.permutations-and-combinations/main.py
@@ -1,11 +1,12 @@
 # - Find the probability of cracking a 4-digit PIN code using the code 8362. Assign your answer to p_crack_4.
 # -0 Find the probability of cracking a 6-digit PIN code using the code 348821. Assign your answer to p_crack_6.
-def factorial(n):
+def factorial(n, k=None):
     """
-    Calculate the n factorial.
+    Calculate the nPk=n⋅(n−1)⋅(n−2)⋅…⋅(n−k+1) formula.
 
     Args:
-        n: numeric value
+        n: number of objects
+        k: number of taking objects
 
     Returns:
         The n! (n factorial).
@@ -14,18 +15,22 @@ def factorial(n):
     --------
     >>> factorial(5)
     120
+    >>> factorial(52, 3)
+    421200
     """
-    if n == 2:
-        return 2
-    return n * factorial(n - 1)
+    if not k:
+        k = n
+    if k == 1:
+        return n
+    return n * factorial(n - 1, k - 1)
 
 
 def main():
-    letters = {"a", "b", "j", "k", "x", "y"}
-    permutations_1 = factorial(len(letters))
-    permutations_2 = factorial(52)
-
-    print(permutations_1, permutations_2, sep='\n')
+    perm_3_52 = factorial(n=52, k=5)
+    print(perm_3_52)
+    assert perm_3_52 == 311875200
+    assert factorial(6) == 720
+    print(perm_3_52, sep='\n')
 
 
 if __name__ == '__main__':
