1a99dd8eb037eb68264500780544aab69fa2c0cf
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-11 23:03:01 -0800

    3. Updating Probabilities

--- challenge_06_probability_statistics/15.conditional-probability-fundamentals/main.py
+++ challenge_06_probability_statistics/15.conditional-probability-fundamentals/main.py
@@ -1,13 +1,14 @@
-# - Consider rolling a fair six-sided die once and calculate:
-#   - The probability of getting a 2. Assign your answer to p_2.
-#   - The probability of getting an odd number (1, 3, or 5). Assign your answer to p_odd.
-#   - The probability of getting a 2 or a 4, Assign your answer to p_2_or_4.
+# - A fair six-sided die is rolled. All we know is that the number we got is less than 5. Calculate:
+#   - The probability of getting a 3. Assign your answer to p_3.
+#   - The probability of getting a 6. Assign your answer to p_6.
+#   - The probability of getting an odd number. Assign your answer to p_odd.
+#   - The probability of getting an even number. Assign your answer to p_even.
 def main():
-    p_2 = 1 / 6
-    p_odd = 3 / 6
-    p_2_or_4 = 2 / 6
-
-    print(p_2, p_odd, p_2_or_4, sep='\n')
+    p_3 = 1 / 4
+    p_6 = 0
+    p_odd = 2 / 4
+    p_even = 2 / 4
+    print(p_3, p_6, p_odd, p_even, sep='\n')
 
 
 if __name__ == '__main__':
