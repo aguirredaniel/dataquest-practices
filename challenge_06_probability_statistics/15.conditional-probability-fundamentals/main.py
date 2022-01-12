f024fdfda3602ab098de94fe8b4ff5a8544e990e
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-11 23:54:59 -0800

    7. Probability Formula

--- challenge_06_probability_statistics/15.conditional-probability-fundamentals/main.py
+++ challenge_06_probability_statistics/15.conditional-probability-fundamentals/main.py
@@ -1,17 +1,23 @@
-# - Two fair six-sided dice are simultaneously rolled, and the two numbers they show are added together. The diagram
-#   below shows all the possible results that we can get from adding the two numbers together.
-# - Find P(A|B), where A is the event where the sum is an even number, and B is the event that the sum is less than
-#   eight.
-#   - Find card(B). Assign your answer to card_b.
-#   - Note that you'll have to treat identical sums differently if they come from different die numbers. On the diagram
-#     above, we see that we have three sums of 4, but they all come from different die outcomes: (3, 1), (2,2), and (1, 3), where the first number describes the outcome of the first die throw, and the second number the outcome of the second die throw.
-#   - Find card(A ∩ B). Assign your answer to card_a_and_b.
-#   - Calculate P(A|B). Assign your answer to p_a_given_b.
+# - A company offering a browser-based task manager tool intends to do some targeted advertising based on people's
+#   browsers. The data they collected about their users is described in the table below:
+# - Find:
+#   - P(Premium | Chrome) — the probability that a randomly chosen user has a premium subscription, provided their
+#     browser is Chrome. Assign your answer to p_premium_given_chrome.
+#   - P(Basic | Safari) — the probability that a randomly chosen user has a basic subscription, provided their browser
+#     is Safari. Assign your answer to p_basic_given_safari.
+#   - P(Free | Firefox)} — the probability that a randomly chosen user has a free subscription, provided their browser
+#     is Firefox. Assign your answer to p_free_given_firefox.
+#   - Between a Chrome user and a Safari user, who is more likely to have a premium subscription? If you think a Chrome
+#     user is the answer, then assign the string 'Chrome' to a variable named more_likely_premium, otherwise assign
+#     'Safari'. To solve this exercise, you'll also need to calculate P(Premium | Safari).
 def main():
-    card_b = 21
-    card_a_and_b = 9
-    p_a_given_b = card_a_and_b / card_b
-    print(p_a_given_b, sep='\n')
+    p_premium_given_chrome = 158 / 2762
+    p_basic_given_safari = 274 / 1288
+    p_free_given_firefox = 2103 / 2285
+    p_premium_given_safari = 120 / 1288
+    more_likely_premium = 'Chrome' if p_premium_given_chrome > p_premium_given_safari else 'Safari'
+
+    print(p_premium_given_chrome, p_basic_given_safari, p_free_given_firefox, more_likely_premium, sep='\n')
 
 
 if __name__ == '__main__':
