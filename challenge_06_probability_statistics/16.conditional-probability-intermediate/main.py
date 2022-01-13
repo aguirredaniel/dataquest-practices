8f00a6063b30667c20e264815326586553af66e0
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-13 00:23:13 -0800

    4. The Multiplication Rule

--- challenge_06_probability_statistics/16.conditional-probability-intermediate/main.py
+++ challenge_06_probability_statistics/16.conditional-probability-intermediate/main.py
@@ -1,15 +1,22 @@
-# - Find:
-#   - P(M|LC) — assign your answer to p_m_given_non_l.
-#   - P(LC|M) — assign your answer to p_non_l_given_m.
-#   - P(M ∩ LC) — assign your answer to p_m_and_non_l.
-#   - P(LC ∩ M) — assign your answer to p_non_l_and_m. Check the hint if you're not sure about exercises 3 and 4.
+# - For the exercises below, we know:
+#   - The probability that a customer buys RAM memory from an electronics store is P(RAM) = 0.0822.
+#   - The probability that a customer buys a gaming laptop is P(GL) = 0.0184.
+#   - The probability that a customer buys RAM memory given that they bought a gaming laptop is P(RAM | GL) = 0.0022.
+# - Calculate:
+#   - P(GL ∩ RAM) — assign your answer to p_gl_and_ram.
+#   - P(RAMC | GL) — assign your answer to p_non_ram_given_gl.
+#   - P(GL ∩ RAMC) — assign your answer to p_gl_and_non_ram..
 def main():
-    p_m_given_non_l = 483 / 1910
-    p_non_l_given_m = 483 / 515
-    p_m_and_non_l = p_m_given_non_l * 1910 / 2000
-    p_non_l_and_m = p_non_l_given_m * 515 / 2000
+    p_ram = 0.0822
+    p_gl = 0.0184
+    p_ram_given_gl = 0.0022
 
-    print(p_m_given_non_l, p_non_l_given_m, p_m_and_non_l, p_non_l_and_m, sep='\n')
+    p_gl_and_ram = p_gl * p_ram_given_gl
+    p_non_ram_given_gl = 1 - p_ram_given_gl
+    p_gl_and_non_ram = p_gl * p_non_ram_given_gl
+    p_gl_or_ram = p_gl + p_ram - p_gl_and_ram
+
+    print(p_gl_and_ram, p_non_ram_given_gl, p_gl_and_non_ram, p_gl_or_ram, sep='\n')
 
 
 if __name__ == '__main__':
