c7f3439600e9260c41ab77d575af6173d885670a
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-13 00:49:27 -0800

    6. Statistical Dependence

--- challenge_06_probability_statistics/16.conditional-probability-intermediate/main.py
+++ challenge_06_probability_statistics/16.conditional-probability-intermediate/main.py
@@ -1,18 +1,20 @@
-# - A fair six-sided die is rolled twice and the following three events are considered:
-#   - Event K — the die showed a 4 on the second roll
-#   - Event L — the die showed a 2 on the first roll
-#   - Event M — the die showed an even number on the second roll
-# - Find whether the following events are independent or not:
-#   -  Events K and L — assign the string 'independent' to a variable named k_and_l if the events are independent,
-#      otherwise assign the string 'dependent'.
-#   -  Events L and M — assign the string 'independent' to a variable named l_and_m if the events are independent,
-#      otherwise assign the string 'dependent'.
-#   - Events K and M — assign the string 'independent' to a variable named k_and_m if the events are independent,
+# - Find whether the following events are independent or not (check the hint if you don't know how to solve this):
+#   - Events L and M — assign the string 'independent' to a variable named l_and_m if the events are independent,
 #     otherwise assign the string 'dependent'.
+#   - Events L and MC — assign the string 'independent' to a variable named l_and_non_m if the events are independent,
+#     otherwise assign the string 'dependent'.
+# - Use the formulas we learned to calculate (you could also calculate the probabilities just by looking at the table in
+#   this case, but try to use the formulas):
+#   - P(L ∩ M) — assign your answer to p_l_and_m.
+#   - P(L ∩ MC) — assign your answer to p_l_and_non_m.
 def main():
-    k_and_l = 'independent'
-    l_and_m = 'independent'
-    k_and_m = 'dependent'
+    l_and_m = 'dependent'
+    l_and_non_m = 'dependent'
+
+    p_l_and_m = 515 / 2000 * 32 / 515
+    p_l_and_non_m = 1485 / 2000 * 58 / 1485
+
+    print(p_l_and_m, p_l_and_non_m, sep='\n')
 
 
 if __name__ == '__main__':
