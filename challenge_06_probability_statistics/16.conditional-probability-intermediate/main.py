88a741ab1ed1a674322cf091b3adcecec0533d1e
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-12 23:30:20 -0800

    3. Order of Conditioning

--- challenge_06_probability_statistics/16.conditional-probability-intermediate/main.py
+++ challenge_06_probability_statistics/16.conditional-probability-intermediate/main.py
@@ -1,19 +1,15 @@
-# - Using the two rules we learned above, find:
-#   - P(BC|M), and assign your answer to p_non_b_given_m.
-#   - P(CC|L), and assign your answer to p_non_c_given_l.
-#   - P(B|C), and assign your answer to p_b_given_c.
-#   - P(B|MC), and assign your answer to p_b_given_non_m. If you think you can't calculate the probability using any of
-#     the two rules above, assign the string 'not possible' to the same variable p_b_given_non_m.
+# - Find:
+#   - P(M|LC) — assign your answer to p_m_given_non_l.
+#   - P(LC|M) — assign your answer to p_non_l_given_m.
+#   - P(M ∩ LC) — assign your answer to p_m_and_non_l.
+#   - P(LC ∩ M) — assign your answer to p_non_l_and_m. Check the hint if you're not sure about exercises 3 and 4.
 def main():
-    p_b_given_m = 0.1486
-    p_c_given_l = 0.0928
-    p_non_b_given_c = 0.7622
+    p_m_given_non_l = 483 / 1910
+    p_non_l_given_m = 483 / 515
+    p_m_and_non_l = p_m_given_non_l * 1910 / 2000
+    p_non_l_and_m = p_non_l_given_m * 515 / 2000
 
-    p_non_b_given_m = 1 - p_b_given_m
-    p_non_c_given_l = 1 - p_c_given_l
-    p_b_given_c = 1 - p_non_b_given_c
-
-    p_b_given_non_m = 'not possible'
+    print(p_m_given_non_l, p_non_l_given_m, p_m_and_non_l, p_non_l_and_m, sep='\n')
 
 
 if __name__ == '__main__':
