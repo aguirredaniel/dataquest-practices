9ee71dfe860f7e4a8b52b4634db9c5277e5e4b57
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-14 23:46:16 -0800

    2. Example Walk-through

--- challenge_06_probability_statistics/17.bayes-theorem/main.py
+++ challenge_06_probability_statistics/17.bayes-theorem/main.py
@@ -1,16 +1,23 @@
-# - For the exercises below, consider the following probabilities:
-#   - The probability of being infected with HIV is 0.00014. That is P ( H I V ) = 0.00014 .
-#   - The probability of being infected with HIV given a positive result from an HIV test is 0.03.
-# - Assess with True or False the following statements:
-#   - Events HIV and T+ are independent. If you think this statement is true, then assign the boolean True to
-#     statement_1, otherwise assign False. Events HIV and HIVC are mutually exclusive.
-#   - If you think this statement is true, then assign the boolean True to statement_2, otherwise assign False.
-#   - Events HIVC and T+ are dependent. If you think this statement is true, then assign the boolean True to
-#     statement_3, otherwise assign False.
+# - We can find the word "secret" in many spam emails. However, some emails are not spam even though they contain the
+#   word "secret." Let's say we know the following probabilities:
+#   - The probability of getting a spam email is 23.88%. That is P ( S p a m ) = 0.2388 .
+#   - The probability of an email containing the word "secret" given that the email is spam is 48.02%. That is
+#     P ( "secret" | S p a m ) = 0.4802 .
+#   - The probability of an email containing the word "secret" given that the email is not spam is 12.84%. That is
+#      P ( "secret" | S p a m C ) = 0.1284 .
+# - Calculate:
+#   - P(SpamC). Assign the result to p_non_spam.
+#   - P(Spam ∩ "secret"). Assign the result to p_spam_and_secret.
+#   - P(SpamC ∩ "secret"). Assign the result to p_non_spam_and_secret. P("secret"). Assign the result to p_secret.
 def main():
-    statement_1 = False
-    statement_2 = True
-    statement_3 = True
+    p_spam = 0.2388  # p(spam)
+    p_secret_given_spam = 0.4802  # p(secret | spam)
+    p_secret_given_non_spam = 0.1284  # p('secret' | not spam)
+
+    p_non_spam = 1 - p_spam
+    p_spam_and_secret = p_spam * p_secret_given_spam
+    p_non_spam_and_secret = p_non_spam * p_secret_given_non_spam
+    p_secret = p_spam * p_secret_given_spam + p_non_spam * p_secret_given_non_spam
 
 
 if __name__ == '__main__':
