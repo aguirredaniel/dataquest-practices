0beba4d647ddc3dcf2b662d82f32fa4ee4526a38
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-14 23:33:16 -0800

    1. Independence vs. Exclusivity

---
+++ challenge_06_probability_statistics/17.bayes-theorem/main.py
@@ -0,0 +1,17 @@
+# - For the exercises below, consider the following probabilities:
+#   - The probability of being infected with HIV is 0.00014. That is P ( H I V ) = 0.00014 .
+#   - The probability of being infected with HIV given a positive result from an HIV test is 0.03.
+# - Assess with True or False the following statements:
+#   - Events HIV and T+ are independent. If you think this statement is true, then assign the boolean True to
+#     statement_1, otherwise assign False. Events HIV and HIVC are mutually exclusive.
+#   - If you think this statement is true, then assign the boolean True to statement_2, otherwise assign False.
+#   - Events HIVC and T+ are dependent. If you think this statement is true, then assign the boolean True to
+#     statement_3, otherwise assign False.
+def main():
+    statement_1 = False
+    statement_2 = True
+    statement_3 = True
+
+
+if __name__ == '__main__':
+    main()
