72c770adbcbdcc18cd13eb83a069aaabcaeff1b8
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-18 00:42:20 -0800

    5. A One-Word Message

--- challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
+++ challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
@@ -1,27 +1,17 @@
-# - A new mobile message has been received: "URGENT!! You have one day left to claim your $873 prize." The following
-#   probabilities are known:
-#   - P(Spam)=0.5
-#   - P(SpamC)=0.5
-#   - P(NewMessage|Spam)=0.75
-#   - P(NewMessage|SpamC)=0.3334
-# - Classify this new message as spam or non-spam:
-#   - Calculate P(Spam|New Message). Assign your answer to p_spam_given_new_message.
-#   - Calculate P(SpamC|New Message). Assign your answer to p_non_spam_given_new_message.
-#   - Classify the message by comparing the probability values. If the message is spam, then assign the string 'spam'
-#     to the variable classification. Otherwise, assign the string 'non-spam'.
+# - Calculate P(SpamC) and assign the answer to p_non_spam.
+# - Calculate P("secret"|SpamC) and assign the answer to p_secret_given_non_spam.
+# - Calculate P(SpamC|"secret") and assign the answer to p_non_spam_given_secret.
+# - Compare P(SpamC|"secret") with P(Spam|"secret") and classify the message "secret" — if the message is spam, then
+#   assign the string 'spam' to the variable classification, otherwise assign the string 'non-spam'.
 def main():
-    p_spam = 0.5
-    p_non_spam = 0.5
-    p_new_message_given_spam = 0.75
-    p_new_message_given_non_spam = 0.3334
+    p_spam_given_secret = 8 / 21
+    p_spam = 2 / 3
+    p_non_spam = 1 - p_spam
+    p_secret_given_non_spam = 1 / 4
+    p_non_spam_given_secret = p_non_spam * p_secret_given_non_spam
+    classification = 'spam' if p_spam_given_secret > p_non_spam_given_secret else 'non-spam'
 
-    p_spam_given_new_message = p_spam * p_new_message_given_spam
-
-    p_non_spam_given_new_message = p_non_spam * p_new_message_given_non_spam
-
-    classification = 'spam' if p_spam_given_new_message > p_non_spam_given_new_message else 'non-spam'
-
-    print(p_spam_given_new_message, p_non_spam_given_new_message, classification, sep='\n')
+    print(p_non_spam, p_secret_given_non_spam, p_non_spam_given_secret, classification, sep='\n')
 
 
 if __name__ == '__main__':
