c542240b03344346dc6314fb89ddb55946e584c4
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-18 00:42:27 -0800

    6. Multiple Words

--- challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
+++ challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
@@ -1,17 +1,16 @@
-# - Calculate P(SpamC) and assign the answer to p_non_spam.
-# - Calculate P("secret"|SpamC) and assign the answer to p_secret_given_non_spam.
-# - Calculate P(SpamC|"secret") and assign the answer to p_non_spam_given_secret.
-# - Compare P(SpamC|"secret") with P(Spam|"secret") and classify the message "secret" — if the message is spam, then
-#   assign the string 'spam' to the variable classification, otherwise assign the string 'non-spam'.
+# -Calculate P(SpamC|w1, w2, w3, w4). Assign the answer to p_non_spam_given_w1_w2_w3_w4. Check the hint if you get stuck
+# - Compare P(SpamC|w1, w2, w3, w4) with P(Spam|w1, w2, w3, w4) and classify the message "secret place secret secret"
+#   — if the message is spam, then assign the string 'spam' to the variable classification. Otherwise, assign the string
+#   'non-spam'.
 def main():
-    p_spam_given_secret = 8 / 21
-    p_spam = 2 / 3
-    p_non_spam = 1 - p_spam
-    p_secret_given_non_spam = 1 / 4
-    p_non_spam_given_secret = p_non_spam * p_secret_given_non_spam
-    classification = 'spam' if p_spam_given_secret > p_non_spam_given_secret else 'non-spam'
+    p_spam_given_w1_w2_w3_w4 = 64 / 4802
 
-    print(p_non_spam, p_secret_given_non_spam, p_non_spam_given_secret, classification, sep='\n')
+    p_non_spam = 2 / 4
+    p_non_spam_given_w1_w2_w3_w4 = p_non_spam * 2 / 9 * 1 / 9 * 2 / 9 * 2 / 9
+
+    classification = 'spam' if p_spam_given_w1_w2_w3_w4 > p_non_spam_given_w1_w2_w3_w4 else 'non-spam'
+
+    print(p_non_spam, classification, sep='\n')
 
 
 if __name__ == '__main__':
