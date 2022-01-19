5c1817ab255971823a090ec0e15c010ed5a6d895
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-18 21:03:07 -0800

    9. Edge Cases

--- challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
+++ challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
@@ -1,16 +1,22 @@
-# -Calculate P(SpamC|w1, w2, w3, w4). Assign the answer to p_non_spam_given_w1_w2_w3_w4. Check the hint if you get stuck
-# - Compare P(SpamC|w1, w2, w3, w4) with P(Spam|w1, w2, w3, w4) and classify the message "secret place secret secret"
-#   — if the message is spam, then assign the string 'spam' to the variable classification. Otherwise, assign the string
-#   'non-spam'.
+# -Calculate P(SpamC|"secret code to unlock the money"). Assign your answer to p_non_spam_given_message.
+# - Print p_spam_given_message and p_non_spam_given_message. Why do you think we got these values? We'll discuss more
+#   about this in the next screen.
 def main():
-    p_spam_given_w1_w2_w3_w4 = 64 / 4802
+    p_spam = 2 / 4
+    p_secret_given_spam = 4 / 7
+    p_the_given_spam = 0 / 7
+    p_money_given_spam = 2 / 7
+    p_spam_given_message = (p_spam * p_secret_given_spam *
+                            p_the_given_spam * p_money_given_spam)
 
-    p_non_spam = 2 / 4
-    p_non_spam_given_w1_w2_w3_w4 = p_non_spam * 2 / 9 * 1 / 9 * 2 / 9 * 2 / 9
+    p_non_spam = 1 - p_spam
+    p_secret_given_non_spam = 2 / 9
+    p_the_given_non_spam = 1 / 9
+    p_money_given_non_spam = 0 / 9
 
-    classification = 'spam' if p_spam_given_w1_w2_w3_w4 > p_non_spam_given_w1_w2_w3_w4 else 'non-spam'
+    p_non_spam_given_message = p_non_spam * p_secret_given_non_spam * p_the_given_non_spam * p_money_given_non_spam
 
-    print(p_non_spam, classification, sep='\n')
+    print(p_spam_given_message, p_non_spam_given_message, sep='\n')
 
 
 if __name__ == '__main__':
