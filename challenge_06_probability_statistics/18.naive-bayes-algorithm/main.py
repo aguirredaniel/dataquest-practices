20b1d66cfe8e0cd46a76c4b0071e2caddf6d779f
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-18 21:13:08 -0800

    10. Additive Smoothing

--- challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
+++ challenge_06_probability_statistics/18.naive-bayes-algorithm/main.py
@@ -3,20 +3,20 @@
 #   about this in the next screen.
 def main():
     p_spam = 2 / 4
-    p_secret_given_spam = 4 / 7
-    p_the_given_spam = 0 / 7
-    p_money_given_spam = 2 / 7
+    p_secret_given_spam = (4 + 1) / (7 + 9)
+    p_the_given_spam = 1 / (7 + 9)
+    p_money_given_spam = (2 + 1) / (7 + 9)
     p_spam_given_message = (p_spam * p_secret_given_spam *
                             p_the_given_spam * p_money_given_spam)
 
     p_non_spam = 1 - p_spam
-    p_secret_given_non_spam = 2 / 9
-    p_the_given_non_spam = 1 / 9
-    p_money_given_non_spam = 0 / 9
+    p_secret_given_non_spam = (2 + 1) / (9 + 9)
+    p_the_given_non_spam = (1 + 1) / (9 + 9)
+    p_money_given_non_spam = 1 / (9 + 9)
 
     p_non_spam_given_message = p_non_spam * p_secret_given_non_spam * p_the_given_non_spam * p_money_given_non_spam
-
-    print(p_spam_given_message, p_non_spam_given_message, sep='\n')
+    classification = 'spam' if p_spam_given_message > p_non_spam_given_message else 'non-spam'
+    print(p_spam_given_message, p_non_spam_given_message, classification, sep='\n')
 
 
 if __name__ == '__main__':
