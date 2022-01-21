cb69b3fe25a5f22d354d659e33997608b07135cb
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-01-20 22:20:18 -0800

    5. Permutation Test

--- challenge_06_probability_statistics/19.significance-testing/main.py
+++ challenge_06_probability_statistics/19.significance-testing/main.py
@@ -12,7 +12,24 @@ def main():
 
     mean_difference = mean_group_b - mean_group_a
 
-    print(mean_difference, sep='\n')
+    all_values = data.flatten()
+    mean_differences = []
+
+    for _ in range(1000):
+        group_a = []
+        group_b = []
+        for weight_lost in all_values:
+            random = np.random.rand(1, 1)
+            if random >= 0.5:
+                group_a.append(weight_lost)
+            else:
+                group_b.append(weight_lost)
+
+        iteration_mean_difference = np.mean(group_b) - np.mean(group_a)
+        mean_differences.append(iteration_mean_difference)
+
+    plt.hist(mean_differences)
+    plt.show()
 
 
 if __name__ == '__main__':
