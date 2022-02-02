b5777babc615f980296b84006eaa45f93301c169
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-02-01 22:41:35 -0800

    10. Using SciPy

--- challenge_06_probability_statistics/20.chi-squared-tests/main.py
+++ challenge_06_probability_statistics/20.chi-squared-tests/main.py
@@ -1,25 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.stats import chisquare
 
 
-# -For each category (White, Black, Asian-Pac-Islander, Amer-Indian-Eskimo, and Other):
-# - compute the difference between the expected and observed counts,
-# - square the difference,
-# - divide by the expected value,
-# - append each result to a list, and
-# - sum the values in the list and assign the result to race_chisq.
+# - Use the scipy.stats.chisquare function to calculate the p-value for the following table:
+# - Assign the result to race_pvalue.
 def chi_squared(observed, expected):
     return (observed - expected) ** 2 / expected
 
 
 def main():
-    table = [(27816, 26146.5), (3124, 3939.9), (1039, 944.3),
-             (311, 260.5), (271, 1269.8), ]
+    observed = np.array([27816, 3124, 1039, 311, 271, ])
+    expected = np.array([26146.5, 3939.9, 944.3, 260.5, 1269.8])
 
-    chi_squareds = (chi_squared(*sample) for sample in table)
-    race_chisq = sum(chi_squareds)
+    race_chisq, race_pvalue = chisquare(observed, expected)
 
-    print(race_chisq)
+    print(race_chisq, race_pvalue, sep='\n')
 
 
 if __name__ == '__main__':
