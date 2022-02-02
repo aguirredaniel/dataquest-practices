91c4686c1c3da01d8e69f856d383eb8165a7b835
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-02-01 21:22:04 -0800

    2. Calculating Differences

---
+++ challenge_06_probability_statistics/20.chi-squared-tests/main.py
@@ -0,0 +1,9 @@
+def main():
+    female_diff = (10771 - 16280.5) / 16280.5
+    male_diff = (21790 - 16280.5) / 16280.5
+
+    print(female_diff, male_diff, sep='\n')
+
+
+if __name__ == '__main__':
+    main()
