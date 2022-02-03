9b94158b347e7ce213c3a130f8bedbf524449927
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-02-02 21:44:17 -0800

    4. Finding statistical significance

--- challenge_06_probability_statistics/21.multi-category-chi-squared-tests/main.py
+++ challenge_06_probability_statistics/21.multi-category-chi-squared-tests/main.py
@@ -1,9 +1,14 @@
+import numpy as np
+from scipy.stats import chisquare
+
+
 def chi_squared(observed, expected):
     return (observed - expected) ** 2 / expected
 
 
-# - Compute the chi-squared value for the observed values and the expected values.
-# - Assign the result to chisq_gender_income.
+# - Use the scipy.stats.chisquare function to find the chi-squared value and p-value for the above observed and expected
+#   counts.
+# - Assign the p-value to pvalue_gender_income.
 def main():
     males_over50k = .241 * .67 * 32561
     males_under50k = 0.759 * .67 * 32561
@@ -13,10 +18,8 @@ def main():
     observed_values = [6662, 15128, 1179, 9592]
     expected_values = [males_over50k, males_under50k, females_over50k, females_under50k]
 
-    chi_squareds = [chi_squared(observed, expected) for observed, expected in zip(observed_values, expected_values)]
-    chisq_gender_income = sum(chi_squareds)
-
-    print(chisq_gender_income)
+    chisquare_value, pvalue_gender_income = chisquare(observed_values, expected_values)
+    print(chisquare_value, pvalue_gender_income, sep='\n')
 
 
 if __name__ == '__main__':
