1d02789a57eebc131b08aa9fda8ad1627e53bf29
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-02-02 22:22:09 -0800

    6. Finding Expected Values

--- challenge_06_probability_statistics/21.multi-category-chi-squared-tests/main.py
+++ challenge_06_probability_statistics/21.multi-category-chi-squared-tests/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import chisquare
+from scipy.stats import chisquare, chi2_contingency
 import pandas as pd
 
 
@@ -7,13 +7,15 @@ def chi_squared(observed, expected):
     return (observed - expected) ** 2 / expected
 
 
-# - Use the pandas.crosstab function to print out a table comparing the sex column of income to the race column of
-#   income.
+# - Use the scipy.stats.chi2_contingency function to calculate the p-value for the sex and race columns of income.
+# - Assign the result to pvalue_gender_race.
 def main():
     income = pd.read_csv('income.csv')
 
     table = pd.crosstab(income["sex"], [income["race"]])
-    print(table)
+    chi2, pvalue_gender_race, dof, expected = chi2_contingency(table)
+
+    print(chi2, pvalue_gender_race, dof, expected, sep='\n')
 
 
 if __name__ == '__main__':
