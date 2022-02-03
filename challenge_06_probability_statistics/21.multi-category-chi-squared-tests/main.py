6408ce2f4f4b1f8c10db33d40d4ba13a24519d8a
Daniel Aguirre <aguirre.gomez.daniel@gmail.com>
2022-02-02 22:19:36 -0800

    5. Cross Tables

--- challenge_06_probability_statistics/21.multi-category-chi-squared-tests/main.py
+++ challenge_06_probability_statistics/21.multi-category-chi-squared-tests/main.py
@@ -1,25 +1,19 @@
 import numpy as np
 from scipy.stats import chisquare
+import pandas as pd
 
 
 def chi_squared(observed, expected):
     return (observed - expected) ** 2 / expected
 
 
-# - Use the scipy.stats.chisquare function to find the chi-squared value and p-value for the above observed and expected
-#   counts.
-# - Assign the p-value to pvalue_gender_income.
+# - Use the pandas.crosstab function to print out a table comparing the sex column of income to the race column of
+#   income.
 def main():
-    males_over50k = .241 * .67 * 32561
-    males_under50k = 0.759 * .67 * 32561
-    females_over50k = 0.241 * .33 * 32561
-    females_under50k = 0.759 * .33 * 32561
+    income = pd.read_csv('income.csv')
 
-    observed_values = [6662, 15128, 1179, 9592]
-    expected_values = [males_over50k, males_under50k, females_over50k, females_under50k]
-
-    chisquare_value, pvalue_gender_income = chisquare(observed_values, expected_values)
-    print(chisquare_value, pvalue_gender_income, sep='\n')
+    table = pd.crosstab(income["sex"], [income["race"]])
+    print(table)
 
 
 if __name__ == '__main__':
